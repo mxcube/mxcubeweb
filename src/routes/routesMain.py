3b39903f99bd7a4dc77c307e64adce1bac4e58ef
Mikel Eguiraun <mikel.eguiraun@maxlab.lu.se>
2015-09-24 13:28:30 +0200

    Added routes, one file per component

---
+++ src/routes/routesMain.py
@@ -0,0 +1,8 @@
+from flask import session, redirect, url_for, render_template, request, Response
+import logging
+from .. import app as mxcube
+
+@mxcube.route("/")
+def serve_static_file():
+	logging.getLogger('HWR').info('[Main] Serving main page')
+	return mxcube.send_static_file('Main.html')
