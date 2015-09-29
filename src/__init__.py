6cd347f51b56b0fc3ab77b20935be5c5ed1aea6d
blissadm@id30_massif3 <blissadm@id30_massif3>
2015-09-29 14:29:04 +0200

    added command line option to specify Hardware Repository XML files path refactored imports, tried to simplify sys.path magic

--- src/__init__.py
+++ src/__init__.py
@@ -1,29 +1,35 @@
 from flask import Flask, session, redirect, url_for, render_template, request, Response
 from flask.ext.socketio import SocketIO
-socketio = SocketIO()
-from threading import Thread
+from optparse import OptionParser
 import os, sys
 
+socketio = SocketIO()
 app = Flask(__name__, static_url_path='')
 app.debug = True
 
-###Importing all the rest-routes
-import routes.routesMain, routes.routesBeamline, routes.routesCollection, routes.routesMockups, routes.routesSample, routes.routesSampleCentring
+opt_parser = OptionParser()
+opt_parser.add_option("-r", "--repository", 
+                      dest="hwr_directory", 
+                      help="Hardware Repository XML files path", 
+                      default=os.path.join(os.path.dirname(__file__), 'HardwareObjects.xml/'))
+cmdline_options, args = opt_parser.parse_args()
 
 ###Initialization of the HardwareObjects
-from HardwareRepository import HardwareRepository
+from .HardwareRepository import HardwareRepository 
+HardwareRepository.addHardwareObjectsDirs([os.path.join(os.path.dirname(__file__), 'HardwareObjects')])
+# this is to allow Hardware Objects to do 'from HardwareRepository import ...'
+sys.path.insert(0, os.path.dirname(__file__)) 
 
-sys.path.insert(0, os.path.dirname(__file__))
-sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'HardwareObjects/'))
-sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'HardwareRepository/HardwareObjects/'))
-
-hwr_directory = os.path.join(os.path.dirname(__file__), 'HardwareObjects.xml/')
-hwr = HardwareRepository.HardwareRepository(os.path.abspath(hwr_directory))
+hwr_directory = cmdline_options.hwr_directory
+hwr = HardwareRepository.HardwareRepository(os.path.abspath(os.path.expanduser(hwr_directory)))
 hwr.connect()
 
 app.resolution = hwr.getHardwareObject("/resolution-mockup")
 app.diffractometer = hwr.getHardwareObject("/minidiff")
 #app.beamline = hwr.getHardwareObject("/beamline-setup")
 #app.queue = hwr.getHardwareObject("/queue-model")
+
+###Importing all REST-routes
+import routes.routesMain, routes.routesBeamline, routes.routesCollection, routes.routesMockups, routes.routesSample, routes.routesSampleCentring
 
 
