fce284b135f63b98078a9a9c50c92f47a99f8b7f
Mikel Eguiraun <Mikel.Eguiraun@maxlab.lu.se>
2015-10-30 11:33:39 +0100

    dynamically update the text of the modal

--- mxcube3/js/SampleView/Functions.jsx
+++ mxcube3/js/SampleView/Functions.jsx
@@ -5,20 +5,18 @@ window.app_dispatcher = _.extend({}, Backbone.Events);
 console.log('dispatcher done');
 
 $(document).ready(function(){
-		console.log('socket starting')
         $('[data-toggle="tooltip"]').tooltip();  
         var socket = io.connect('http://' + document.domain + ':' + location.port+'/test');// + namespace);
-        console.log('http://' + document.domain + ':' + location.port+'/test')// + namespace)
         socket.on('connect', function() {
             socket.emit('my event', {data: 'I\'m connected!'});
         });
-
         socket.emit('my event', {data: '#emit_data'}); 
         socket.on('newSignal', function(msg) {
             console.log('message received')
             var d = new Date()
             $('#log').append('<br>'+d.toLocaleString()+ '  #' +msg['sender']+'::'+msg['signal']+ '::Data: '+msg['data'] );
             if(msg['signal'] == 'centringSuccessful'){
+                $("#Modal_Centring").find('p').html('<p>'+msg['signal']+'</p>')
                 $('#Modal_Centring').modal('show')
             };
             console.log(msg)
