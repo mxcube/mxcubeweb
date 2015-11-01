d20f4dfa7ce842242eecad9bd457b84808964180
Matias Guijarro <guijarro@esrf.fr>
2015-11-01 07:52:51 +0100

    added Bootstrap grid to contain UI components added navbar, sample filtering input, check sc contents button and 'Go to data collection' components, using React Bootstrap

--- mxcube3/ui/main.jsx
+++ mxcube3/ui/main.jsx
@@ -1,10 +1,35 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import SampleGrid from 'SampleGrid'
-import { samples_list } from 'test-samples-list'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import SampleGrid from 'SampleGrid';
+import { samples_list } from 'test-samples-list';
+import { Navbar, NavBrand, Input, Button, Glyphicon  } from "react-bootstrap";
 
-var sample_grid = ReactDOM.render(<SampleGrid />, 
-                  document.getElementById("container"))
+const innerSearchIcon = (
+    <Button><Glyphicon glyph="search"/></Button>
+);
+
+const searchInput = (
+    <form className="form-horizontal">
+        <Input type="text" label="Filter" labelClassName="col-xs-1" wrapperClassName="col-xs-3" buttonAfter={innerSearchIcon}/>
+    </form>
+);
+
+const navbar = (
+    <Navbar inverse fluid>
+        <NavBrand><a href="#">MXCuBE 3</a></NavBrand>
+    </Navbar>
+);
+
+const checkScContents = (
+    <Button className="btn-primary">Check sample changer contents</Button>
+);
+
+
+ReactDOM.render(navbar, document.getElementById("header"));
+let filter_input = ReactDOM.render(searchInput, document.getElementById("filter_input"));
+let check_sc_contents = ReactDOM.render(checkScContents, document.getElementById("check_sc_contents"));
+let sample_grid = ReactDOM.render(<SampleGrid/>, document.getElementById("sample_grid"));
+let go_to_dc = ReactDOM.render(<span className='pull-right'><a href='data_collection'>Go to data collection<Glyphicon glyph='menu-right'/></a></span>, document.getElementById("go_to_dc"));
 
 sample_grid.add_samples(samples_list)
 
