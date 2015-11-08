0de758aba94492f92662a47ab711df5e8b8cdd86
Matias Guijarro <guijarro@id02det1.esrf.fr>
2015-11-08 06:59:24 +0100

    added logout state

--- mxcube3/ui/Login.jsx
+++ mxcube3/ui/Login.jsx
@@ -2,7 +2,7 @@ import ReactDOM from 'react-dom';
 import React from 'react';
 import classNames from 'classnames';
 import "bootstrap-webpack!bootstrap-webpack/bootstrap.config.js";
-import { Nav, Input, ButtonInput } from "react-bootstrap";
+import { Nav, Input, ButtonInput, NavItem } from "react-bootstrap";
 
 export default class LoginForm extends React.Component {
     constructor(props) {
@@ -19,17 +19,24 @@ export default class LoginForm extends React.Component {
         }});
     }
 
+    logOut() {
+        this.setState({proposal: null});
+    }
+
     render() {
         let login_input_form = "";
         if (this.state.proposal) {
-            login_input_form = (<p className="text-info">{this.state.proposal.Proposal.title}</p>)
+            login_input_form = (<form className="navbar-form">
+                                    <p className="navbar-text">{this.state.proposal.Proposal.title}</p>
+                                    <ButtonInput bsSize="small" bsStyle="info" value="Log out" onClick={this.logOut.bind(this)}/>
+                               </form>);
         } else {
-            login_input_form = (<form className="navbar-form" action=""><Input bsSize="small" ref="proposal" type="text" name="proposal" placeholder="Proposal"/>{' '}
-              <Input bsSize="small" ref="password" type="password" name="password" placeholder="Password"/>{' '}
-              <ButtonInput bsSize="small" bsStyle="info" value="Sign in" onClick={this.signIn.bind(this)}/></form>);
+            login_input_form = (<form className="navbar-form" action="">
+                                    <Input bsSize="small" ref="proposal" type="text" name="proposal" placeholder="Proposal"/>{' '}
+                                    <Input bsSize="small" ref="password" type="password" name="password" placeholder="Password"/>{' '}
+                                    <ButtonInput bsSize="small" bsStyle="info" value="Sign in" onClick={this.signIn.bind(this)}/>
+                                </form>);
         }
-	return (<Nav right eventKey={0}>
-              {login_input_form}
-        </Nav>);
+	return (<Nav right eventKey={0}>{login_input_form}</Nav>);
     }    
 }
