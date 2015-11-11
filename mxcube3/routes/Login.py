5b581fe80ad62be0a01f469d5cd15825ee5ad0db
Matias Guijarro <guijarro@esrf.fr>
2015-11-11 06:50:42 +0100

    fixed proposal parsing

--- mxcube3/routes/Login.py
+++ mxcube3/routes/Login.py
@@ -7,6 +7,6 @@ import itertools
 def login():
     Proposal = request.args['proposal']
     password = request.args['password']
-    proposal = "".join(itertools.ifilterfalse(lambda c: c.isdigit(), Proposal))
+    proposal = "".join(itertools.takewhile(lambda c: not c.isdigit(), Proposal))
     prop_number = Proposal[len(proposal):]
     return jsonify(mxcube.db_connection.get_proposal(proposal, prop_number))
