8ab1592f2b0259ae1f379f010c78bccd56fc4593
Matias Guijarro <guijarro@id02det1.esrf.fr>
2015-11-11 08:27:29 +0100

    changed route to follow rules for API calls

--- mxcube3/ui/Login.jsx
+++ mxcube3/ui/Login.jsx
@@ -15,7 +15,7 @@ export default class LoginForm extends React.Component {
         let proposal = this.refs.proposal.getValue();
         let password = this.refs.password.getValue();
         let self = this;
-        $.ajax({ url: 'mxcube/api/login', type: 'GET', data: { proposal: proposal, password: password }, success: function(res) {
+        $.ajax({ url: 'mxcube/api/v0.1/login', type: 'GET', data: { proposal: proposal, password: password }, success: function(res) {
            self.setState({proposal: res}); 
         }, error: function(req, error_string, exc) {
             window.error_notification.notify(error_string);
