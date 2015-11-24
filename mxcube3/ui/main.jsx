4091f3d79d0c1d9143c2bc39febef564a94c2680
Fredrik Bolmsten <fredrik.bolmsten@maxlab.lu.se>
2015-11-24 16:40:12 +0100

    Changed to work with react router

--- mxcube3/ui/main.jsx
+++ mxcube3/ui/main.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router, Route, Link } from 'react-router'
+import SampleView from './SampleView/MainView'
+import SampleGridMain from './samples'
 import LoginForm from 'Login';
 import { Navbar, NavBrand, Nav, NavItem } from "react-bootstrap";
 import ErrorNotificationPanel from 'Logging';
@@ -16,41 +19,52 @@ class MXNavbar extends React.Component {
     }
 
     render() {
-      return (<Navbar inverse fluid>
+      return (
+      <div>
+      <Navbar inverse fluid>
         <NavBrand>MXCuBE 3</NavBrand>
         <Nav navbar>
-          <NavItem eventKey={1} active={(this.state.active === 'samples') ? true : false} href="#">Samples</NavItem>
-          <NavItem eventKey={2} active={(this.state.active === 'dc') ? true : false} href="#dc">Data collection</NavItem>
+          <NavItem eventKey={1} active={(this.state.active === 'samples') ? true : false} href="#/samplegrid">Samples</NavItem>
+          <NavItem eventKey={2} active={(this.state.active === 'dc') ? true : false} href="#/datacollection">Data Collection</NavItem>
         </Nav>
         <LoginForm/>
-      </Navbar>)
+      </Navbar>
+      {this.props.children}
+      </div>
+      )
     }
 }
 
-window.error_notification = ReactDOM.render(<ErrorNotificationPanel/>, document.getElementById("error_notification_panel"));
-let navbar = ReactDOM.render(<MXNavbar/>, document.getElementById("header"));
+ReactDOM.render((
+  <Router>
+    <Route path="/" component={MXNavbar}>
+      <Route path="samplegrid" component={SampleGridMain}/>
+      <Route path="datacollection" component={SampleView}/>
+    </Route>
+  </Router>
+), document.getElementById("main"));
 
-var resolveRoute = function() {
-  // If no hash or hash is '#' we lazy load the SampleGrid component
-  if (!location.hash || location.hash.length === 1) {
-    require.ensure([], function () {
-      navbar.set_active('samples');
-      $('#samples').show();
-      $('#dc').hide();
-      require("samples"); 
-    });
+// var resolveRoute = function() {
+//   // If no hash or hash is '#' we lazy load the SampleGrid component
+//   if (!location.hash || location.hash.length === 1) {
+//     require.ensure([], function () {
+//       navbar.set_active('samples');
+//       $('#samples').show();
+//       $('#dc').hide();
+//       require("samples"); 
+//     });
 
-  // Or if route is #dc we lazy load that
-  } else if (location.hash === '#dc') {
-    require.ensure([], function () {
-      navbar.set_active('dc');
-      $('#dc').show();
-      $('#samples').hide();
-      require("./SampleView/MainView"); 
-    });
-  }
-};
+//   // Or if route is #dc we lazy load that
+//   } else if (location.hash === '#dc') {
+//     require.ensure([], function () {
+//       navbar.set_active('dc');
+//       $('#dc').show();
+//       $('#samples').hide();
+//       require("./SampleView/MainView"); 
+//     });
+//   }
+// };
 
-window.onhashchange = resolveRoute;
+// window.onhashchange = resolveRoute;
 
-resolveRoute();
+// resolveRoute();
