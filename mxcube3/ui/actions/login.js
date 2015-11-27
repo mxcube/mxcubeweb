5933d7bf1b049d0f2cc749fc3b80609b85d0e532
Jie Nan <jie.nan@maxlab.lu.se>
2015-11-27 12:12:44 +0000

    fix Login

--- mxcube3/ui/actions/login.js
+++ mxcube3/ui/actions/login.js
@@ -1,6 +1,19 @@
 export function doLogin(proposal, password) {
-    return { type: "LOGIN", proposal, password }
+    var result = {};
+    $.ajax({ url: 'mxcube/api/v0.1/login', type: 'GET', data: {proposal:proposal, password: password},
+// not recommended way (Synchronous XMLHttpRequest on the main thread is deprecated), but it works, looking into a better solution. 
+                async: false, 
+                success: function(data) {
+                    // res is a Proposal object
+                    result= data;
+                }, 
+                error: function(req, error_string, exc) {
+		    console.log ("ajax error")
+                }
+            })
+    return { type: "LOGIN", result }
 }
+
 
 export function doSignOut() {
     return { type: "SIGNOUT" }
--- mxcube3/ui/reducers/login.js
+++ mxcube3/ui/reducers/login.js
@@ -1,21 +1,10 @@
 export default (state={}, action) => {
     switch (action.type) {
         case 'LOGIN':
-            let result = {};
-     
-            window.error_notification.clear();
-     
-            $.ajax({ url: 'mxcube/api/v0.1/login', type: 'GET', data: action, 
-                success: function(res) {
-                    // res is a Proposal object
-                    result = res;
-                }, 
-                error: function(req, error_string, exc) {
-                    window.error_notification.notify(error_string);
-                }
-            })
-            //return { Proposal: { title: "HELLO" } }
-            return result
+// moved the login ajax to actions, since "Redux assumes that you never mutate the objects it gives to you in the reducer. Every single time, you must return the new state object"
+            window.error_notification.clear(); 
+            return action.result
+//            return Object.assign(action.result)
         case 'SIGNOUT':
             window.error_notification.clear();
             return { Proposal: null }
