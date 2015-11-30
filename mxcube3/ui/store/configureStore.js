0e2c1e2a73cfea2b0917156a175c01c464bd024d
Fredrik Bolmsten <fredrik.bolmsten@maxlab.lu.se>
2015-11-30 12:38:37 +0100

    Made store with middleware

--- mxcube3/ui/store/configureStore.js
+++ mxcube3/ui/store/configureStore.js
@@ -3,12 +3,12 @@ import logger from 'redux-logger'
 import thunk from 'redux-thunk'
 import rootReducer from '../reducers'
 
-export default function configureStore() {
+export default function configureStore(initialState) {
 
 	const middleware = [ thunk, logger() ];
 
 	const createStoreWithMiddleware = applyMiddleware(...middleware)(createStore)
-	const store = createStoreWithMiddleware(rootReducer)
+	const store = createStoreWithMiddleware(rootReducer, initialState)
 
   if (module.hot) {
     // Enable Webpack hot module replacement for reducers
