b6cd0bbdb529f0ae1f8f2f4c34484087315b8b61
Matias Guijarro <guijarro@id02det1.esrf.fr>
2015-12-03 21:03:19 +0100

    fixed 'fetch' call in login actions

--- mxcube3/ui/actions/login.js
+++ mxcube3/ui/actions/login.js
@@ -2,11 +2,17 @@ import fetch from 'isomorphic-fetch'
 
 export function doLogin(proposal, password) {
     return function(dispatch) {
-         fetch('mxcube/api/v0.1/login', { method: 'GET', body: {proposal, password} })
-            .then(response => response.json())
-            .then(json => {
+         fetch('mxcube/api/v0.1/login', { 
+            method: 'POST', 
+            headers: {
+                'Accept': 'application/json',
+                'Content-type': 'application/json'
+            },
+            body: JSON.stringify({ proposal, password })
+          }).then(response => response.json())
+          .then(json => {
                 dispatch(afterLogin(json));
-            })
+          })
     }
 }
 
