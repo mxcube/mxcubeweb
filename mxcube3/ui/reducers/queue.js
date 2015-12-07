0b1a2d2e612940ffc6d1db3aeb978c65ae5b0e34
Fredrik Bolmsten <fredrik.bolmsten@maxlab.lu.se>
2015-12-07 15:57:42 +0100

    Minor grammatic change to queue reducer

--- mxcube3/ui/reducers/queue.js
+++ mxcube3/ui/reducers/queue.js
@@ -6,11 +6,9 @@ export default (state={
     switch (action.type) {
         case 'ADD_SAMPLE':
 
-        	let index = state.index +1;
-        	
             return Object.assign({},
              			state, 
-             			{todo:[...state.todo, action.type]},
+             			{todo:[...state.todo, action.id]},
              			
              			);
         default:
