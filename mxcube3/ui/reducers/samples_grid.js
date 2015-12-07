2da27f29b4b0c62bbe11ab8a101de4108e9b1be7
Fredrik Bolmsten <fredrik.bolmsten@maxlab.lu.se>
2015-12-07 08:33:31 +0100

    Changed to work with object instead of array

--- mxcube3/ui/reducers/samples_grid.js
+++ mxcube3/ui/reducers/samples_grid.js
@@ -1,16 +1,14 @@
-export default (state={ samples_list: [] }, action) => {
+export default (state={ samples_list: {} }, action) => {
     switch (action.type) {
     case "UPDATE_SAMPLES":
         return Object.assign({}, state, { samples_list: action.samples_list });
     case "TOGGLE_SELECTED":
-        let sample_item = state.samples_list[action.index]
-        let selected = !sample_item.selected;
-    	//if (! sample_item.loadable) { selected = false };
-        sample_item.selected = selected;
-        let samples_list = [ ...state.samples_list.slice(0, action.index),
-                           sample_item, 
-                           ...state.samples_list.slice(action.index+1) ]
-	return Object.assign({}, state, { samples_list });
+        let sample_item = {}
+        sample_item[action.index] = state.samples_list[action.index];
+        sample_item[action.index].selected = !sample_item[action.index].selected ;
+    
+
+	return Object.assign({}, state, { sample_item  });
     default:
         return state
     }
