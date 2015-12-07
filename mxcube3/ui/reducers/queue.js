2ccaa8783aeff4f128fb7060b8f03d8027c6404a
Fredrik Bolmsten <fredrik.bolmsten@maxlab.lu.se>
2015-12-07 08:33:48 +0100

    Clean up

--- mxcube3/ui/reducers/queue.js
+++ mxcube3/ui/reducers/queue.js
@@ -1,6 +1,4 @@
 export default (state={
-	samples:{},
-	index: 0,
 	todo:[],
 	history:[],
 	current:0
@@ -9,19 +7,11 @@ export default (state={
         case 'ADD_SAMPLE':
 
         	let index = state.index +1;
-        	let sample = {};
-        	sample[index] = {
-        		//Add sample information here
-        		info: "Sample " + index
-        	};
-
-
+        	
             return Object.assign({},
              			state, 
-             			{ samples: Object.assign({}, 
-             			state.samples, sample)}, 
-             			{index: index},
-             			{todo:[...state.todo, index]}
+             			{todo:[...state.todo, action.type]},
+             			
              			);
         default:
             return state;
