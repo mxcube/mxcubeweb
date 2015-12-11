1fded7704c02952e1994a5065aa5516c7e23a458
Fredrik Bolmsten <fredrik.bolmsten@maxlab.lu.se>
2015-12-11 14:06:44 +0100

    Creating the tree structure needed to make the queue

--- mxcube3/ui/containers/SampleQueueContainer.js
+++ mxcube3/ui/containers/SampleQueueContainer.js
@@ -1,26 +1,24 @@
 import React, { Component, PropTypes } from 'react'
 import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
-import SampleQueue from '../components/General/SampleQueue';
+import SampleQueue from '../components/SampleQueue/SampleQueue';
+import SampleQueueButtons from '../components/SampleQueue/SampleQueueButtons';
 import * as QueueActions from '../actions/queue'
+import * as SampleActions from '../actions/samples_grid'
+
 
 class SampleQueueContainer extends Component {
 
     
   render() {
+    const selected = this.props.selected;
     return (
+
+
       <div className="row">
-      <div className="col-xs-12">
-        <div className="panel panel-primary text-center">
-                <div className="panel-heading">
-                  <h8 className="panel-title text-center">SampleQueue</h8>
-                </div>
-                <div className="panel-body">
-                {this.props.todo.map((function(result,index) {
-                      return <SampleQueue key={index} sampledata={this.props.samples_list[result]}/>;
-                  }.bind(this)))}
-                </div>
-              </div>
+            <div className="col-xs-12">
+                <SampleQueue data={this.props}/>;
+                <SampleQueueButtons addMethod={this.props.sampleActions.sendSampleMethod.bind(this,selected.queue_id, selected.sample_id)} />
             </div>
       </div>
     )
@@ -29,17 +27,43 @@ class SampleQueueContainer extends Component {
 
 
 function mapStateToProps(state) {
-  return {
 
-    todo: state.queue.todo,
-    samples_list: state.samples_grid.samples_list
-    
-  }
+  let samples = [];
+  state.queue.todo.map((sample,index) => {
+
+    const sampleData = state.samples_grid.samples_list[sample.sample_id];
+
+    let list_methods = (sampleData.methods ? sampleData.methods.map( (method) =>{ return {module: "Centring"}; } ) : []);
+
+    samples.push({
+      module: 'Vial ' + sampleData.id + " " + sampleData.proteinAcronym,
+      queue_id: sample.queue_id,
+      sample_id: sample.sample_id,
+      list_index: index,
+      children :  (sampleData.methods ? sampleData.methods.map( (method) =>{
+        return {module: method.name};
+      } 
+        ) : [])
+    });
+  });
+
+  let tree = {
+  module: 'Sample Queue - TODO',
+  children: samples,
+  root: true
+  };
+
+
+  return { 
+          tree: tree,
+          selected: state.queue.selected
+    }
 }
 
 function mapDispatchToProps(dispatch) {
  return {
-    actions: bindActionCreators(QueueActions, dispatch)
+    actions: bindActionCreators(QueueActions, dispatch),
+    sampleActions : bindActionCreators(SampleActions, dispatch)
   }
 }
 
