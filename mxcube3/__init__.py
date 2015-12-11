e5a30b7c425dabe9f59f0430eb561125e5c90c05
Mikel Eguiraun <mikel.eguiraun@gmail.com>
2015-12-11 09:37:15 +0100

    removed routes.Sample import (not the file), those methods are now in routes/Queue

--- mxcube3/__init__.py
+++ mxcube3/__init__.py
@@ -52,11 +52,9 @@ if not app.debug or os.environ.get("WERKZEUG_RUN_MAIN") == "true":
   app.beamline = hwr.getHardwareObject(cmdline_options.beamline_setup)
   app.diffractometer = app.beamline.getObjectByRole("diffractometer")
   app.db_connection = app.beamline.getObjectByRole("lims_client")
-  #app.resolution = app.beamline.getObjectByRole("resolution")
   app.queue = app.beamline.getObjectByRole("queue-model")
-#  app.queue = app.beamline.getObjectByRole("queue") #the queue-model already contains the QueueManager
   app.sample_changer = app.beamline.getObjectByRole("sample_changer")
 
   ###Importing all REST-routes
-  import routes.Main, routes.Login, routes.Beamline, routes.Collection, routes.Mockups, routes.Sample, routes.SampleCentring, routes.SampleChanger, routes.Queue
+  import routes.Main, routes.Login, routes.Beamline, routes.Collection, routes.Mockups, routes.SampleCentring, routes.SampleChanger, routes.Queue
 
