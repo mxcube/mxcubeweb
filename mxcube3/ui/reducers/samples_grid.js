1afc132ab4d0d26c40f32bf7ca1dbaaaeedffc6c
Matias Guijarro <guijarro@id02det1.esrf.fr>
2015-12-11 14:23:18 +0100

    re-indentation

--- mxcube3/ui/reducers/samples_grid.js
+++ mxcube3/ui/reducers/samples_grid.js
@@ -1,33 +1,33 @@
-import { addSample } from '../actions/queue'
-
-export default (state={ samples_list: {}, filter_text: "" }, action) => {
+export default (state={ samples_list: {}, filter_text: "", login_data: {} }, action) => {
     switch (action.type) {
     case "UPDATE_SAMPLES":
-        return Object.assign({}, state, { samples_list: action.samples_list });
+          // should have session samples
+          return Object.assign({}, state, { samples_list: action.samples_list });
     case "TOGGLE_SELECTED":
       {
-        // Creating a new SampleItem with the "selected" state toggled
-        let sample_item = {};
-        sample_item[action.index] = Object.assign({}, state.samples_list[action.index], {selected : !state.samples_list[action.index].selected}  );
+          // Creating a new SampleItem with the "selected" state toggled
+          let sample_item = {};
+          sample_item[action.index] = Object.assign({}, state.samples_list[action.index], {selected : !state.samples_list[action.index].selected}  );
 
-        // Creating new Samplelist
-        let samples_list = Object.assign({}, state.samples_list, sample_item);
+          // Creating new Samplelist
+          let samples_list = Object.assign({}, state.samples_list, sample_item);
 
-    return Object.assign({}, state, {samples_list: samples_list});
+          return Object.assign({}, state, {samples_list: samples_list});
       }
     case "SELECT_ALL":
       { 
-        // Creating a new SampleList with the "selected" state toggled to "true"
-        let samples_list = {};
-        Object.keys(state.samples_list).forEach(function (key) {
-            samples_list[key] = Object.assign({}, state.samples_list[key], {selected : true}  );
-        });
+          // Creating a new SampleList with the "selected" state toggled to "true"
+          let samples_list = {};
+          Object.keys(state.samples_list).forEach(function (key) {
+              samples_list[key] = Object.assign({}, state.samples_list[key], {selected : true}  );
+          });
 
-        return Object.assign({}, state,  {samples_list: samples_list}); 
+          return Object.assign({}, state,  {samples_list: samples_list}); 
       }
     case "FILTER":
       {
-        return Object.assign({}, state, { filter_text: action.filter_text });
+          return Object.assign({}, state, { filter_text: action.filter_text });
+      }
     case "SET_SAMPLES_INFO":
       {
           let samples_list = {};
