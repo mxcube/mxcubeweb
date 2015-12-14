b96355cf779977ae0c487a3ea30a811ab007989d
blissadm@id30_massif3 <blissadm@id30_massif3>
2015-12-14 11:56:39 +0100

    convert_to_dict: if object is already a dict, just update from it (useful for tests)

--- mxcube3/routes/Login.py
+++ mxcube3/routes/Login.py
@@ -8,15 +8,18 @@ import types
 
 def convert_to_dict(ispyb_object):
     d = {}
-    for key in ispyb_object.__keylist__:
-      val = getattr(ispyb_object, key)
-      if type(val) == types.InstanceType:
-          val = convert_to_dict(val)
-      elif type(val) == types.ListType:
-          val = [convert_to_dict(x) if type(x)==types.InstanceType else x for x in val]
-      elif type(val) == types.DictType:
-          val = dict([(k, convert_to_dict(x) if type(x)==types.InstanceType else x) for k, x in val.iteritems()])
-      d[key] = val
+    if type(ispyb_object) == types.DictType:
+        d.update(ispyb_object)
+    else:
+        for key in ispyb_object.__keylist__:
+            val = getattr(ispyb_object, key)
+            if type(val) == types.InstanceType:
+                val = convert_to_dict(val)
+            elif type(val) == types.ListType:
+                val = [convert_to_dict(x) if type(x)==types.InstanceType else x for x in val]
+            elif type(val) == types.DictType:
+               val = dict([(k, convert_to_dict(x) if type(x)==types.InstanceType else x) for k, x in val.iteritems()])
+            d[key] = val
     return d
 
 @mxcube.route("/mxcube/api/v0.1/login", methods=["POST"])
