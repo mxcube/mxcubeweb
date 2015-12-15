57049039f9c044244b6a8902e7199406d92b7c14
Matias Guijarro <guijarro@id02det1.esrf.fr>
2015-12-15 20:32:50 +0100

    register for log events directly in main.jsx, attach objects to "window", set timeout to retry connection if it fails

--- mxcube3/ui/main.jsx
+++ mxcube3/ui/main.jsx
@@ -12,6 +12,19 @@ require("file?name=[name].[ext]!index.html");
 
 const store = configureStore({}); //samples_grid: {samples_list: samples_list}});
 
+window.log_records = [];
+window.log_records_connect = function() {
+    clearTimeout(window.log_records_keepalive);
+    window.log_records_keepalive = setTimeout(window.log_records_connect, 5*1000); 
+    window.log_records_source = new EventSource('mxcube/api/v0.1/logging_stream');
+    window.log_records_source.addEventListener('message', function(e) {
+        window.log_records.push(JSON.parse(e.data));
+        clearTimeout(window.log_records_keepalive);
+        window.log_records_keepalive = setTimeout(window.log_records_connect, 30*1000);
+    }, false);
+};
+window.log_records_connect();
+
 ReactDOM.render((
   <Provider store={store}>
 	  <Router>
