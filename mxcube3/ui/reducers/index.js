76682000bf7f72d66c34e297ae4585afc5845f7f
Fredrik Bolmsten <fredrik.bolmsten@maxlab.lu.se>
2015-12-15 11:29:49 +0100

    Added the redux-form to reducers
    I have both added it and it also clears it self when a add goes through

--- mxcube3/ui/reducers/index.js
+++ mxcube3/ui/reducers/index.js
@@ -2,12 +2,23 @@ import { combineReducers } from 'redux'
 import login from './login'
 import queue from './queue'
 import samples_grid from './samples_grid'
+import {reducer as formReducer} from 'redux-form';
 
 
 const rootReducer = combineReducers({
   login,
   queue,
-  samples_grid
+  samples_grid,
+  form: formReducer.plugin({
+    characterisation: (state, action) => { // <------ 'characterisation' is name of form given to reduxForm()
+      switch(action.type) {
+        case "ADD_METHOD":
+          return undefined;       // <--- blow away form data
+        default:
+          return state;
+      }
+    }
+    })
 })
 
 export default rootReducer
