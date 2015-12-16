1f0235347143e4b0c2160e8813ce002683be2f40
Matias Guijarro <guijarro@id02det1.esrf.fr>
2015-12-16 10:52:20 +0100

    first ok implementation of Logging component

--- mxcube3/ui/components/Logging.jsx
+++ mxcube3/ui/components/Logging.jsx
@@ -33,9 +33,9 @@ export class ErrorNotificationPanel extends React.Component {
 export class Logging extends React.Component {
     constructor(props) {
         super(props);
-        this.npages = 10;
-        this.records_per_page = 2;
-        this.state = { log_records: [], curr_page: 1, start_page: 1, last_page: 1, total_pages: 0 };
+        this.npages = 15;
+        this.records_per_page = 20;
+        this.state = { log_records: [], curr_page: 1, start_page: 1, last_page: 1, total_pages: 1 };
     }
 
     componentDidMount() {
@@ -51,6 +51,11 @@ export class Logging extends React.Component {
         window.log_records_source.removeEventListener('message', this._handle_log_record);
     }
 
+    firstPageClicked(e) {
+        e.preventDefault();
+        this.setState({ curr_page: 1, start_page: 1, last_page: Math.min(this.state.total_pages,this.npages) })
+    }
+ 
     previousClicked(e) {
         e.preventDefault();
         var sp = this.state.start_page; 
@@ -74,9 +79,23 @@ export class Logging extends React.Component {
         this.setState({ curr_page: p, start_page: sp, last_page: lp });
     }
 
+    lastPageClicked(e) {
+        e.preventDefault();
+        this.setState({ curr_page: this.state.total_pages, 
+                        start_page: Math.max(this.state.total_pages-this.npages+1,1), 
+                        last_page: this.state.total_pages });
+    }
+
     pageClicked(i,e) {
         e.preventDefault();
        	this.setState({curr_page: i });
+    }
+
+    toggleStackTrace(i,e) {
+        e.preventDefault();
+        let log_record = this.state.log_records[i];
+        log_record.show_stack = !log_record.show_stack; 
+        this.setState({log_records: this.state.log_records});
     }
 
     render() {
@@ -87,7 +106,8 @@ export class Logging extends React.Component {
         
         let total_pages = Math.floor(this.state.log_records.length / this.records_per_page);
         if (this.state.log_records.length % this.records_per_page) { total_pages++ };
-        this.state.total_pages = total_pages;
+        this.state.total_pages = Math.max(total_pages, 1);
+
         while (((this.state.last_page-this.state.start_page)<(this.npages-1))&&(this.state.last_page < this.state.total_pages)) {
             this.state.last_page++;
         }                                      
@@ -96,15 +116,32 @@ export class Logging extends React.Component {
             pages.push(<li className={active} key={i}><a href="#" onClick={this.pageClicked.bind(this, i)}>{i}</a></li>);
         }
  
-        i = 0;
-        for (log_record of this.state.log_records.slice((this.state.curr_page-1)*this.records_per_page, this.state.curr_page*this.records_per_page)) {
-            log_output.push(<pre key={i} style={{margin: "0px", border: "0px"}}>{log_record.message}</pre>)
+        i = (this.state.curr_page-1)*this.records_per_page;
+        for (log_record of this.state.log_records.slice(i, i+this.records_per_page)) {
+            let log_msg;
+            if (log_record.stack_trace) {
+                if (log_record.show_stack) {
+                    log_msg = (<td><a href="#" onClick={this.toggleStackTrace.bind(this, i)}>{log_record.message}</a><br></br><pre className="bg-warning">{log_record.stack_trace}</pre></td>)
+                } else {
+                    log_msg = (<td><a href="#" onClick={this.toggleStackTrace.bind(this, i)}>{log_record.message}</a></td>)
+                }
+            } else {
+                log_msg = (<td>{log_record.message}</td>)
+            }
+            log_output.push(<tr key={i}><td>{log_record.timestamp}</td><td>{log_record.logger}</td><td>{log_record.severity}</td>{log_msg}</tr>)
             ++i;
         }
         
         return (<div className='col-xs-12'>
+                    <row>
+                    <div className="col-xs-12 text-center" style={{float: "none", margin: "0 auto"}}>
                     <nav>
                         <ul className="pagination">
+                            <li>
+                                <a href="#" onClick={this.firstPageClicked.bind(this)}>
+                                    <span aria-hidden="true">first</span>
+                                </a>
+                            </li>
                             <li>
                                 <a href="#" onClick={this.previousClicked.bind(this)}>
                                     <span aria-hidden="true">&laquo;</span>
@@ -116,9 +153,32 @@ export class Logging extends React.Component {
                                     <span aria-hidden="true">&raquo;</span>
                                 </a>
                             </li>
+                            <li>
+		                <a href="#" onClick={this.lastPageClicked.bind(this)}>
+                                    <span aria-hidden="true">{"last"+(this.state.last_page<this.state.total_pages ? " ("+this.state.total_pages+")" : "")}</span>
+                                </a>
+                            </li>
                         </ul>
                     </nav>
-                    {log_output}
+                    </div>
+                    </row>
+                    <row>
+                    <div className="col-xs-12">
+                    <table className="table table-condensed table-striped">
+                        <thead>
+                            <tr>
+                                <th className="col-sm-2">Time</th>
+                                <th className="col-sm-1">Logger</th>
+                                <th className="col-sm-1">Severity</th>
+                                <th>Message</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {log_output}
+                        </tbody>
+                    </table>
+                    </div>
+                    </row>
                </div>)
     }
 }
