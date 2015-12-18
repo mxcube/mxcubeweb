4aeab638362c6030f16c64da192d905b5577dd1d
Matias Guijarro <guijarro@id02det1.esrf.fr>
2015-12-18 15:32:03 +0100

    data received as argument in websocket callback is directly a json object

--- mxcube3/ui/main.jsx
+++ mxcube3/ui/main.jsx
@@ -18,8 +18,7 @@ window.log_records_connect = function() {
     //clearTimeout(window.log_records_keepalive);
     //window.log_records_keepalive = setTimeout(window.log_records_connect, 5*1000); 
     window.log_records_ws = io.connect('http://' + document.domain + ':' + location.port+"/logging");
-    window.log_records_ws.on('log_record', (e) => {
-        let record = JSON.parse(e.data);
+    window.log_records_ws.on('log_record', (record) => {
         window.log_records.push(record);
         //clearTimeout(window.log_records_keepalive);
         //window.log_records_keepalive = setTimeout(window.log_records_connect, 30*1000);
