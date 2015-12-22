ee81ab6190974c3f087927b2456dd0a4c3077653
Matias Guijarro <guijarro@id02det1.esrf.fr>
2015-12-22 15:41:32 +0100

    added CLEAR_QUEUE action for Queue; clear queue when changing from manually mounted samples and sample changer

--- mxcube3/ui/actions/samples_grid.js
+++ mxcube3/ui/actions/samples_grid.js
@@ -1,4 +1,5 @@
 import fetch from 'isomorphic-fetch'
+import { clearAll } from './queue'
 
 export function doUpdateSamples(samples_list) {
     return { type: "UPDATE_SAMPLES", samples_list }
@@ -73,9 +74,11 @@ export function doToggleManualMount() {
         const { samples_grid } = getState();
         if (samples_grid.manual_mount) {
             dispatch(doSetManualMount(false));
+            dispatch(clearAll());
             dispatch(doGetSamplesList());
         } else {
             dispatch(doSetManualMount(true));
+            dispatch(clearAll());
             dispatch(doUpdateSamples([{id:"0", sample_info: { sampleName: "mounted sample"}}])); 
         }
     }
