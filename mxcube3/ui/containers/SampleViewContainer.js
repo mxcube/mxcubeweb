39a5d910d125b2b01401f2907d536f99d0f6b6a2
Fredrik Bolmsten <fredrik.bolmsten@maxlab.lu.se>
2016-01-22 11:09:41 +0100

    Implemented fabric

--- mxcube3/ui/containers/SampleViewContainer.js
+++ mxcube3/ui/containers/SampleViewContainer.js
@@ -3,14 +3,14 @@ import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
 import SampleImage from '../components/SampleView/SampleImage';
 import * as QueueActions from '../actions/queue'
+import * as SampleActions from '../actions/samples_grid'
+import { showForm } from '../actions/methodForm'
+
 
 class SampleViewContainer extends Component {
   render() {
     return (
       <div className="row">
-
-   
-    {/* The main experimental control panel */}
     <div className="col-xs-12">
             <div className="col-md-12">
               <div className="panel panel-primary text-center">
@@ -30,15 +30,18 @@ class SampleViewContainer extends Component {
 
 
 function mapStateToProps(state) {
-  return {
-    todos: "Map to state"
-  }
+  return { 
+          current : state.queue.current,
+          sampleInformation: state.samples_grid.samples_list,
+          lookup: state.queue.lookup,
+    }
 }
 
 function mapDispatchToProps(dispatch) {
- return {
-    //actions: (sample) => dispatch(addSample(sample))
-    actions: bindActionCreators(QueueActions, dispatch)
+ return  {
+    queueActions: bindActionCreators(QueueActions, dispatch),
+    sampleActions : bindActionCreators(SampleActions, dispatch),
+    showForm : bindActionCreators(showForm, dispatch)
   }
 }
 
