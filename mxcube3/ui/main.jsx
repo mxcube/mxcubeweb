a864e42bea98f0e5b58b3245bdbaf859d944bfc5
Fredrik Bolmsten <fredrik.bolmsten@maxlab.lu.se>
2016-01-25 11:22:28 +0100

    Set default route to samplechanger view

--- mxcube3/ui/main.jsx
+++ mxcube3/ui/main.jsx
@@ -1,7 +1,7 @@
 import 'babel-polyfill';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router, Route, Link } from 'react-router';
+import { Router, Route, Link, IndexRoute } from 'react-router';
 import SampleViewContainer from './containers/SampleViewContainer';
 import SampleGridContainer from './containers/SampleGridContainer';
 import LoginContainer from './containers/LoginContainer';
@@ -24,7 +24,7 @@ ReactDOM.render((
   <Provider store={store}>
 	  <Router>
 		  <Route path="/" component={Main} onEnter={requireAuth}>
-			  <Route path="samplegrid" component={SampleGridContainer} onEnter={requireAuth}/>
+		  	  <IndexRoute component={SampleGridContainer} onEnter={requireAuth}/>
 			  <Route path="datacollection" component={SampleViewContainer} onEnter={requireAuth}/>
         	  <Route path="logging" component={Logging} onEnter={requireAuth}/>
 		  </Route>
