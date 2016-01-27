22572f31d989114d71e40f77108ae8ed615b6c18
Fredrik Bolmsten <fredrik.bolmsten@maxlab.lu.se>
2016-01-27 09:39:55 +0100

    Added a simple way of retrieving a lost session
    This will save both the queue state and the samplegrid state to the server

--- mxcube3/ui/containers/MXNavbarContainer.js
+++ mxcube3/ui/containers/MXNavbarContainer.js
@@ -4,13 +4,13 @@ import { connect } from 'react-redux';
 import { doLogin, getLoginInfo } from '../actions/login';
 import MXNavbar from '../components/MXNavbar/MXNavbar'
 import { doSignOut } from '../actions/login'
+import { sendState, getState } from '../actions/queue'
 
 
 
 class MXNavbarContainer extends Component {
 
   render() {
- 
     return (
     	<MXNavbar userInfo={this.props.userInfo} signOut={this.props.signOut} loggedIn={this.props.loggedIn} />
     )
@@ -21,13 +21,15 @@ class MXNavbarContainer extends Component {
 function mapStateToProps(state) {
         return { 
             userInfo: state.login.data,
-            loggedIn: state.login.loggedIn
+            loggedIn: state.login.loggedIn,
+            queueState: state.queue,
+            sampleGridState: state.samples_grid
         }
 }
 
 function mapDispatchToProps(dispatch) {
     return {
-        signOut: () => dispatch(doSignOut())
+        signOut: () => dispatch(doSignOut()),
     }
 }
 
