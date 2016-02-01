a10ae31bd19d2f2cc4628e699a6aaba0a9eaf92c
Mikel Eguiraun <mikel.eguiraun@gmail.com>
2016-02-01 19:08:47 +0100

    PEPipying (again)
    but leaving out...
    
    - 2 blank lines between methods
    - line too long
    
    ... to facilitate the merging

--- mxcube3/__init__.py
+++ mxcube3/__init__.py
@@ -1,8 +1,9 @@
 from __future__ import absolute_import
-from flask import Flask, session, redirect, url_for, render_template, request, Response
+from flask import Flask
 from flask.ext.socketio import SocketIO
 from optparse import OptionParser
-import os, sys
+import os
+import sys
 import logging
 import gevent
 # some Hardware Objects rely on BlissFramework.Utils.widget_colors,
@@ -10,12 +11,14 @@ import gevent
 # time being:
 from types import ModuleType
 import collections
-bf=ModuleType("BlissFramework")
-bfu=ModuleType("BlissFramework.Utils")
-wc=collections.namedtuple("widget_colors", ["LIGHT_GREEN", "LIGHT_RED", "LIGHT_YELLOW", "LIGHT_BLUE", "SKY_BLUE", "DARK_GRAY","WHITE", "GRAY", "GREEN", "RED"])
+bf = ModuleType("BlissFramework")
+bfu = ModuleType("BlissFramework.Utils")
+wc = collections.namedtuple("widget_colors", ["LIGHT_GREEN", "LIGHT_RED",\
+      "LIGHT_YELLOW", "LIGHT_BLUE", "SKY_BLUE", "DARK_GRAY", "WHITE", \
+      "GRAY", "GREEN", "RED"])
 setattr(bfu, "widget_colors", wc(*[None]*10))
-sys.modules["BlissFramework"]=bf
-sys.modules["BlissFramework.Utils"]=bfu
+sys.modules["BlissFramework"] = bf
+sys.modules["BlissFramework.Utils"] = bfu
 ###
 
 opt_parser = OptionParser()
@@ -38,53 +41,54 @@ opt_parser.add_option("-q", "--queue-model",
 cmdline_options, args = opt_parser.parse_args()
 
 socketio = SocketIO()
-app = Flask(__name__, static_url_path='')	
+app = Flask(__name__, static_url_path='')
 app.debug = True
-
-socketio.init_app(app) # this line important for socketio msg, otherwise no msg is sent...
+# this line important for socketio msg, otherwise no msg is sent...
+socketio.init_app(app)
 
 # the following test prevents Flask from initializing twice
 # (because of the Reloader)
 if not app.debug or os.environ.get("WERKZEUG_RUN_MAIN") == "true":
   ###Initialization of the HardwareObjects
-  # this is to allow Hardware Objects to do 'from HardwareRepository import ...'
-  sys.path.insert(0, os.path.dirname(__file__)) 
-  from HardwareRepository import HardwareRepository as hwr, setLogFile
-  hwr.addHardwareObjectsDirs([os.path.join(os.path.dirname(__file__), 'HardwareObjects')])
+  # this is to allow Hardware Objects to do
+  # 'from HardwareRepository import ...'
+    sys.path.insert(0, os.path.dirname(__file__))
+    from HardwareRepository import HardwareRepository as hwr, setLogFile
+    hwr.addHardwareObjectsDirs([os.path.join(os.path.dirname(__file__), 'HardwareObjects')])
 
-  hwr_directory = cmdline_options.hwr_directory
-  hwr = hwr.HardwareRepository(os.path.abspath(os.path.expanduser(hwr_directory)))
-  hwr.connect()
-  log_file = cmdline_options.log_file
-  if log_file:
-     setLogFile(log_file)
+    hwr_directory = cmdline_options.hwr_directory
+    hwr = hwr.HardwareRepository(os.path.abspath(os.path.expanduser(hwr_directory)))
+    hwr.connect()
+    log_file = cmdline_options.log_file
+    if log_file:
+        setLogFile(log_file)
 
-  # installs logging handler to send messages to clients
-  import logging_handler
-  root_logger = logging.getLogger()
-  root_logger.setLevel(logging.DEBUG)
-  custom_log_handler = logging_handler.MX3LoggingHandler()
-  custom_log_handler.setLevel(logging.INFO)
-  root_logger.addHandler(custom_log_handler)
-  app.log_handler = custom_log_handler
+    # installs logging handler to send messages to clients
+    import logging_handler
+    root_logger = logging.getLogger()
+    root_logger.setLevel(logging.DEBUG)
+    custom_log_handler = logging_handler.MX3LoggingHandler()
+    custom_log_handler.setLevel(logging.INFO)
+    root_logger.addHandler(custom_log_handler)
+    app.log_handler = custom_log_handler
 
   ###Importing all REST-routes
-  import routes.Main, routes.Login, routes.Beamline, routes.Collection, routes.Mockups, routes.SampleCentring, routes.SampleChanger, routes.Queue
+    import routes.Main, routes.Login, routes.Beamline, routes.Collection, routes.Mockups, routes.SampleCentring, routes.SampleChanger, routes.Queue
 
-  def complete_initialization(app):
-      app.beamline = hwr.getHardwareObject(cmdline_options.beamline_setup)
-      app.session = app.beamline.getObjectByRole("session")
-      app.collect = app.beamline.getObjectByRole("collect")
-      app.diffractometer = app.beamline.getObjectByRole("diffractometer")
-      routes.SampleCentring.init_signals()
-      app.db_connection = app.beamline.getObjectByRole("lims_client")
-      app.queue = hwr.getHardwareObject(cmdline_options.queue_model)
-      routes.Queue.init_signals()
-      app.sample_changer = app.beamline.getObjectByRole("sample_changer")
+    def complete_initialization(app):
+        app.beamline = hwr.getHardwareObject(cmdline_options.beamline_setup)
+        app.session = app.beamline.getObjectByRole("session")
+        app.collect = app.beamline.getObjectByRole("collect")
+        app.diffractometer = app.beamline.getObjectByRole("diffractometer")
+        routes.SampleCentring.init_signals()
+        app.db_connection = app.beamline.getObjectByRole("lims_client")
+        app.queue = hwr.getHardwareObject(cmdline_options.queue_model)
+        routes.Queue.init_signals()
+        app.sample_changer = app.beamline.getObjectByRole("sample_changer")
 
-  # starting from here, requests can be received by server;
-  # however, objects are not all initialized, so requests can return errors
-  # TODO: synchronize web UI with server operation status
-  gevent.spawn(complete_initialization, app)
+    # starting from here, requests can be received by server;
+    # however, objects are not all initialized, so requests can return errors
+    # TODO: synchronize web UI with server operation status
+    gevent.spawn(complete_initialization, app)
 
 
--- mxcube3/routes/SampleChanger.py
+++ mxcube3/routes/SampleChanger.py
@@ -1,7 +1,6 @@
-from flask import session, redirect, url_for, render_template, request, Response, jsonify
+from flask import request, Response, jsonify
 from mxcube3 import app as mxcube
 import logging
-import itertools
 from .Queue import queueList, lastQueueNode
 
 @mxcube.route("/mxcube/api/v0.1/sample_changer/samples_list", methods=['GET'])
@@ -10,33 +9,41 @@ def get_samples_list():
     samples = {}
     for s in samples_list:
         sample_dm = s.getID() or ""
-        samples.update({s.getAddress(): { "id": s.getAddress(), "location": ":".join(map(str, s.getCoords())), "code": sample_dm, "methods": {} } })
-
+        samples.update(
+            {s.getAddress():
+                {
+                    "id": s.getAddress(),
+                    "location": ":".join(map(str, s.getCoords())),
+                    "code": sample_dm,
+                    "methods": {}
+                }
+             }
+            )
     return jsonify(samples)
 
 @mxcube.route("/mxcube/api/v0.1/sample_changer/<sample>/mount", methods=['PUT'])
 def mountSample(sample):
-	try:
-		sampleNode = mxcube.queue.get_node(int(sample))
-		sampleLocation = sampleNode.location
-		lastQueueNode.update({'id' : int(sample), 'sample':str(sampleLocation[0]+':'+sampleLocation[1])})
-		#mxcube.sample_changer.load_sample
-		#TODO: figure out how to identify the sample for the sc, selectsample&loadsamplae&etc
-		logging.getLogger('HWR').info('[SC] %s sample mounted, location: %s' %(sample, sampleLocation))
-		return Response(status = 200)
-	except Exception:
-		logging.getLogger('HWR').exception('[SC] sample could not be mounted')
-		return Response(status = 409)
+    try:
+        sampleNode = mxcube.queue.get_node(int(sample))
+        sampleLocation = sampleNode.location
+        lastQueueNode.update({'id': int(sample), 'sample': str(sampleLocation[0] + ':' + sampleLocation[1])})
+        #mxcube.sample_changer.load_sample
+        #TODO: figure out how to identify the sample for the sc, selectsample&loadsamplae&etc
+        logging.getLogger('HWR').info('[SC] %s sample mounted, location: %s' % (sample, sampleLocation))
+        return Response(status=200)
+    except Exception:
+        logging.getLogger('HWR').exception('[SC] sample could not be mounted')
+        return Response(status=409)
 
 @mxcube.route("/mxcube/api/v0.1/sample_changer/<sample>/unmount", methods=['PUT'])
 def unmountSample(sample):
-	try:
-		sampleNode = mxcube.queue.get_node(int(sample))
-		sampleLocation = sampleNode.location
-		#mxcube.sample_changer.load_sample
-		#TODO: figure out how to identify the sample for the sc, selectsample&loadsamplae&etc
-		logging.getLogger('HWR').info('[SC] %s sample mounted, location: %s' %(sample, sampleLocation))
-		return Response(status = 200)
-	except Exception:
-		logging.getLogger('HWR').exception('[SC] sample could not be mounted')
-		return Response(status = 409)
+    try:
+        sampleNode = mxcube.queue.get_node(int(sample))
+        sampleLocation = sampleNode.location
+        #mxcube.sample_changer.load_sample
+        #TODO: figure out how to identify the sample for the sc, selectsample&loadsamplae&etc
+        logging.getLogger('HWR').info('[SC] %s sample mounted, location: %s' % (sample, sampleLocation))
+        return Response(status=200)
+    except Exception:
+        logging.getLogger('HWR').exception('[SC] sample could not be mounted')
+        return Response(status=409)
