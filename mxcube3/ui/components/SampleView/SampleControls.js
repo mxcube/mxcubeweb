6b5de2534e94a9c46e04a9f8c7fdf7ed6f397f93
Fredrik Bolmsten <fredrik.bolmsten@maxlab.lu.se>
2016-03-22 10:40:45 +0100

    Tested new way of setting light strength

--- mxcube3/ui/components/SampleView/SampleControls.js
+++ mxcube3/ui/components/SampleView/SampleControls.js
@@ -10,10 +10,8 @@ export default class SampleControls extends React.Component {
         this.setZoom = this.setZoom.bind(this);
         this.setLigthStrengthFront = this.setLigthStrength.bind(this,"FrontLight");
         this.setLigthStrengthBack = this.setLigthStrength.bind(this,"BackLight");
-        this.backLightOn = this.setLightOn.bind(this,"back");
-        this.frontLightOn = this.setLightOn.bind(this,"front");
-        this.backLightOff = this.setLightOff.bind(this,"back");
-        this.frontLightOff = this.setLightOff.bind(this,"front");
+        this.toogleBackLight = this.toogleLight.bind(this,"back");
+        this.toogleFrontLight = this.toogleLight.bind(this,"front");
     }
 
 
@@ -31,12 +29,15 @@ export default class SampleControls extends React.Component {
         this.props.sampleActions.sendMotorPosition(name, option.target.value);
     }
 
-    setLightOff(name){
-        this.props.sampleActions.sendLightOff(name);
-    }
 
-    setLightOn(name){
-        this.props.sampleActions.sendLightOn(name);
+    toogleLight(name){
+
+        let lighStatus = this.props.sampleViewState.lightOn[name];
+        if(lighStatus){
+            this.props.sampleActions.sendLightOff(name);
+        }else{
+            this.props.sampleActions.sendLightOn(name);
+        }
     }
 
 
@@ -50,19 +51,24 @@ render() {
                 <a href="#" id="downloadLink" type="button" data-toggle="tooltip"  title="Take snapshot" className="btn btn-link  pull-center" onClick={this.takeSnapShot} download><i className="fa fa-2x fa-fw fa-camera"></i></a>                            
                 <button type="button" data-toggle="tooltip"  title="Start auto centring" className="btn btn-link  pull-center" onClick={this.props.sampleActions.sendStartAutoCentring}><i className="fa fa-2x fa-fw fa-arrows"></i></button>
                 <button type="button" data-toggle="tooltip"  title="Start 3-click centring" className="btn btn-link  pull-center" onClick={this.props.sampleActions.sendStartClickCentring}><i className="fa fa-2x fa-fw fa-circle-o-notch"></i></button>
+                <button type="button" data-toggle="tooltip"  title="Zoom out" className="btn btn-link  pull-center"><i className="fa fa-2x fa-fw fa fa-search-minus"></i></button>
+                <input className="bar" type="range" id="zoom-control" min="0" max="9" step="1" defaultValue={this.props.sampleViewState.zoom} onChange={this.setZoom}/>
+                <button type="button" data-toggle="tooltip"  title="Zoom in" className="btn btn-link  pull-center"><i className="fa fa-2x fa-fw fa fa-search-plus"></i></button>
+                    
                 <button type="button" data-toggle="tooltip"  title="Abort Centring" className="btn btn-link  pull-center" onClick={this.props.sampleActions.sendAbortCentring}><i className="fa fa-2x fa-fw fa-times"></i></button>
-                <button id="zoom-button" type="button" data-toggle="tooltip"  title="Zoom out" className="btn btn-link  pull-center">
-                    <div id="zoom-control">
-                    <input className="bar" type="range" min="0" max="9" step="1" defaultValue={this.props.sampleViewState.zoom} onChange={this.setZoom} />
+
+                <button type="button" data-toggle="tooltip"  title="Backlight On/Off" className="btn btn-link pull-center light-button">
+                    <i id="back-light-icon" className={"fa fa-2x fa-fw fa fa-lightbulb-o" + (this.props.sampleViewState.lightOn.back ? " light-on" : "")} onClick={this.toogleBackLight}></i>
+                    <div id="back-light">
+                        <input className="bar light-control" type="range" min="0.0" max="2.0" step="0.1" defaultValue={this.props.sampleViewState.motors.BackLight.position} onChange={this.setLigthStrengthBack} />
                     </div>
-                    <i id="zoom-icon" className="fa fa-2x fa-fw fa fa-search"></i>
                 </button>
-                <button type="button" data-toggle="tooltip"  title="Light On/Off" className="btn btn-link  pull-center" onClick={this.backLightOff}><i className="fa fa-2x fa-fw fa fa-lightbulb-o"></i> </button>
-                <input className="bar" type="range" id="light-control" min="0.0" max="2.0" step="0.1" defaultValue={this.props.sampleViewState.motors.BackLight.position} onChange={this.setLigthStrengthBack} />
-                <button type="button" data-toggle="tooltip"  title="Light On/Off" className="btn btn-link  pull-center" onClick={this.backLightOn}><i className="fa fa-2x fa-fw fa fa-lightbulb-o light-on"></i> </button>
-                <button type="button" data-toggle="tooltip"  title="Light On/Off" className="btn btn-link  pull-center" onClick={this.frontLightOff}><i className="fa fa-2x fa-fw fa fa-lightbulb-o"></i> </button>
-                <input className="bar" type="range" id="light-control" min="0.0" max="2.0" step="0.1" defaultValue={this.props.sampleViewState.motors.FrontLight.position} onChange={this.setLigthStrengthFront} />
-                <button type="button" data-toggle="tooltip"  title="Light On/Off" className="btn btn-link  pull-center" onClick={this.frontLightOn}><i className="fa fa-2x fa-fw fa fa-lightbulb-o light-on"></i> </button>
+                <button type="button" data-toggle="tooltip"  title="Frontlight On/Off" className="btn btn-link pull-center light-button">
+                    <i id="front-light-icon" className={"fa fa-2x fa-fw fa fa-lightbulb-o" + (this.props.sampleViewState.lightOn.front ? " light-on" : "")} onClick={this.toogleFrontLight}></i>
+                    <div id="front-light">
+                        <input className="bar light-control" type="range" min="0.0" max="2.0" step="0.1" defaultValue={this.props.sampleViewState.motors.FrontLight.position} onChange={this.setLigthStrengthFront} />
+                    </div>                   
+                </button>                                
 
             </div>
         </div>
