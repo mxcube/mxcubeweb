dfe84dd72f111cd55ce2be8cfa4017e947df8916
Fredrik Bolmsten <fredrik.bolmsten@maxlab.lu.se>
2016-03-22 13:23:34 +0100

    Revert back to old padding

--- mxcube3/ui/containers/SampleGridContainer.js
+++ mxcube3/ui/containers/SampleGridContainer.js
@@ -38,7 +38,7 @@ class SampleGridContainer extends React.Component {
           );
       
           return (<div className="row">
-            <div className="navbar-default col-xs-12" style={{position: 'fixed', zIndex:1}}>
+            <div className="navbar-default col-xs-12" style={{position: 'fixed', zIndex:1, padding: 10}}>
               <div className="row">
                 <div className="col-xs-3">
                     <form className="form-horizontal">
