1f8e0a21244dc65490ac3ce331017cbae7b1df2f
Fredrik Bolmsten <fredrik.bolmsten@maxlab.lu.se>
2016-03-31 15:32:31 +0200

    Changed so that zoom is only called onMouseUp
    This spares the hardware a bit

--- mxcube3/ui/components/SampleView/SampleControls.js
+++ mxcube3/ui/components/SampleView/SampleControls.js
@@ -21,7 +21,7 @@ export default class SampleControls extends React.Component {
     }
 
     takeSnapShot(){
-        document.getElementById("downloadLink").href = this.state.canvas.toDataURL();
+        document.getElementById("downloadLink").href = this.props.canvas.toDataURL();
     }
 
 
@@ -54,7 +54,19 @@ render() {
                     <i className={"fa fa-2x fa-fw fa-circle-o-notch" + (this.props.sampleViewState.clickCentring ? " button-active" : "")}></i>
                 </button>
                 <button type="button" data-toggle="tooltip"  title="Zoom out" className="btn btn-link  pull-center"><i className="fa fa-2x fa-fw fa fa-search-minus"></i></button>
-                <input className="bar" type="range" id="zoom-control" min="0" max="9" step="1" defaultValue={this.props.sampleViewState.zoom} onChange={this.setZoom}/>
+                <input className="bar" type="range" id="zoom-control" min="0" max="9" step="1" defaultValue={this.props.sampleViewState.zoom} onMouseUp={this.setZoom} list="volsettings"/>
+                <datalist id="volsettings">
+                    <option>0</option>
+                    <option>1</option>
+                    <option>2</option>
+                    <option>3</option>
+                    <option>4</option>
+                    <option>5</option>
+                    <option>6</option>
+                    <option>7</option>
+                    <option>8</option>
+                    <option>9</option>
+                </datalist>
                 <button type="button" data-toggle="tooltip"  title="Zoom in" className="btn btn-link  pull-center"><i className="fa fa-2x fa-fw fa fa-search-plus"></i></button>
                     
                 <button type="button" data-toggle="tooltip"  title="Abort Centring" className="btn btn-link  pull-center" onClick={this.props.sampleActions.sendAbortCentring}><i className="fa fa-2x fa-fw fa-times"></i></button>
