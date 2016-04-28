066246eef2d46a1c1eac20ee5d40d593596b2722
Fredrik Bolmsten <fredrik.bolmsten@maxlab.lu.se>
2016-04-28 11:44:16 +0200

    Motorinput component

--- mxcube3/ui/components/SampleView/MotorControl.js
+++ mxcube3/ui/components/SampleView/MotorControl.js
@@ -1,131 +1,83 @@
 
 'use strict';
-
 import React from 'react';
-import "bootstrap-webpack!bootstrap-webpack/bootstrap.config.js";
-import { OverlayTrigger, Popover } from 'react-bootstrap';
+import MotorInput from './MotorInput';
 
 class MotorControl extends React.Component {
 
-
-  constructor(props) {
-        super(props);
-        this.handleType = this.handleType.bind(this);
-        this.handleClick = this.handleClick.bind(this);
-
-  }
-
-    handleType(e){
-        e.preventDefault(); // Let's stop this event.
-        e.stopPropagation(); // Really this time.
-        if([13,38,40].includes(e.keyCode)){
-            this.props.sampleActions.sendMotorPosition(e.target.name, e.target.valueAsNumber);
-        }
-    }
-
-    handleClick(e){
-        this.props.sampleActions.sendMotorPosition(e.target.name, e.target.valueAsNumber);
-    }
-
     render() { 
-        const motors = this.props.motors;
-        const Phi = motors.Phi.position.toFixed(2);
-        const PhiY = motors.PhiY.position.toFixed(2);
-        const PhiZ = motors.PhiZ.position.toFixed(2);
-        const Focus = motors.Focus.position.toFixed(2);
+        const {Phi, PhiY, PhiZ, Focus} = this.props.motors;
+        const save = this.props.save;
 
         return (
        
         <div className="sample-controlls sample-controlls-top">
 
-                <div className="row">
+            <div className="row">
 
                 <div className="col-sm-2">
                     <label className="motor-name">Omega: </label>
-                    <OverlayTrigger trigger="click" placement="bottom" rootClose overlay={
-                        <Popover id="Omega" title="Omega">
-                            <div className="form-inline">
-                                <div className="form-group">
-                                    <form onSubmit={this.handleType} noValidate>
-                                        <input className="form-control input-sm" title="asdas" onKeyUp={this.handleType} onClick={this.handleClick} type="number" step="90" defaultValue={Phi} name="Phi"/>
-                                    </form>
-                                </div>
-                            </div>
-                        </Popover>
-                        }
-                    >
-                        <span className="motor-value">{Phi}&deg;</span>
-                    </OverlayTrigger>
+                    <MotorInput 
+                        title="Omega" 
+                        save={save} 
+                        value={Phi.position} 
+                        motorName="Phi" 
+                        step="90" 
+                        suffix="&deg;"
+                        decimalPoints="2"
+                    />
                 </div>
-
-
 
                 <div className="col-sm-2">
                     <label className="motor-name">Kappa: </label>
-                    <span className="motor-value">NA</span>
                 </div>
 
                 <div className="col-sm-2">
                     <label className="motor-name">Phi: </label>
-                    <span className="motor-value">NA</span>
+
                 </div>
 
                 <div className="col-sm-2">
                     <label className="motor-name">Y: </label>
-                    <OverlayTrigger trigger="click" placement="bottom" rootClose overlay={
-                        <Popover id="Y" title="Y">
-                            <div className="form-inline">
-                                <div className="form-group">
-                                    <form onSubmit={this.handleType} noValidate>
-                                        <input className="form-control input-sm" onKeyUp={this.handleType} onClick={this.handleClick} type="number" step="0.1" defaultValue={PhiY} name="PhiY"/>
-                                    </form>
-                                </div>
-                            </div>
-                        </Popover>
-                        }
-                    >
-                        <span className="motor-value">{PhiY} mm</span>
-                    </OverlayTrigger>
+                    <MotorInput 
+                        title="Y" 
+                        save={save} 
+                        value={PhiY.position} 
+                        motorName="PhiY" 
+                        step="0.1" 
+                        suffix="mm"
+                        decimalPoints="2"
+                    />                    
                 </div>
-
 
                 <div className="col-sm-2">
                     <label className="motor-name">Z: </label>
-                    <OverlayTrigger trigger="click" placement="bottom" rootClose overlay={
-                        <Popover id="Z" title="Z">
-                            <div className="form-inline">
-                                <div className="form-group">
-                                    <form onSubmit={this.handleType} noValidate>
-                                        <input className="form-control input-sm" onKeyUp={this.handleType} onClick={this.handleClick} type="number" step="0.1" defaultValue={PhiZ} name="PhiZ"/>
-                                    </form>
-                                </div>
-                            </div>
-                        </Popover>
-                        }
-                    >
-                        <span className="motor-value">{PhiZ} mm</span>
-                    </OverlayTrigger>
+                    <MotorInput 
+                        title="Z" 
+                        save={save} 
+                        value={PhiZ.position} 
+                        motorName="PhiZ" 
+                        step="0.1" 
+                        suffix="mm"
+                        decimalPoints="2"
+                    />   
                 </div>
 
                <div className="col-sm-2">
                     <label className="motor-name">Focus: </label>
-                    <OverlayTrigger trigger="click" placement="bottom" rootClose overlay={
-                        <Popover id="Focus" title="Focus">
-                            <div className="form-inline">
-                                <div className="form-group">
-                                    <form onSubmit={this.handleType} noValidate>
-                                        <input className="form-control input-sm" onKeyUp={this.handleType} onClick={this.handleClick} type="number" step="0.1" defaultValue={Focus} name="Focus"/>
-                                    </form>
-                                </div>
-                            </div>
-                        </Popover>
-                        }
-                    >
-                        <span className="motor-value">{Focus} mm</span>
-                    </OverlayTrigger>
+                    <MotorInput 
+                        title="Focus" 
+                        save={save} 
+                        value={Focus.position} 
+                        motorName="Focus" 
+                        step="0.1" 
+                        suffix="mm"
+                        decimalPoints="2"
+                    />                    
                 </div>
 
-                </div>
+            </div>
+
         </div>
 
         );
--- mxcube3/ui/components/SampleView/SampleControls.js
+++ mxcube3/ui/components/SampleView/SampleControls.js
@@ -2,6 +2,8 @@
 import './SampleView.css';
 import React from 'react'
 import { OverlayTrigger, Popover } from 'react-bootstrap';
+import MotorInput from './MotorInput';
+
 
 export default class SampleControls extends React.Component {
 
@@ -9,14 +11,10 @@ export default class SampleControls extends React.Component {
         super(props);
         this.takeSnapShot = this.takeSnapShot.bind(this);
         this.setZoom = this.setZoom.bind(this);
-        this.setLigthStrength = this.setLigthStrength.bind(this);
         this.setApertureSize = this.setApertureSize.bind(this);
-        this.setLigthStrengthStep = this.setLigthStrengthStep.bind(this);
         this.toogleFrontLight = this.toogleLight.bind(this,'front');
         this.toogleBackLight = this.toogleLight.bind(this,'back');
     }
-
-
 
     setZoom(option){
         this.props.sampleActions.sendZoomPos(option.target.value);
@@ -30,19 +28,7 @@ export default class SampleControls extends React.Component {
         this.props.sampleActions.sendChangeAperture(option.target.value);
     }    
 
-    setLigthStrength(option){
-        option.preventDefault(); 
-        option.stopPropagation();
-        if([13,38,40].includes(option.keyCode)){
-            this.props.sampleActions.sendMotorPosition(option.target.name, option.target.value);
-        }
-    }
-    setLigthStrengthStep(option){
-            this.props.sampleActions.sendMotorPosition(option.target.name, option.target.value);
-    }
-
     toogleLight(name){
-
         let lighStatus = this.props.sampleViewState.lightOn[name];
         if(lighStatus){
             this.props.sampleActions.sendLightOff(name);
@@ -50,8 +36,6 @@ export default class SampleControls extends React.Component {
             this.props.sampleActions.sendLightOn(name);
         }
     }
-
-
 
 
 render() {
@@ -62,7 +46,7 @@ render() {
                         <Popover id="Aperture" title="Aperture">
                             <div className="form-inline">
                                 <div className="form-group">
-                                    <form onSubmit={this.setLigthStrength} noValidate>
+                                    <form>
                                         <select className="form-control" defaultValue={this.props.sampleViewState.currentAperture} onChange={this.setApertureSize}>
                                             {this.props.sampleViewState.apertureList.map((val, i) => {
                                                 return <option key={i} value={val}>{val}</option>
@@ -74,11 +58,15 @@ render() {
                         </Popover>
                         }
                     >
-                        <button type="button" data-toggle="tooltip"  title="Start auto centring" className="btn btn-link pull-center"><i className="fa fa-2x fa-fw fa-dot-circle-o"></i></button>
+                        <button type="button" data-toggle="tooltip"  title="Set Aperture" className="btn btn-link pull-center"><i className="fa fa-2x fa-fw fa-dot-circle-o"></i></button>
                     </OverlayTrigger>
-                <button type="button" data-toggle="tooltip"  title="Measure distance" className="btn btn-link  pull-center"><i className="fa fa-2x fa-fw fa-calculator"></i></button>                              
+                <button type="button" data-toggle="tooltip"  title="Measure distance" className="btn btn-link  pull-center">
+                    <i className="fa fa-2x fa-fw fa-calculator"></i>
+                </button>                              
                 <a href="#" id="downloadLink" type="button" data-toggle="tooltip"  title="Take snapshot" className="btn btn-link  pull-center" onClick={this.takeSnapShot} download><i className="fa fa-2x fa-fw fa-camera"></i></a>                            
-                <button type="button" data-toggle="tooltip"  title="Start auto centring" className="btn btn-link pull-center" onClick={this.props.sampleActions.sendStartAutoCentring}><i className="fa fa-2x fa-fw fa-arrows"></i></button>
+                <button type="button" data-toggle="tooltip"  title="Start auto centring" className="btn btn-link pull-center" onClick={this.props.sampleActions.sendStartAutoCentring}>
+                    <i className="fa fa-2x fa-fw fa-arrows"></i>
+                </button>
                 <button type="button" data-toggle="tooltip"  title="Start 3-click centring" className="btn btn-link pull-center" onClick={this.props.sampleActions.sendStartClickCentring}>
                     <i className={"fa fa-2x fa-fw fa-circle-o-notch" + (this.props.sampleViewState.clickCentring ? " button-active" : "")}></i>
                 </button>
@@ -96,48 +84,36 @@ render() {
                     <option>8</option>
                     <option>9</option>
                 </datalist>
-                <button type="button" data-toggle="tooltip"  title="Zoom in" className="btn btn-link  pull-center"><i className="fa fa-2x fa-fw fa fa-search-plus"></i></button>
-                    
-                <button type="button" data-toggle="tooltip"  title="Abort Centring" className="btn btn-link  pull-center" onClick={this.props.sampleActions.sendAbortCentring}><i className="fa fa-2x fa-fw fa-times"></i></button>
-
+                <button type="button" data-toggle="tooltip"  title="Zoom in" className="btn btn-link  pull-center">
+                    <i className="fa fa-2x fa-fw fa fa-search-plus"></i>
+                </button>
+                <button type="button" data-toggle="tooltip"  title="Abort Centring" className="btn btn-link  pull-center" onClick={this.props.sampleActions.sendAbortCentring}>
+                    <i className="fa fa-2x fa-fw fa-times"></i>
+                </button>
                 <button type="button" data-toggle="tooltip"  title="Backlight On/Off" className="btn btn-link pull-center light-button">
                     <i id="back-light-icon" className={"fa fa-2x fa-fw fa fa-lightbulb-o" + (this.props.sampleViewState.lightOn.back ? " button-active" : "")} onClick={this.toogleBackLight}></i>
                 </button>
-                    <OverlayTrigger trigger="click" placement="top" rootClose overlay={
-                        <Popover id="Backlight" title="Backlight">
-                            <div className="form-inline">
-                                <div className="form-group">
-                                    <form onSubmit={this.setLigthStrength} noValidate>
-                                        <input className="form-control input-sm" onKeyUp={this.setLigthStrength} onClick={this.setLigthStrengthStep} type="number" step="0.1" min="0" max="2" defaultValue={this.props.sampleViewState.motors.BackLight.position} name="BackLight"/>
-                                    </form>
-                                </div>
-                            </div>
-                        </Popover>
-                        }
-                    >
-                        <span className="motor-value">{this.props.sampleViewState.motors.BackLight.position}</span>
-                    </OverlayTrigger>
+                <MotorInput 
+                    title="BackLight" 
+                    save={this.props.sampleActions.sendMotorPosition} 
+                    value={this.props.sampleViewState.motors.BackLight.position} 
+                    motorName="BackLight" 
+                    step="0.1"
+                    decimalPoints="2"
+                />
                 <button type="button" data-toggle="tooltip"  title="Frontlight On/Off" className="btn btn-link pull-center light-button">
                     <i id="front-light-icon" className={"fa fa-2x fa-fw fa fa-lightbulb-o" + (this.props.sampleViewState.lightOn.front ? " button-active" : "")} onClick={this.toogleFrontLight}></i>
-                </button>
-                    <OverlayTrigger trigger="click" placement="top" rootClose overlay={
-                        <Popover id="Backlight" title="Backlight">
-                            <div className="form-inline">
-                                <div className="form-group">
-                                    <form onSubmit={this.setLigthStrength} noValidate>
-                                        <input className="form-control input-sm" onKeyUp={this.setLigthStrength} onClick={this.setLigthStrengthStep} type="number" step="0.1" min="0" max="2" defaultValue={this.props.sampleViewState.motors.FrontLight.position} name="FrontLight"/>
-                                    </form>
-                                </div>
-                            </div>
-                        </Popover>
-                        }
-                    >
-                        <span className="motor-value">{this.props.sampleViewState.motors.FrontLight.position}</span>
-                    </OverlayTrigger>                                           
-
+                </button>                                        
+                <MotorInput 
+                    title="FrontLight" 
+                    save={this.props.sampleActions.sendMotorPosition} 
+                    value={this.props.sampleViewState.motors.FrontLight.position} 
+                    motorName="FrontLight" 
+                    step="0.1"
+                    decimalPoints="2"
+                />
             </div>
         </div>
-
         );        
 }
 }
