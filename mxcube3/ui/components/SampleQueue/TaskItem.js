c4c5d541e034856efa3fa61b7e592f60c1879478
Fredrik Bolmsten <fredrik.bolmsten@maxlab.lu.se>
2016-04-28 15:25:58 +0200

    Color for running sample

--- mxcube3/ui/components/SampleQueue/TaskItem.js
+++ mxcube3/ui/components/SampleQueue/TaskItem.js
@@ -99,7 +99,11 @@ export default class TaskItem extends Component {
     const { data, isDragging, connectDragSource, connectDropTarget } = this.props;
     const opacity = isDragging ? 0 : 1;
     let taskCSS = cx('node node-sample',{
-        'passive': this.props.checked.indexOf(this.props.id) === -1
+        'passive': this.props.checked.indexOf(this.props.id) === -1,
+        'active': data.state===1,
+        'success': data.state===2,
+        'error': data.state===3,
+        'warning': data.state===4
     });
     return connectDragSource(connectDropTarget(
       <div className={taskCSS} style={{ opacity }}>
