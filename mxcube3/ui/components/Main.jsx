5872e4b53c151f99cac1847154ce18233f01c940
Fredrik Bolmsten <fredrik.bolmsten@maxlab.lu.se>
2016-05-03 16:41:56 +0200

    Removed queue from main window

--- mxcube3/ui/components/Main.jsx
+++ mxcube3/ui/components/Main.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import SampleQueueContainer from '../containers/SampleQueueContainer'
 import MXNavbarContainer from '../containers/MXNavbarContainer'
 import TaskContainer from '../containers/TaskContainer'
 import PleaseWaitDialog from '../containers/PleaseWaitDialog'
@@ -20,10 +19,7 @@ export default class Main extends React.Component {
                       <MXNavbarContainer location={this.props.location}/>
                       <div className="container-fluid">
                         <div className="row">
-                          <div className="col-xs-2">
-                            <SampleQueueContainer/>
-                          </div>
-                          <div className="col-xs-10 main-content">
+                          <div className="col-xs-12 main-content">
                                {this.props.children}
                           </div>
                         </div>
