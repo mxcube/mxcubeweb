cda38301e10e1cac4d683d0ce240388b8f4438c6
Fredrik Bolmsten <fredrik.bolmsten@maxlab.lu.se>
2016-05-10 15:35:19 +0200

    Hide more fields in datacollection

--- mxcube3/ui/components/Tasks/DataCollection.js
+++ mxcube3/ui/components/Tasks/DataCollection.js
@@ -40,27 +40,78 @@ class DataCollection extends React.Component {
     this.props.hide();
   }
 
+  handleShowHide(e){
+    if(e.target.innerHTML === 'Show More'){
+        e.target.innerHTML = 'Show Less';
+    }else{
+        e.target.innerHTML = 'Show More';
+    }
+  }
+
 
   render() {
-    const {fields: {num_images, first_image, exp_time, resolution, osc_start , energy, osc_range, transmission, shutterless, inverse_beam,centringMethod, detector_mode, kappa, kappa_phi, space_group, prefix, run_number, beam_size }} = this.props;
+    const {fields: {num_images, first_image, exp_time, resolution, osc_start , energy, osc_range, transmission, shutterless, inverse_beam,centringMethod, detector_mode, kappa, kappa_phi, space_group, prefix, run_number, beam_size, dir }} = this.props;
 
     return (
         <Modal show={this.props.show} onHide={this.props.hide}>
             <Modal.Header closeButton>
-                <Modal.Title>Standard Collection</Modal.Title>
+                <Modal.Title>Standard Data Collection</Modal.Title>
             </Modal.Header>
             <Modal.Body>
-                <h5>Acquisition</h5>
-                <hr />
+                    <div className="task-title-head">
+                        <span className="task-title-body">
+                            Data location
+                        </span>
+                    </div>
+                
+                    <form className="form-horizontal">
+
+                     <div className="form-group">
+
+                        <label className="col-sm-12 control-label">Path: /home/20160502/RAWDATA/{dir.value} </label>
+                    </div>  
+
+                     <div className="form-group">
+
+                        <label className="col-sm-2 control-label">Subdirectory</label>
+                        <div className="col-sm-4">
+                            <input type="text" className="form-control" {...dir}/>
+                        </div>
+
+                    </div>
+
+                    <div className="form-group">
+                        <label className="col-sm-12 control-label">Filename: { prefix.value + '_' + run_number.value + '_xxxx.cbf'}</label>
+                    </div>
+
+                    <div className="form-group">
+                        <label className="col-sm-3 control-label">Prefix</label>
+                        <div className="col-sm-3">
+                            <input type="text" className="form-control" {...prefix}/>
+                        </div>
+
+                        <label className="col-sm-3 control-label">Run number</label>
+                        <div className="col-sm-3">
+                            <input type="number" className="form-control" {...run_number}/>
+                        </div>
+
+                    </div>
+                    </form>
+
+                <div className="task-title-head">
+                    <span className="task-title-body">
+                        Acquisition
+                    </span>
+                </div>
                 <form className="form-horizontal">
 
                     <div className="form-group">
-                        <label className="col-sm-3 control-label">Oscillation range:</label>
+                        <label className="col-sm-3 control-label">Oscillation range</label>
                         <div className="col-sm-3">
                             <input type="number" className="form-control" {...osc_range} />
                         </div>
 
-                        <label className="col-sm-3 control-label">First Image:</label>
+                        <label className="col-sm-3 control-label">First Image</label>
                         <div className="col-sm-3">
                             <input type="number" className="form-control" {...first_image} />
                         </div>
@@ -68,34 +119,21 @@ class DataCollection extends React.Component {
                     </div>
 
                     <div className="form-group">
-                        <label className="col-sm-3 control-label">Oscillation start:</label>
+                        <label className="col-sm-3 control-label">Oscillation start</label>
                         <div className="col-sm-3">
                             <input type="number" className="form-control" {...osc_start} />
                         </div>
 
-                        <label className="col-sm-3 control-label">Number of images:</label>
+                        <label className="col-sm-3 control-label">Number of images</label>
                         <div className="col-sm-3">
                             <input type="number" className="form-control" {...num_images} />
                         </div>
 
                     </div>
-                    <div className="form-group">
-
-                        <label className="col-sm-3 control-label">Kappa:</label>
-                        <div className="col-sm-3">
-                            <input type="number" className="form-control" {...kappa} />
-                        </div>
-
-                        <label className="col-sm-3 control-label">Phi:</label>
-                        <div className="col-sm-3">
-                            <input type="number" className="form-control" {...kappa_phi} />
-                        </div>
-
-                    </div>
 
                     <div className="form-group">
 
-                        <label className="col-sm-3 control-label">Exposure time(ms):</label>
+                        <label className="col-sm-3 control-label">Exposure time(ms)</label>
                         <div className="col-sm-3">
                             <input type="number" className="form-control" {...exp_time} />
                         </div>
@@ -110,7 +148,7 @@ class DataCollection extends React.Component {
 
                         <label className="col-sm-3 control-label">Energy (KeV)</label>
                         <div className="col-sm-3">
-                            <input type="number" className="form-control" {...energy} />
+                            <input type="number" className="form-control" step="0.1" {...energy} />
                         </div>
 
                         <label className="col-sm-3 control-label">
@@ -131,89 +169,92 @@ class DataCollection extends React.Component {
 
                         <label className="col-sm-3 control-label">Resolution (A)</label>
                         <div className="col-sm-3">
-                            <input type="number" className="form-control" {...resolution} />
+                            <input type="number" className="form-control" step="0.1" {...resolution} />
                         </div>
-
-                        <label className="col-sm-3 control-label">Detector mode:</label>
-                        <div className="col-sm-3">
-                             <select className="form-control" {...detector_mode}>
-                                <option value="0">0</option>
-                                <option value="C18">C18</option>
-                                <option value="C2">C2</option>
-                            </select>
-                        </div>
-
 
                     </div>
 
+                    <div className="collapse" id="acquistion">
 
-                    <div className="form-group">
+                        <div className="form-group">
 
-                        <label className="col-sm-3 control-label">Beam size:</label>
-                        <div className="col-sm-3">
-                            <select className="form-control" {...beam_size}>
-                                {this.props.apertureList.map((val, i) => {
-                                    return <option key={i} value={val}>{val}</option>
-                                })}
-                            </select>
+                            <label className="col-sm-3 control-label">Kappa</label>
+                            <div className="col-sm-3">
+                                <input type="number" className="form-control" {...kappa} />
+                            </div>
+
+                            <label className="col-sm-3 control-label">Phi</label>
+                            <div className="col-sm-3">
+                                <input type="number" className="form-control" {...kappa_phi} />
+                            </div>
+
                         </div>
 
 
-                        <label className="col-sm-3 control-label">Subwedge size:</label>
+                        <div className="form-group">
+
+                            <label className="col-sm-3 control-label">Beam size</label>
+                            <div className="col-sm-3">
+                                <select className="form-control" {...beam_size}>
+                                    {this.props.apertureList.map((val, i) => {
+                                        return <option key={i} value={val}>{val}</option>
+                                    })}
+                                </select>
+                            </div>
+
+
+                            <label className="col-sm-3 control-label">Subwedge size</label>
+                            <div className="col-sm-3">
+                                <input type="number" className="form-control" />
+                            </div>
+
+                        </div>
+
+
+                        <div className="form-group">
+                            <label className="col-sm-3 control-label">Detector mode</label>
+                            <div className="col-sm-3">
+                                 <select className="form-control" {...detector_mode}>
+                                    <option value="0">0</option>
+                                    <option value="C18">C18</option>
+                                    <option value="C2">C2</option>
+                                </select>
+                            </div>
+
+                            <label className="col-sm-3 control-label">
+                                <input type="checkbox" {...shutterless} />
+                                Shutterless
+                            </label>
+
+                            <label className="col-sm-3 control-label">
+                                <input type="checkbox" {...inverse_beam} />
+                                Inverse beam
+                            </label>
+
+                        </div>
+
+                    </div>
+                    <p className="text-right">
+                        <a data-toggle="collapse" data-target="#acquistion" aria-expanded="false" aria-controls="acquistion" onClick={this.handleShowHide} >
+                            Show More
+                        </a>
+                    </p>
+
+                    <div className="task-title-head">
+                        <span className="task-title-body">
+                            Processing
+                        </span>
+                    </div>
+
+                    <div className="collapse" id="processing">
+
+                    <div className="form-group">
+                        <label className="col-sm-3 control-label">N.o. residues</label>
                         <div className="col-sm-3">
                             <input type="number" className="form-control" />
                         </div>
 
-                    </div>
-
-                    <div className="form-group">
-
-                        <label className="col-sm-3 control-label">
-                            <input type="checkbox" {...shutterless} />
-                            Shutterless
-                        </label>
-
-                        <label className="col-sm-3 control-label">
-                            <input type="checkbox" {...inverse_beam} />
-                            Inverse beam
-                        </label>
-
-                    </div>
-
-
-
-                    <h5>Data location</h5>
-                    <hr />
-
-                     <div className="form-group">
-
-                        <label className="col-sm-12 control-label">File name: /user/biomax/experiment/{prefix.value + '/' + run_number.value}</label>
-                    </div>  
-
-                     <div className="form-group">
-
-                        <label className="col-sm-3 control-label">Prefix:</label>
-                        <div className="col-sm-3">
-                            <input type="number" className="form-control" {...prefix}/>
-                        </div>
-
-                        <label className="col-sm-3 control-label">Run number:</label>
-                        <div className="col-sm-3">
-                            <input type="number" className="form-control" {...run_number}/>
-                        </div>
-
-                    </div>
-
-                    <h5>Processing</h5>
-                    <hr />
-
-                    <div className="form-group">
-                        <label className="col-sm-3 control-label">N.o. residues:</label>
-                        <div className="col-sm-3">
-                            <input type="number" className="form-control" />
-                        </div>
-
-                        <label className="col-sm-3 control-label">Space group:</label>
+                        <label className="col-sm-3 control-label">Space group</label>
                         <div className="col-sm-3">
                              <select className="form-control" {...space_group}>
                                 <option value="1"></option>
@@ -227,37 +268,43 @@ class DataCollection extends React.Component {
                     <h6>Unit cell:</h6>
                     <div className="form-group">
 
-                        <label className="col-sm-2 control-label">a:</label>
+                        <label className="col-sm-2 control-label">a</label>
                         <div className="col-sm-2">
                             <input type="number" className="form-control" />
                         </div>
 
-                        <label className="col-sm-2 control-label">b:</label>
+                        <label className="col-sm-2 control-label">b</label>
                         <div className="col-sm-2">
                             <input type="number" className="form-control" />
                         </div>
 
-                        <label className="col-sm-2 control-label">c:</label>
+                        <label className="col-sm-2 control-label">c</label>
                         <div className="col-sm-2">
                             <input type="number" className="form-control" />
                         </div>
 
-                        <label className="col-sm-2 control-label"> &alpha;:</label>
+                        <label className="col-sm-2 control-label"> &alpha;</label>
                         <div className="col-sm-2">
                             <input type="number" className="form-control" />
                         </div>
 
-                        <label className="col-sm-2 control-label">  &beta;:</label>
+                        <label className="col-sm-2 control-label">  &beta;</label>
                         <div className="col-sm-2">
                             <input type="number" className="form-control" />
                         </div>
 
-                        <label className="col-sm-2 control-label">  &gamma;:</label>
+                        <label className="col-sm-2 control-label">  &gamma;</label>
                         <div className="col-sm-2">
                             <input type="number" className="form-control" />
+                        </div>
                         </div>
 
                     </div>
+                    <p className="text-right">
+                        <a data-toggle="collapse" data-target="#processing" aria-expanded="false" aria-controls="processing" onClick={this.handleShowHide}>
+                            Show More
+                        </a>
+                    </p>
                 </form>
             </Modal.Body>
             <Modal.Footer>
@@ -279,10 +326,10 @@ class DataCollection extends React.Component {
 
 DataCollection = reduxForm({ // <----- THIS IS THE IMPORTANT PART!
   form: 'datacollection',                           // a unique name for this form
-  fields: ['num_images', 'first_image', 'exp_time', 'resolution', 'osc_start' , 'energy', 'osc_range', 'transmission', 'shutterless','inverse_beam','centringMethod', 'detector_mode', 'kappa', 'kappa_phi', 'space_group', 'prefix', 'run_number', 'beam_size'] // all the fields in your form
+  fields: ['num_images', 'first_image', 'exp_time', 'resolution', 'osc_start' , 'energy', 'osc_range', 'transmission', 'shutterless','inverse_beam','centringMethod', 'detector_mode', 'kappa', 'kappa_phi', 'space_group', 'prefix', 'run_number', 'beam_size', 'dir'] // all the fields in your form
 },
 state => ({ // mapStateToProps
-  initialValues: {...state.taskForm.taskData.parameters, beam_size: state.sampleview.currentAperture} // will pull state into form's initialValues
+  initialValues: {...state.taskForm.taskData.parameters, beam_size: state.sampleview.currentAperture, prefix: "data", run_number: 1, dir: 'username'} // will pull state into form's initialValues
 }))(DataCollection);
 
 export default DataCollection;
--- mxcube3/ui/reducers/taskForm.js
+++ mxcube3/ui/reducers/taskForm.js
@@ -3,7 +3,7 @@ const initialState = {
   taskData: {},
   pointId: -1,
   showForm: '',
-  defaultParameters:{ parameters: { num_images: 2, transmission: 30, exp_time: 10, osc_start: 0.0, osc_range: 0.5, resolution: 2.5, energy: 12.5, kappa: 0, kappa_phi: 0, strategy_complexity: 1, account_rad_damage: true, opt_sad: false } }
+  defaultParameters:{ parameters: { num_images: 2, transmission: 30, exp_time: 10, osc_start: 0.0, osc_range: 0.5, resolution: 2.5, energy: 12.5, kappa: 0, kappa_phi: 0, strategy_complexity: 1, account_rad_damage: true, opt_sad: false, shutterless: true } }
 };
 
 export default (state = initialState, action) => {
