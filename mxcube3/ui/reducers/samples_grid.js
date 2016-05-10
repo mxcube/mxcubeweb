4511a833f5586b5729f6e91259667fb8e960fd4f
Marcus Oskarsson <marcus.oscarsson@esrf.fr>
2016-05-10 08:39:26 +0200

    Preliminary linting fixes before staring work with Sample Grid view
    Conflicts:
    
    	mxcube3/ui/reducers/beamline.js

--- mxcube3/ui/reducers/samples_grid.js
+++ mxcube3/ui/reducers/samples_grid.js
@@ -1,18 +1,26 @@
 import { omit } from 'lodash/object';
 
-export default (state = { samples_list: {}, filter_text: '', selected: {}, clicked_task: Object(), manualMount: { set: false, id: 0 }, login_data: {} }, action) => {
+const initialState = { samples_list: {},
+                       filter_text: '',
+                       selected: {},
+                       clicked_task: Object(),
+                       manualMount: { set: false, id: 0 },
+                       login_data: {} };
+
+export default (state = initialState, action) => {
   switch (action.type) {
     case 'UPDATE_SAMPLES':
           // should have session samples
       return Object.assign({}, state, { samples_list: action.samples_list });
     case 'ADD_SAMPLE_TO_GRID':
-          // should have session samples
-      return { ...state, samples_list: { ...state.samples_list, [action.id] : action.data }, manualMount : { ...state.manualMount, id : state.manualMount.id + 1 } };
+
+      return { ...state, samples_list: { ...state.samples_list, [action.id]: action.data },
+               manualMount: { ...state.manualMount, id: state.manualMount.id + 1 } };
     case 'TOGGLE_SELECTED':
       {
-        let new_selected = Object.assign({}, state.selected);
-        new_selected[action.index] = !state.selected[action.index];
-        return Object.assign({}, state, { selected: new_selected });
+        const newSelected = Object.assign({}, state.selected);
+        newSelected[action.index] = !state.selected[action.index];
+        return Object.assign({}, state, { selected: newSelected });
       }
     case 'CLICKED_TASK':
       {
@@ -20,13 +28,13 @@ export default (state = { samples_list: {}, filter_text: '', selected: {}, click
       }
     case 'SELECT_ALL':
       {
-          // Creating a new SampleList with the "selected" state toggled to "true"
-        let new_selected = {};
-        Object.keys(state.samples_list).forEach(function (key) {
-            new_selected[key] = action.selected;
-          });
+        // Creating a new SampleList with the "selected" state toggled to "true"
+        const newSelected = {};
+        Object.keys(state.samples_list).forEach((key) => {
+          newSelected[key] = action.selected;
+        });
 
-        return Object.assign({}, state, { selected: new_selected });
+        return Object.assign({}, state, { selected: newSelected });
       }
     case 'UNSELECT_ALL':
       {
@@ -38,49 +46,52 @@ export default (state = { samples_list: {}, filter_text: '', selected: {}, click
       }
     case 'SET_SAMPLES_INFO':
       {
-        let samples_list = {};
-        Object.keys(state.samples_list).forEach(key => {
-            let sample = state.samples_list[key];
-            let sample_info;
-            for (sample_info of action.sample_info_list) {
-            if (sample_info.code) {
-                      // find sample with data matrix code
-                if (sample.code == sample_info.code) {
-                    samples_list[key] = Object.assign({}, sample, { sample_info: sample_info });
-                    break;
-                  }
-              } else {
-                      // check with sample changer location
-                let lims_location = sample_info.containerSampleChangerLocation + ':' + sample_info.sampleLocation;
-                if (sample.location == lims_location) {
-                    samples_list[key] = Object.assign({}, sample, { sample_info: sample_info });
-                    break;
-                  }
+        const samplesList = {};
+
+        Object.keys(state.samplesList).forEach(key => {
+          const sample = state.samplesList[key];
+          let sampleInfo;
+          for (sampleInfo of action.sample_info_list) {
+            if (sampleInfo.code) {
+              // find sample with data matrix code
+              if (sample.code === sampleInfo.code) {
+                samplesList[key] = Object.assign({}, sample, { sample_info: sampleInfo });
+                break;
               }
+            } else {
+              // check with sample changer location
+              const limsLocation = `${sampleInfo.containerSampleChangerLocation} : ${sampleInfo.sampleLocation}`;
+
+              if (sample.location === limsLocation) {
+                samplesList[key] = Object.assign({}, sample, { sample_info: sampleInfo });
+                break;
+              }
+            }
           }
-            if (samples_list[key] === undefined) {
-            samples_list[key] = Object.assign({}, sample, { sample_info: null });
+          if (samplesList[key] === undefined) {
+            samplesList[key] = Object.assign({}, sample, { sample_info: null });
           }
-          });
-        return Object.assign({}, state, { samples_list: samples_list });
+        });
+        return Object.assign({}, state, { samples_list: samplesList });
       }
     case 'SET_MANUAL_MOUNT':
       {
-        return Object.assign({}, state, { manualMount: { ...state.manualMount, set : action.manual } });
+        const data = { manualMount: { ...state.manualMount, set: action.manual } };
+        return Object.assign({}, state, data);
       }
     case 'ADD_METHOD':
       {
         return Object.assign({}, state,
-             { samples_list : { ...state.samples_list,
-              [action.index] : { ...state.samples_list[action.index],
-                tasks : { ...state.samples_list[action.index].tasks, [action.queue_id] :
+             { samples_list: { ...state.samples_list,
+              [action.index]: { ...state.samples_list[action.index],
+                tasks: { ...state.samples_list[action.index].tasks, [action.queue_id]:
                 {
                   type: action.task_type,
                   label: action.task_type.split(/(?=[A-Z])/).join(' '),
                   sample_id: action.index,
                   queue_id: action.queue_id,
                   parent_id: action.parent_id,
-                  parameters : action.parameters,
+                  parameters: action.parameters,
                   state: 0
                 }
                 }
@@ -90,14 +101,14 @@ export default (state = { samples_list: {}, filter_text: '', selected: {}, click
     case 'CHANGE_METHOD':
       {
         return Object.assign({}, state,
-             { samples_list : { ...state.samples_list,
-              [action.index] : { ...state.samples_list[action.index],
-                tasks : { ...state.samples_list[action.index].tasks, [action.queue_id] :
+             { samples_list: { ...state.samples_list,
+              [action.index]: { ...state.samples_list[action.index],
+                tasks: { ...state.samples_list[action.index].tasks, [action.queue_id]:
                 {
                   ...state.samples_list[action.index].tasks[action.queue_id],
                   type: action.parameters.Type,
                   queue_id: action.queue_id,
-                  parameters : action.parameters
+                  parameters: action.parameters
                 } }
               }
              } }
@@ -106,9 +117,9 @@ export default (state = { samples_list: {}, filter_text: '', selected: {}, click
     case 'REMOVE_METHOD':
       {
         return Object.assign({}, state,
-             { samples_list : { ...state.samples_list,
-              [action.index] : { ...state.samples_list[action.index],
-                tasks : omit(state.samples_list[action.index].tasks, [action.queue_id])
+             { samples_list: { ...state.samples_list,
+              [action.index]: { ...state.samples_list[action.index],
+                tasks: omit(state.samples_list[action.index].tasks, [action.queue_id])
               }
              } }
           );
@@ -116,9 +127,9 @@ export default (state = { samples_list: {}, filter_text: '', selected: {}, click
     case 'REMOVE_SAMPLE':
       {
         return Object.assign({}, state,
-             { samples_list : { ...state.samples_list,
-              [action.index] : { ...state.samples_list[action.index],
-                tasks : {}
+             { samples_list: { ...state.samples_list,
+              [action.index]: { ...state.samples_list[action.index],
+                tasks: {}
               }
              } }
           );
@@ -126,9 +137,9 @@ export default (state = { samples_list: {}, filter_text: '', selected: {}, click
     case 'ADD_METHOD_RESULTS':
       {
         return Object.assign({}, state,
-             { samples_list : { ...state.samples_list,
-              [action.index] : { ...state.samples_list[action.index],
-                tasks : { ...state.samples_list[action.index].tasks, [action.queue_id] :
+             { samples_list: { ...state.samples_list,
+              [action.index]: { ...state.samples_list[action.index],
+                tasks: { ...state.samples_list[action.index].tasks, [action.queue_id]:
                 {
                   ...state.samples_list[action.index].tasks[action.queue_id],
                   state: action.state
@@ -141,9 +152,10 @@ export default (state = { samples_list: {}, filter_text: '', selected: {}, click
       return state; // action.sampleGridState;
     case 'SET_INITIAL_STATUS':
       {
-        return { ...state, manualMount : { set: !action.data.useSC, id: 0 } };
+        return { ...state, manualMount: { set: !action.data.useSC, id: 0 } };
       }
     default:
       return state;
   }
 };
+
