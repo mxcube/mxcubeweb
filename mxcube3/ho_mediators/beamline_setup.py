1a0b17e9f327fcda258292747ace211ab894f85d
blissadm@id30_massif3 <blissadm@id30_massif3>
2016-05-24 17:16:59 +0200

    Added base class for mediator objects Using motor states as defined by the abstract motor interface

--- mxcube3/ho_mediators/beamline_setup.py
+++ mxcube3/ho_mediators/beamline_setup.py
@@ -2,7 +2,52 @@
 from mxcube3 import socketio
 
 
-class BeamlineSetupMediator(object):
+class MOTOR_STATE:
+     NOTINITIALIZED = "NOTINITIALIZED"
+     UNUSABLE = "UNUSABLE"
+     READY = "READY"
+     MOVESTARTED = "MOVESTARTED"
+     MOVING = "MOVING"
+     ONLIMIT = "ONLIMIT"
+
+     VALUE_TO_STR = {0: "NOTINITIALIZED",
+                     1: "UNUSABLE",
+                     2: "READY",
+                     3: "MOVESTARTED",
+                     4: "MOVING",
+                     5: "ONLIMIT"}
+
+
+class INOUT_STATE:
+    IN = "in"
+    OUT = "out"
+    UNDEFINED = "undefined"
+
+    VALUE_TO_STR = {0: IN,
+                    1: OUT,
+                    2: UNDEFINED}
+
+    STR_TO_VALUE = {IN: 0,
+                    OUT: 1,
+                    UNDEFINED: 2}
+
+
+BEAMLINE_SETUP = None
+
+# Sinlgeton like interface is needed to keep the same referance to the 
+# mediator object and its corresponding hardware objects, so that signal
+# system wont cleanup signal handlers. (PyDispatcher removes signal handlers
+# when a object is garabge collected)
+def BeamlineSetupMediator(*args):
+    global BEAMLINE_SETUP
+
+    if BEAMLINE_SETUP is None:
+        BEAMLINE_SETUP = _BeamlineSetupMediator(*args)
+
+    return BEAMLINE_SETUP
+
+
+class _BeamlineSetupMediator(object):
     """
     Mediator between Beamline route and BeamlineSetup hardware object. Providing
     missing functionality while the HardwareObjects are frozen. The
@@ -11,17 +56,20 @@ class BeamlineSetupMediator(object):
     """
     def __init__(self, beamline_setup):
         self._bl = beamline_setup
+        self._ho_dict = {}
 
 
     def getObjectByRole(self, name):
         ho = self._bl.getObjectByRole(name.lower())
 
         if name == "energy":
-            return EnergyHOMediator(ho)
+            return self._ho_dict.setdefault(name, EnergyHOMediator(ho, "energy"))
         elif name == "resolution":
-            return ResolutionHOMediator(ho)
+            return self._ho_dict.setdefault(name, ResolutionHOMediator(ho, "resolution"))
         elif name == "transmission":
-            return TransmissionHOMediator(ho)
+            return self._ho_dict.setdefault(name, TransmissionHOMediator(ho, "transmission"))
+        elif name == "fast_shutter":
+            return self._ho_dict.setdefault(name, InOutHOMediator(ho, "fastShutter"))
         else:
             return ho
 
@@ -32,45 +80,72 @@ class BeamlineSetupMediator(object):
         """
         energy =  self.getObjectByRole("energy")
         transmission = self.getObjectByRole("transmission")
-        resolution = self.getObjectByRole("resolution")        
+        resolution = self.getObjectByRole("resolution")    
         fast_shutter = self.getObjectByRole("fast_shutter")
         
-        data = {"energy": {"name": "energy",
-                           "value": energy.get(),
-                           "limits": (0, 1000, 0.1),
-                           "state": energy.state(),
-                           "msg": ""
-                           },
-                "transmission": {"name": "transmission",
-                                 "value": transmission.get(),
-                                 "limits": (0, 1000, 0.1),
-                                 "state": transmission.state(),
-                                 "msg": ""},
-                "resolution": {"name": "resolution",
-                               "value": resolution.get(),
-                               "limits": (0, 1000, 0.1),
-                               "state": resolution.state(),
-                               "msg": ""},
-                "fastShutter": {"name": "fastShutter",
-                                "value": fast_shutter.getActuatorState(True),
-                                "limits": (0, 1, 1),
-                                "state": fast_shutter.getActuatorState(True)}}
+        data = {"energy": energy.dict_repr(),
+                "transmission": transmission.dict_repr(),
+                "resolution": resolution.dict_repr(),
+                "fastShutter": fast_shutter.dict_repr()}
 
         return data
 
 
-class EnergyHOMediator(object):
-    """
-    Mediator for Energy Hardware Object, a web socket is used communicate
-    information on longer running processes.
-    """
-    def __init__(self, ho):
+class HOMediatorBase(object):
+    def __init__(self, ho, name=''):
         """
         :param HardwareObject ho: Hardware object to mediate for.
         :returns: None
         """
         self._ho = ho
+        self._name = name
+
+
+    def __getattr__(self, attr):
+        if attr.startswith("__"):
+            raise AttributeError(attr)
+        return getattr(self._ho, attr)
+
+    def set(self, value):
+        pass
+
+
+    def get(self):
+        pass
+
+
+    def state(self):
+        pass
+
+
+    def stop(self):
+        pass
+
+
+    def limits(self):
+        return (0, 1, 1)
+
+
+    def dict_repr(self):
+        data = {"name": self._name,
+                "value": self.get(),
+                "limits":self.limits(),
+                "state": self.state(),
+                "msg": ""}
+
+        return data
+
+
+
+class EnergyHOMediator(HOMediatorBase):
+    """
+    Mediator for Energy Hardware Object, a web socket is used communicate
+    information on longer running processes.
+    """
+    def __init__(self, ho, name=''):
+        super(EnergyHOMediator, self).__init__(ho, name)
         ho.connect("energyChanged", self.value_change)
+
 
     def __getattr__(self, attr):
         return getattr(self._ho, attr)
@@ -112,24 +187,28 @@ class EnergyHOMediator(object):
 
 
     def state(self):
-        state = "IDLE"
+        state = MOTOR_STATE.READY
 
-        if self._ho._abort:
-            state = "ABORTED"
-        elif self._ho.moving:
-            state = "BUSY"
+        try:
+            state = self._ho.energy_motor.getState()
+        except Exception as ex:
+            pass
 
         return state
 
 
+    def stop(self):
+        self._ho.stop()
+
+
     def value_change(self, energy, wavelength):
         data = {"name": "energy", "value": energy, "state": self.state(), "msg": ""}
-        socketio.emit("value_change", data, namespace="/beamline/energy")
+        socketio.emit("beamline_value_change", data, namespace="/hwr")
 
 
-class InOutHoMediator(object):
-    def __init__(self, ho):
-        self._ho = ho
+class InOutHOMediator(HOMediatorBase):
+    def __init__(self, ho, name=''):
+        super(InOutHOMediator, self).__init__(ho, name)
         ho.connect("actuatorStateChanged", self.value_change)
 
 
@@ -137,36 +216,31 @@ class InOutHoMediator(object):
         return getattr(self._ho, attr)
 
 
-    def set(self, value=None):
-        state = self._ho.getActuatorState(True)
-
-        if state == "in":
-            self._ho.actuatorOut(False)
-        elif state == "out":
-            self._ho.actuatorIn(False)
+    def set(self, state):
+        if state == INOUT_STATE.IN:
+            self._ho.actuatorIn()
+        elif state == INOUT_STATE.OUT:
+            self._ho.actuatorOut()
 
 
     def get(self):
-        state = self._ho.getActuatorState(True)
+        return self._ho.getActuatorState()
 
-        if state == "in":
-            state = True
-        else:
-            state = False
 
-        return state
+    def stop(self):
+        self._ho.stop()
+
 
     def state(self):
-        return self._ho.getActuatorState(True)
+        return self._ho.getActuatorState()
 
 
-    def value_change(self, energy, wavelength):
-        data = self.dict_repr()
-        socketio.emit("value_change", data, namespace="/beamline/energy")
+    def value_change(self):
+        pass
 
 
     def dict_repr(self):
-        data = {"name": "fastShutter",
+        data = {"name": self._name,
                 "value": self.get(),
                 "limits": (0, 1, 1),
                 "state": self.state()}
@@ -175,9 +249,11 @@ class InOutHoMediator(object):
 
 
 
-class TransmissionHOMediator(object):
-    def __init__(self, ho):
-        self._ho = ho
+class TransmissionHOMediator(HOMediatorBase):
+    def __init__(self, ho, name=''):
+        super(TransmissionHOMediator, self).__init__(ho, name)
+        ho.connect("attFactorChanged", self.value_change)
+
 
     def __getattr__(self, attr):
         return getattr(self._ho, attr)
@@ -185,7 +261,7 @@ class TransmissionHOMediator(object):
 
     def set(self, value):
         try:
-            self._ho.setValue(value, True)
+            self._ho.set_value(round(float(value), 2))
         except Exception as ex:
             raise ValueError("Can't set transmission: %s" % str(ex))
 
@@ -202,20 +278,26 @@ class TransmissionHOMediator(object):
         return transmission
 
 
+    def stop(self):
+        self._ho.stop()
+
+
     def state(self):
-        return "IDLE"
+        return MOTOR_STATE.READY if self._ho.isReady() else MOTOR_STATE.MOVING
 
 
-class ResolutionHOMediator(object):
-    def __init__(self, ho):
-        self._ho = ho
+    def value_change(self, value):
+        socketio.emit("beamline_value_change", self.dict_repr(), namespace="/hwr")
 
-    def __getattr__(self, attr):
-        return getattr(self._ho, attr)
+
+class ResolutionHOMediator(HOMediatorBase):
+    def __init__(self, ho, name=''):
+        super(ResolutionHOMediator, self).__init__(ho, name)
+        ho.connect("valueChanged", self.value_change)
 
 
     def set(self, value):
-        self._ho.newResolution(value)
+        self._ho.move(round(float(value), 3))
         return self.get()
 
 
@@ -229,5 +311,15 @@ class ResolutionHOMediator(object):
         return resolution
 
 
+    def stop(self):
+        self._ho.stop()
+
+
     def state(self):
-        return "IDLE"
+        return MOTOR_STATE.VALUE_TO_STR.get(self._ho.getState(), 0)
+
+
+    def value_change(self, value):
+        socketio.emit("beamline_value_change", self.dict_repr(), namespace="/hwr")
+        
+
