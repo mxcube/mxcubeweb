8474d02afadcc1cb0137f16d6a4e10ba3f9a9511
blissadm@id30_massif3 <blissadm@id30_massif3>
2016-05-25 14:00:11 +0200

    Replaced the switch control with a custom control consisting of two buttons and a div for state information.

--- mxcube3/ui/components/InOutSwitch/InOutSwitch.jsx
+++ mxcube3/ui/components/InOutSwitch/InOutSwitch.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
-
-
-import Switch from 'react-bootstrap-switch';
 import 'react-bootstrap-switch/src/less/bootstrap3/build.less';
+import { Button, ButtonGroup } from 'react-bootstrap';
+
+import './style.css';
 
 
 export default class InOutSwitch extends React.Component {
   constructor(props) {
     super(props);
-    this.onChange = this.onChange.bind(this);
+    this.setIn = this.setIn.bind(this);
+    this.setOut = this.setOut.bind(this);
   }
 
 
@@ -16,10 +17,16 @@ export default class InOutSwitch extends React.Component {
   }
 
 
-  onChange(state){
+  setIn(){
     if (this.props.onSave !== undefined) {
-      const value = state ? 'in' : 'out';
-      this.props.onSave(this.props.pkey, value);
+       this.props.onSave(this.props.pkey, 'in');
+    }
+  }
+
+
+  setOut(){
+    if (this.props.onSave !== undefined) {
+       this.props.onSave(this.props.pkey, 'out');
     }
   }
 
@@ -28,20 +35,32 @@ export default class InOutSwitch extends React.Component {
     return nextProps.data !== this.props.data;
   }
 
-
+  
   render() {
-    const switchState = this.props.data.state === 'in' ? true : false;
+    const isIn = this.props.data.state === 'in' ? true : false;
+    const inButtonStyle = isIn ? 'success' : 'default';
+    const outButtonStyle = isIn ? 'default' : 'success';
+    let msgBgStyle = "bg-warning";
+    
+    if (this.props.data.state === 'in'){
+      msgBgStyle = "bg-success";
+    } else if (this.props.data.state === 'out'){
+      msgBgStyle = "bg-danger";
+    }
+
 
     return (
       <div>
-        <Switch
-          size="mini"
-          onText={this.props.onText}
-          offText={this.props.offText}
-          state={switchState}
-          labelText={this.props.labelText}
-	  onChange={this.onChange}
-        />
+        <div className="">{this.props.labelText}</div>
+        <ButtonGroup>
+          <div className={`inout-switch-msg ${msgBgStyle}`}>{this.props.data.msg}</div>
+          <Button className="" bsStyle={inButtonStyle} bsSize="small" onClick={this.setIn} active={isIn}>
+	    {this.props.onText}
+          </Button>
+          <Button bsStyle={outButtonStyle} bsSize="small" className="" onClick={this.setOut} active={!isIn}>
+            {this.props.offText}
+          </Button>
+        </ButtonGroup>
       </div>
     )
   }
@@ -49,10 +68,10 @@ export default class InOutSwitch extends React.Component {
 
 
 InOutSwitch.defaultProps = {
-  onText: 'Opened',
-  offText: 'Closed',
+  onText: 'Open',
+  offText: 'Close',
   labelText: '',
   pkey: undefined,
   onSave: undefined,
-  data: { value: 'undefined', state: 'IN', msg: '' }
+  data: { value: 'undefined', state: 'IN', msg: 'UNKNOWN' }
 };
