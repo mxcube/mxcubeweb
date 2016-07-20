6fc5e3ea467363e28e19667bfd509d8d3c655768
Mikel Eguiraun <mikel.eguiraun@maxiv.lu.se>
2016-07-20 10:00:14 +0200

    hasattr() was always True #?!

--- mxcube3/__init__.py
+++ mxcube3/__init__.py
@@ -82,7 +82,7 @@ if not app.debug or os.environ.get("WERKZEUG_RUN_MAIN") == "true":
         app.session = app.beamline.getObjectByRole("session")
         app.collect = app.beamline.getObjectByRole("collect")
         app.diffractometer = app.beamline.getObjectByRole("diffractometer")
-        if not hasattr(app.diffractometer, 'centring_motors_list'):
+        if getattr(app.diffractometer, 'centring_motors_list') is None:
             # centring_motors_list is the list of roles corresponding to diffractometer motors
             app.diffractometer.centring_motors_list = app.diffractometer.getPositions().keys()
         app.db_connection = app.beamline.getObjectByRole("lims_client")
