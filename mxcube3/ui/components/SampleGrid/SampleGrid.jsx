4ffe54461b66076e94831e30459d6cde8818f749
Marcus Oskarsson <marcus.oscarsson@esrf.fr>
2016-09-26 09:43:32 +0200

    Added overlays for sample and collection information Added execution status for tasks in sample grid

--- mxcube3/ui/components/SampleGrid/SampleGrid.jsx
+++ mxcube3/ui/components/SampleGrid/SampleGrid.jsx
@@ -290,6 +290,8 @@ export default class SampleGrid extends React.Component {
             showTaskParametersForm={this.props.showTaskParametersForm}
             toggleMovable={this.props.toggleMovable}
             picked={this.props.queue.queue[sample.sampleID]}
+            rootPath={this.props.queue.rootPath}
+            displayData={this.props.queue.displayData[sample.sampleID]}
             moving={this.props.moving[key]}
             moveItem={this.moveItem}
             canMove={this.canMove}
