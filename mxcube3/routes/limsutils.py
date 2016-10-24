e487a06fca8cb57a7fe0bb54deaa4350b35f8cbf
Marcus Oskarsson <marcus.oscarsson@esrf.fr>
2016-10-24 14:53:14 +0200

    Cleanup, improved doc and removed unnecessary call to limsutils.login

--- mxcube3/routes/limsutils.py
+++ mxcube3/routes/limsutils.py
@@ -76,9 +76,15 @@ def convert_to_dict(ispyb_object):
             if type(val) == types.InstanceType:
                 val = convert_to_dict(val)
             elif type(val) == types.ListType:
-                val = [convert_to_dict(x) if type(x) == types.InstanceType else x for x in val]
+                val = [convert_to_dict(x)
+                        if type(x) == types.InstanceType else x
+                        for x in val]
+                
             elif type(val) == types.DictType:
-                val = dict([(k, convert_to_dict(x) if type(x) == types.InstanceType else x) for k, x in val.iteritems()])
+                val = dict([(k, convert_to_dict(x)
+                            if type(x) == types.InstanceType else x)
+                            for k, x in val.iteritems()])
+
             d[key] = val
 
     return d
