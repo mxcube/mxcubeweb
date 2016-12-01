3565bca0f61db5f688d03f442729230b8b59c6c1
Mikel Eguiraun <mikel.eguiraun@gmail.com>
2016-12-01 09:59:30 +0100

    Implement getLimits for energy, det_dist and res

--- mxcube3/ho_mediators/beamline_setup.py
+++ mxcube3/ho_mediators/beamline_setup.py
@@ -5,7 +5,7 @@ import logging
 
 BEAMLINE_SETUP = None
 
-# Sinlgeton like interface is needed to keep the same referance to the 
+# Sinlgeton like interface is needed to keep the same referance to the
 # mediator object and its corresponding hardware objects, so that the signal
 # system wont cleanup signal handlers. (PyDispatcher removes signal handlers
 # when a object is garabge collected)
@@ -37,7 +37,7 @@ class _BeamlineSetupMediator(object):
             else:
                 ho = self._bl.getObjectByRole(name.lower())
         except Exception:
-            logging.getLogger("HWR").exception("Failed to get get object with role: %s" %name)
+            logging.getLogger("HWR").exception("Failed to get get object with role: %s" % name)
 
         if name == "energy":
             return self._ho_dict.setdefault(name, EnergyHOMediator(ho, "energy"))
@@ -57,18 +57,18 @@ class _BeamlineSetupMediator(object):
             return self._ho_dict.setdefault(name, DetectorDistanceHOMediator(ho, "dtox"))
         else:
             return ho
-       
+
 
     def dict_repr(self):
         """
         :returns: Dictionary value-representation for each beamline attribute
-        """        
+        """
 #        capillary = self.getObjectByRole("capillary")
 
         data = dict()
-        
+
         try:
-            energy =  self.getObjectByRole("energy")
+            energy = self.getObjectByRole("energy")
             data.update({"energy": energy.dict_repr()})
         except Exception:
             logging.getLogger("HWR").exception("Failed to get energy info")
@@ -208,7 +208,7 @@ class HOMediatorBase(object):
         """
         data = {"name": self._name,
                 "value": self.get(),
-                "limits":self.limits(),
+                "limits": self.limits(),
                 "state": self.state(),
                 "msg": self.msg()}
 
@@ -280,6 +280,20 @@ class EnergyHOMediator(HOMediatorBase):
 
     def stop(self):
         self._ho.stop()
+
+
+    def limits(self):
+        """
+        :returns: The energy limits.
+        """
+        try:
+            energy_limits = self._ho.getEnergyLimits()
+        except (AttributeError, TypeError):
+            energy_limits = (0, 50)
+            logging.getLogger("HWR").exception("Failed to get get object with role: %s" % name)
+            raise ValueError("Could not get limits")
+
+        return energy_limits
 
 
 class InOutHOMediator(HOMediatorBase):
@@ -473,6 +487,16 @@ class ResolutionHOMediator(HOMediatorBase):
 
         return resolution
 
+    def limits(self):
+        """
+        :returns: The resolution limits.
+        """
+        try:
+            resolution_limits = self._ho.getLimits()
+        except (AttributeError, TypeError):
+            raise ValueError("Could not get limits")
+
+        return resolution_limits
 
     def stop(self):
         self._ho.stop()
@@ -501,6 +525,18 @@ class DetectorDistanceHOMediator(HOMediatorBase):
             detdist = 0
 
         return detdist
+
+
+    def limits(self):
+        """
+        :returns: The detector distance limits.
+        """
+        try:
+            detdist_limits = self._ho.getLimits()
+        except (AttributeError, TypeError):
+            raise ValueError("Could not get limits")
+
+        return detdist_limits
 
 
     def stop(self):
