0e031129eca2bd0f68a6f9eaa6f50ba05374e500
Mikel Eguiraun <mikel.eguiraun@gmail.com>
2016-12-06 12:02:30 +0100

    linting fixes

--- mxcube3/ui/components/Tasks/validate.js
+++ mxcube3/ui/components/Tasks/validate.js
@@ -1,4 +1,4 @@
-var everpolate = require('everpolate');
+const everpolate = require('everpolate');
 
 const validate = (values, props) => {
   const errors = {};
@@ -6,15 +6,15 @@ const validate = (values, props) => {
     // for some reason redux-form is loaded before the initial status @##@!
     return errors;
   }
-  let curr_energy = parseFloat(values.energy);
-  let curr_res = parseFloat(values.resolution);
-  let energies = props.motorLimits.resolution.limits.map(function(value,index) { return value[0]; });
-  let limits_min = props.motorLimits.resolution.limits.map(function(value,index) { return value[1]; });
-  let limits_max = props.motorLimits.resolution.limits.map(function(value,index) { return value[2]; });
+  const currEnergy = parseFloat(values.energy);
+  const currRes = parseFloat(values.resolution);
+  const energies = props.motorLimits.resolution.limits.map(value => value[0]);
+  const limitsMin = props.motorLimits.resolution.limits.map(value => value[1]);
+  const limitsMax = props.motorLimits.resolution.limits.map(value => value[2]);
   // here we update the resolution limits based on the energy the typed in the form,
   // the limits come from a table sent by the client
-  let res_min = everpolate.linear(curr_energy, energies, limits_min);
-  let res_max = everpolate.linear(curr_energy, energies, limits_max);
+  const resMin = everpolate.linear(currEnergy, energies, limitsMin);
+  const resMax = everpolate.linear(currEnergy, energies, limitsMax);
 
   if (values.num_images === '' ||
       parseInt(values.num_images, 10) > props.acqParametersLimits.number_of_images) {
@@ -29,11 +29,11 @@ const validate = (values, props) => {
   if (values.exp_time === '' || values.exp_time > props.acqParametersLimits.exposure_time) {
     errors.exp_time = 'Exposure time above the limit';
   }
-  if (!(curr_res > res_min && curr_res < res_max)) {
+  if (!(currRes > resMin && currRes < resMax)) {
     errors.resolution = 'Resolution outside working range';
   }
-  if (!(curr_energy > props.motorLimits.energy.limits[0] &&
-        curr_energy < props.motorLimits.energy.limits[1])) {
+  if (!(currEnergy > props.motorLimits.energy.limits[0] &&
+        currEnergy < props.motorLimits.energy.limits[1])) {
     errors.energy = 'Energy outside working range';
   }
   if (values.num_images === '' || values.osc_range > props.acqParametersLimits.osc_range) {
