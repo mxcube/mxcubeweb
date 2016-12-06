fa3904e0613b4ea05f5b34cb06c3651e3de58cd3
Mikel Eguiraun <mikel.eguiraun@gmail.com>
2016-12-06 17:32:08 +0100

    Promises chaining
    I promise that I will not promise again in vain... wtf line of code...
    there is a better way for sure...

--- mxcube3/ui/actions/general.js
+++ mxcube3/ui/actions/general.js
@@ -161,8 +161,8 @@ export function getInitialStatus() {
       queue.then(parse).then(json => { state.queue = json; }).catch(notify),
       motors.then(parse).then(json => { state.Motors = json; }).catch(notify),
       beamInfo.then(parse).then(json => { state.beamInfo = json; }).catch(notify),
-      beamlineSetup.then(parse).then(json => { state.beamlineSetup = json; }).catch(notify),
-      beamlineSetup.then(parse).then(path => { state.beamlineSetup = path; }).catch(notify),
+      beamlineSetup.then(parse).then(json => { state.beamlineSetup = json; return json;}).then(
+        json => { state.datapath = json.path; }).catch(notify),
       sampleVideoInfo.then(parse).then(json => { state.Camera = json; }).catch(notify),
       diffractometerInfo.then(parse).then(json => { Object.assign(state, json); }).catch(notify),
       dcParameters.then(parse).then(json => { state.dcParameters = json; }).catch(notify),
