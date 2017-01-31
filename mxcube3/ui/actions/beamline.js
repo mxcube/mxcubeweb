541421e845c6ba43197288aa8152535a9862af14
Mikel Eguiraun <mikel.eguiraun@gmail.com>
2017-01-31 16:22:49 +0100

    State changes: - Under state.beamline: new 'actuators' and 'movables' - Corresponding actions and reducers

--- mxcube3/ui/actions/beamline.js
+++ mxcube3/ui/actions/beamline.js
@@ -13,18 +13,27 @@ export const STATE = {
 export const BL_ATTR_SET = 'BL_ATTR_SET';
 export const BL_ATTR_GET_ALL = 'BL_ATTR_GET_ALL';
 export const BL_ATTR_SET_STATE = 'BL_ATTR_SET_STATE';
+export const BL_ATTR_MOV_SET_STATE = 'BL_ATTR_MOV_SET_STATE';
+export const BL_ATTR_ACT_SET_STATE = 'BL_ATTR_ACT_SET_STATE';
 export const BL_MACH_INFO = 'BL_MACH_INFO';
-
+export const BL_ATTR_MOV_SET = 'BL_ATTR_MOV_SET';
+export const BL_ATTR_ACT_SET = 'BL_ATTR_ACT_SET';
 
 export function setBeamlineAttrAction(data) {
   return { type: BL_ATTR_SET, data };
 }
 
+export function setBeamlineMovAttrAction(data) {
+  return { type: BL_ATTR_MOV_SET, data };
+}
+
+export function setBeamlineActAttrAction(data) {
+  return { type: BL_ATTR_ACT_SET, data };
+}
 
 export function getBeamlineAttrsAction(data) {
   return { type: BL_ATTR_GET_ALL, data };
 }
-
 
 export function setMachInfo(info) {
   return { type: BL_MACH_INFO, info };
@@ -33,6 +42,20 @@ export function setMachInfo(info) {
 export function busyStateAction(name) {
   return {
     type: BL_ATTR_SET_STATE,
+    data: { name, state: STATE.BUSY }
+  };
+}
+
+export function busyMovStateAction(name) {
+  return {
+    type: BL_ATTR_MOV_SET_STATE,
+    data: { name, state: STATE.BUSY }
+  };
+}
+
+export function busyActStateAction(name) {
+  return {
+    type: BL_ATTR_ACT_SET_STATE,
     data: { name, state: STATE.BUSY }
   };
 }
@@ -59,11 +82,17 @@ export function sendGetAllAttributes() {
 }
 
 
-export function sendSetAttribute(name, value) {
+export function sendSetAttribute(name, value, type) {
   const url = `mxcube/api/v0.1/beamline/${name}`;
 
   return (dispatch) => {
-    dispatch(busyStateAction(name));
+    if (type === 'movable') {
+      dispatch(busyMovStateAction(name));
+    } else if (type === 'actuator') {
+      dispatch(busyActStateAction(name));
+    } else {
+      dispatch(busyStateAction(name));
+    }
     fetch(url, {
       method: 'PUT',
       headers: {
@@ -74,7 +103,13 @@ export function sendSetAttribute(name, value) {
       body: JSON.stringify({ name, value })
     }).then(response => response.json())
           .then(data => {
-            dispatch(setBeamlineAttrAction(data));
+            if (data.type === 'actuator') {
+              dispatch(setBeamlineActAttrAction(data));
+            } else if (data.type === 'movable') {
+              dispatch(setBeamlineMovAttrAction(data));
+            } else {
+              dispatch(setBeamlineAttrAction(data));
+            }
           }, () => {
             throw new Error(`PUT ${url} failed`);
           });
