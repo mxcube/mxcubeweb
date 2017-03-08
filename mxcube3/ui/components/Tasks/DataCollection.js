9152bcea0009147eaa4f0c5082e59887b22b1c82
blissadm@id30_massif3 <blissadm@id30_massif3>
2017-03-08 11:41:37 +0100

    added ReduxInputField component, to prevent re-rendering components at every key press and having focus lost added col1 and col2 options to InputField fixed path and filename display use latest options from InputField component to make a better layout linting

--- mxcube3/ui/components/Tasks/DataCollection.js
+++ mxcube3/ui/components/Tasks/DataCollection.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { reduxForm } from 'redux-form';
+import { reduxForm, formValueSelector } from 'redux-form';
 import { Modal, Button, Form, Row, Col } from 'react-bootstrap';
 import validate from './validate';
 import { FieldsHeader,
@@ -61,17 +61,21 @@ class DataCollection extends React.Component {
         <Modal.Body>
           <FieldsHeader title="Data location" />
           <Form horizontal>
-            <StaticField label="Path" data="xxx" />
+            <StaticField label="Path" data={this.props.path} />
             <Row>
-              <Col xs={6}>
-                <InputField propName="subdir" label="Subdirectory" />
+              <Col xs={12}>
+                <InputField propName="subdir" label="Subdirectory" col1="4" col2="8" />
               </Col>
             </Row>
-            <StaticField label="Filename" data="xxx.yyy.zzz" />
-            <FieldsRow>
-                <InputField propName="prefix" label="Prefix" />
-                <InputField propName="run_number" label="Run number" />
-            </FieldsRow>
+            <Row>
+              <Col xs={8}>
+                <InputField propName="prefix" label="Prefix" col1="6" col2="6" />
+              </Col>
+              <Col xs={4}>
+                <InputField propName="run_number" label="Run number" col1="4" col2="8" />
+              </Col>
+            </Row>
+            <StaticField label="Filename" data={this.props.filename} />
           </Form>
 
           <FieldsHeader title="Acquisition" />
@@ -135,15 +139,24 @@ DataCollection = reduxForm({
   validate
 })(DataCollection);
 
-DataCollection = connect(state => ({
-  motorLimits: state.beamline.motorsLimits,
-  acqParametersLimits: state.taskForm.acqParametersLimits,
-  initialValues: {
-    ...state.taskForm.taskData.parameters,
-    beam_size: state.sampleview.currentAperture
-  }
-})
-)(DataCollection);
+const selector = formValueSelector('datacollection');
+
+DataCollection = connect(state => {
+  const subdir = selector(state, 'subdir');
+  const prefix = selector(state, 'prefix');
+  const runNumber = selector(state, 'run_number');
+
+  return {
+    path: `${state.queue.rootPath}/${subdir}`,
+    filename: `${prefix}_${runNumber}.???`,
+    motorLimits: state.beamline.motorsLimits,
+    acqParametersLimits: state.taskForm.acqParametersLimits,
+    initialValues: {
+      ...state.taskForm.taskData.parameters,
+      beam_size: state.sampleview.currentAperture
+    }
+  };
+})(DataCollection);
 
 export default DataCollection;
 
--- mxcube3/ui/components/Tasks/fields.js
+++ mxcube3/ui/components/Tasks/fields.js
@@ -6,62 +6,70 @@ import { Row,
          Checkbox,
          FormControl,
          ControlLabel,
-         Label,
          Button } from 'react-bootstrap';
 
 export const FieldsHeader = ({ title }) => (
   <Row>
     <Col xs={12}>
-      <center><h4><Label>{title}</Label></h4></center>
+      <center><b style={{ padding: '0.5em', backgroundColor: 'white' }}>{title}</b></center>
+      <hr style={{ marginTop: '-10px' }}></hr>
     </Col>
   </Row>
 );
 
 export const StaticField = ({ label, data }) => (
-  <Row>
-    <Col xs={2} componentClass={ControlLabel}>{label}</Col>
-    <Col xs={10}>
-      <FormControl.Static>{data}</FormControl.Static>
-    </Col>
-  </Row>
+    <FormGroup>
+      <Col xs={12} style={{ textAlign: 'left' }}>
+        <FormControl.Static><b>{label}:</b>{' '}{data}</FormControl.Static>
+      </Col>
+    </FormGroup>
 );
 
-export const InputField = ({ propName, label }) => (
-   <Field name={propName}
-     component={ (prop) => <FormGroup controlId={prop.input.name} validationState={prop.meta.error ? 'error' : null }>
-                  <Col xs={4} componentClass={ControlLabel}>{label}</Col>
-                  <Col xs={8}>
-                    <FormControl value={prop.input.value} onChange={prop.input.onChange} {...prop} />
-                  </Col>
-                </FormGroup>
-              }
+const ReduxInputField = (prop) => (
+       <FormGroup controlId={prop.input.name} validationState={prop.meta.error ? 'error' : null}>
+         <Col xs={prop.col1 || 8} componentClass={ControlLabel} style={{ textAlign: 'left' }}>
+           {prop.label}
+         </Col>
+         <Col xs={prop.col2 || 4}>
+           <FormControl value={prop.input.value} onChange={prop.input.onChange} {...prop} />
+         </Col>
+       </FormGroup>
+);
+
+export const InputField = (prop) => (
+   <Field name={prop.propName}
+     component={ReduxInputField}
+     {...prop}
    />
 );
 
 export const CheckboxField = ({ propName, label }) => (
    <Field name={propName}
      component={ (prop) =>
-                <FormGroup controlId={prop.input.name} validationState={prop.meta.error ? 'error' : null }>
-                  <Col xs={12}>
-                    <Checkbox inline value={prop.input.value} onChange={prop.input.onChange} {...prop}>{label}</Checkbox>
-                  </Col>
-                </FormGroup>
-              }
+       <FormGroup controlId={prop.input.name} validationState={prop.meta.error ? 'error' : null }>
+         <Col xs={8} componentClass={ControlLabel} style={{ textAlign: 'left' }}>{label}</Col>
+         <Col xs={4}>
+           <Checkbox value={prop.input.value} onChange={prop.input.onChange} {...prop} />
+         </Col>
+       </FormGroup>
+     }
    />
 );
 
 export const SelectField = ({ propName, label, list }) => (
    <Field name={propName}
      component={ (prop) =>
-                <FormGroup controlId={prop.input.name} validationState={prop.meta.error ? 'error' : null }>
-                  <Col xs={4} componentClass={ControlLabel}>{label}</Col>
-                  <Col xs={8}>
-                    <FormControl componentClass="select" value={prop.input.value} onChange={prop.input.onChange} {...prop}>
-                      {list.map((val, i) => <option key={i} value={val}>{val}</option>)}
-                    </FormControl>
-                  </Col>
-                </FormGroup>
-              }
+       <FormGroup controlId={prop.input.name} validationState={prop.meta.error ? 'error' : null }>
+         <Col xs={8} componentClass={ControlLabel} style={{ textAlign: 'left' }}>{label}</Col>
+           <Col xs={4}>
+             <FormControl componentClass="select" value={prop.input.value}
+               onChange={prop.input.onChange} {...prop}
+             >
+               {list.map((val, i) => <option key={i} value={val}>{val}</option>)}
+             </FormControl>
+           </Col>
+       </FormGroup>
+     }
    />
 );
 
@@ -75,7 +83,7 @@ export const FieldsRow = ({ children }) => (
    </Row>
 );
 
-
+/* eslint-disable react/no-set-state */
 export class CollapsableRows extends React.Component {
   constructor(props) {
     super(props);
@@ -89,11 +97,20 @@ export class CollapsableRows extends React.Component {
       <Row>
         <Col xs={12}>
           { this.state.collapsed ?
-            <Button bsStyle="link" className="pull-right" onClick={() => {this.setState({ collapsed: false });}}>Show more</Button>
-          : <Button bsStyle="link" className="pull-right" onClick={() => {this.setState({ collapsed: true });}}>Show less</Button> }
+            <Button bsStyle="link" className="pull-right"
+              onClick={() => {this.setState({ collapsed: false });}}
+            >
+               Show more
+            </Button> :
+            <Button bsStyle="link" className="pull-right"
+              onClick={() => {this.setState({ collapsed: true });}}
+            >
+               Show less
+            </Button>
+         }
         </Col>
       </Row>
     </div>);
   }
 }
-
+/* eslint-enable react/no-set-state */
