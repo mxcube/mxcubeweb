9ae502710ce777ff20bdc21db8607cce8c3bae14
Marcus Oskarsson <marcus.oscarsson@esrf.fr>
2017-03-15 09:54:34 +0100

    Smaller improvements

--- mxcube3/ui/components/SampleChanger/SampleChanger.js
+++ mxcube3/ui/components/SampleChanger/SampleChanger.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import { Panel, Button, OverlayTrigger, Tooltip, Glyphicon, ButtonToolbar } from 'react-bootstrap';
+import { Panel, Button, DropdownButton, Glyphicon, } from 'react-bootstrap';
+import { ContextMenu, MenuItem, ContextMenuTrigger } from 'react-contextmenu';
+
 import './SampleChanger.css';
 /* eslint-disable react/no-multi-comp */
 
@@ -8,16 +10,18 @@ export class SampleChangerTree extends React.Component {
     let titleBackground;
 
     if (this.props.state === 'READY') {
-      titleBackground = 'primary';
+      titleBackground = 'default';
     } else if (this.props.state === 'MOVING') {
       titleBackground = 'warning';
     } else {
       titleBackground = 'danger';
     }
 
-    return (<Panel header={this.props.title} bsStyle={titleBackground}>
-            {this.props.children}
-        </Panel>);
+    return (
+      <Panel style={{ marginTop: '0.5em' }} header={this.props.title} bsStyle={titleBackground}>
+        {this.props.children}
+      </Panel>
+    );
   }
 }
 
@@ -49,50 +53,53 @@ export class SampleChangerTreeNode extends React.Component {
     this.props.refresh();
   }
 
+  treeNodeCbxClick(e) {
+    const treeNodeIcon = document.getElementById(`${e.target.id}icon`);
+    if (treeNodeIcon) {
+      if (e.target.checked) {
+        treeNodeIcon.className = 'fa fa-minus-square-o';
+      } else {
+        treeNodeIcon.className = 'fa fa-plus-square-o';
+      }
+    }
+  }
+
   render() {
     const inputId = this.getUniqueId();
-    const scanTooltip = (
-             <Tooltip>Scan</Tooltip>
-         );
-    const selectTooltip = (
-             <Tooltip>Select</Tooltip>
-         );
-    const refreshTooltip = (
-             <Tooltip>Refresh</Tooltip>
-         );
-    const selectBtnClasses = 'btn btn-xs btn-link custom-btn-color';
-    const selectedGlyph = this.props.selected ? 'check' : 'unchecked';
-    let select;
-    if (this.props.root) {
-      select = (<OverlayTrigger placement="top" overlay={refreshTooltip}>
-               <span>
-                 <Button className={selectBtnClasses} bsSize="xsmall" onClick={this.refreshClicked}>
-                   <Glyphicon glyph="refresh" />
-                 </Button>
-               </span>
-             </OverlayTrigger>);
-    } else {
-      select = (<OverlayTrigger placement="top" overlay={selectTooltip}>
-               <span>
-                 <Button className={selectBtnClasses} bsSize="xsmall" onClick={this.selectClicked}>
-                   <Glyphicon glyph={selectedGlyph} />
-                 </Button>
-               </span>
-             </OverlayTrigger>);
-    }
-    return (<li className="treeLi">
-             <input type="checkbox" className="treeNode" id={inputId} />
-             {select}
-             <label htmlFor={inputId} className="treeNodeLabel">{this.props.label}</label>
-             <OverlayTrigger placement="right" overlay={scanTooltip}>
-               <Button bsStyle="default" bsSize="xsmall" onClick={this.scanClicked}>
-                 <Glyphicon glyph="qrcode" />
-               </Button>
-             </OverlayTrigger>
-             <ul className="treeUl">
-                 {this.props.children}
-             </ul>
-         </li>);
+
+    return (
+      <div>
+        <li className="treeLi">
+          <input type="checkbox"
+            className="treeNode"
+            id={inputId}
+            onClick={this.treeNodeCbxClick}
+          />
+            <ContextMenuTrigger id={`${this.props.label}`}>
+              <label htmlFor={inputId} className="treeNodeLabel">
+                <i id={`${inputId}icon`} className="fa fa-plus-square-o" />
+                &nbsp;{this.props.label}
+              </label>
+            </ContextMenuTrigger>
+          <ul className="treeUl">
+            {this.props.children}
+          </ul>
+        </li>
+
+        <ContextMenu id={`${this.props.label}`}>
+          <li role="heading" className="dropdown-header">
+            <b>Container {this.props.label}</b>
+          </li>
+          <MenuItem divider />
+          <MenuItem onClick={this.scanClicked}>
+            Scan
+          </MenuItem>
+          <MenuItem onClick={this.selectClicked}>
+            Move to this container
+          </MenuItem>
+        </ContextMenu>
+      </div>
+    );
   }
 }
 
@@ -101,7 +108,6 @@ export class SampleChangerTreeItem extends React.Component {
     super(props);
     this.itemClicked = this.itemClicked.bind(this);
     this.state = { allow_control: false };
-
     this.loadSample = this.loadSample.bind(this);
     this.unloadSample = this.unloadSample.bind(this);
   }
@@ -120,31 +126,57 @@ export class SampleChangerTreeItem extends React.Component {
     this.props.unload(this.props.label);
   }
 
-  render() {
-    let scControl = '';
-    if (this.state.allow_control) {
-      scControl = (
-        <ButtonToolbar>
-          <Button bsStyle="primary" bsSize="xsmall" onClick={this.loadSample}>Load sample</Button>
-          <Button bsStyle="primary" bsSize="xsmall" onClick={this.unloadSample}>Unload here</Button>
-        </ButtonToolbar>
-      );
-    }
-    const dm = (<span><i>{this.props.dm}</i></span>);
 
-    return (<li className="treeLi" onClick={this.itemClicked}>
-            <span className="treeNodeLabel">{this.props.label}</span>{dm}
-            {scControl}
-        </li>);
+  render() {
+    const ls = this.props.status === 'Loaded' ? { display: 'inline' } : { display: 'none' };
+
+    return (
+      <div>
+        <li className="treeLi">
+          <div className="sampleLabel">
+            <span style={{ verticalAlign: 'middle' }}>
+              {this.props.label}
+            </span>
+
+            <DropdownButton
+              style={{ fontStyle: 'italic', padding: '0.2em 0.2em' }}
+              title={`${this.props.dm}`}
+              bsStyle="link"
+            >
+              <li role="heading" className="dropdown-header">
+                <b>Position {this.props.label}</b>
+              </li>
+              <MenuItem divider />
+              <MenuItem onClick={this.loadSample}>
+                Mount
+              </MenuItem>
+              <MenuItem onClick={this.unloadSample}>
+                Umount to this position
+              </MenuItem>
+            </DropdownButton>
+              <span style={{ verticalAlign: 'middle' }}>
+                &nbsp;
+                <Glyphicon style={ls} glyph="arrow-left" /> <b style={ls}>(Mounted)</b>
+              </span>
+          </div>
+        </li>
+       </div>
+    );
   }
 }
 
 export default class SampleChanger extends React.Component {
   constructor(props) {
     super(props);
-
     this.buildTree = this.buildTree.bind(this);
+    this.scan = this.scan.bind(this);
   }
+
+
+  scan() {
+    this.props.scan('');
+  }
+
 
   buildTree(node, root) {
     if (node.children) {
@@ -152,25 +184,49 @@ export default class SampleChanger extends React.Component {
       for (const c of node.children) {
         childNodes.push(this.buildTree(c));
       }
+
       return React.createElement(SampleChangerTreeNode,
-                                 { label: node.name, selected: node.selected,
+                                 { label: node.name,
+                                   selected: node.selected,
                                    root,
                                    dm: node.id,
                                    select: this.props.select,
+                                   status: node.status,
                                    scan: this.props.scan,
                                    refresh: this.props.refresh },
                                  childNodes);
     }
+
     return React.createElement(SampleChangerTreeItem,
-                               { label: node.name, dm: node.id, load: this.props.load,
+                               { label: node.name,
+                                 dm: node.id,
+                                 load: this.props.load,
+                                 status: node.status,
                                  unload: this.props.unload });
   }
 
   render() {
     const nodes = this.buildTree(this.props.contents, true);
-    return (<SampleChangerTree title="Sample Changer" state={this.props.state}>
-              {nodes}
-            </SampleChangerTree>);
+    return (
+      <div style={{ marginTop: '1em' }}>
+        <SampleChangerTree
+          title={`Sample Changer (${this.props.state})`}
+          state={this.props.state}
+        >
+          <div>
+            <Button bsStyle="default" onClick={this.props.refresh}>
+               <Glyphicon glyph="refresh" /> Refresh
+            </Button>
+            <span style={{ marginLeft: '1em' }} />
+            <Button bsStyle="default" onClick={this.scan}>
+              <Glyphicon glyph="qrcode" /> Scan all containers
+            </Button>
+            <div style={{ marginBottom: '1em' }} />
+            {nodes}
+          </div>
+        </SampleChangerTree>
+      </div>
+    );
   }
 }
 
