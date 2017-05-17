a27e482e2ad8b6dfc59c043694620d278543152c
Marcus Oscarsson <marucs.oscarsson@esrf.fr>
2017-05-17 15:28:11 +0200

    Fixed naming mistake

--- mxcube3/ui/components/PopInput/DefaultBusy.jsx
+++ mxcube3/ui/components/PopInput/DefaultBusy.jsx
@@ -4,7 +4,7 @@ import { Button, ButtonToolbar } from 'react-bootstrap';
 
 import './style.css';
 
-export default class DefaultInput extends React.Component {
+export default class DefaultBusy extends React.Component {
 
   constructor(props) {
     super(props);
@@ -30,6 +30,6 @@ export default class DefaultInput extends React.Component {
 }
 
 
-DefaultInput.defaultProps = {
+DefaultBusy.defaultProps = {
   onCancel: undefined,
 };
