91ddf4abb6197c37bbb55a5d1c815585b3a75dd9
Marcus Oscarsson <marucs.oscarsson@esrf.fr>
2017-06-20 16:48:22 +0200

    Added new component LabeledValue for simple display of values

---
+++ mxcube3/ui/components/LabeledValue/LabeledValue.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { Label } from 'react-bootstrap';
+
+import './style.css';
+
+
+export default class LabeledValue extends React.Component {
+  render() {
+    let bsStyle = 'info';
+    return (
+      <div>
+        <span>
+          <div>
+            <Label
+              bsStyle="default"
+              style={{ backgroundColor: 'transparent', display: 'block',
+                       marginBottom: '3px', color: '#000' }}
+            >
+              {this.props.name}
+            </Label>
+          </div>
+          <div>
+            <Label
+              bsStyle={bsStyle}
+              style={{ backgroundColor: 'transparent', display: 'block',
+                       fontSize: '100%', borderRadius: '0px', color: '#000' }}
+            >
+              {this.props.value}
+            </Label>
+          </div>
+        </span>
+      </div>
+    );
+  }
+}
+
+LabeledValue.defaultProps = {
+  extraInfo: {},
+  value: 0,
+  name: ''
+};
