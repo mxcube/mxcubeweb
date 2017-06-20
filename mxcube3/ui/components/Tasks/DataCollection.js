c37e61556be99fd561b2797c2093a70d8b16d831
Mikel Eguiraun <mikel.eguiraun@maxiv.lu.se>
2017-06-20 10:19:45 +0200

    fix retrieving position

--- mxcube3/ui/components/Tasks/DataCollection.js
+++ mxcube3/ui/components/Tasks/DataCollection.js
@@ -160,8 +160,8 @@ DataCollection = connect(state => {
   const prefix = selector(state, 'prefix');
   const runNumber = selector(state, 'run_number');
   const fileSuffix = state.taskForm.fileSuffix === 'h5' ? '_master.h5' : '_????.cbf';
-  const position = state.taskForm.pointID === '' ? state.taskForm.pointID : 'PX';
-
+  const position = state.taskForm.pointID === '' ? 'PX' : state.taskForm.pointID ;
+  
   return {
     path: `${state.queue.rootPath}/${subdir}`,
     filename: `${prefix}_${position}_${runNumber}${fileSuffix}`,
--- mxcube3/ui/components/Tasks/Helical.js
+++ mxcube3/ui/components/Tasks/Helical.js
@@ -159,7 +159,7 @@ Helical = connect(state => {
   const prefix = selector(state, 'prefix');
   const runNumber = selector(state, 'run_number');
   const fileSuffix = state.taskForm.fileSuffix === 'h5' ? '_master.h5' : '_????.cbf';
-  const position = state.taskForm.pointID === '' ? state.taskForm.pointID : 'LX';
+  const position = state.taskForm.pointID === '' ? 'LX' : state.taskForm.pointID;
 
   return {
     path: `${state.queue.rootPath}/${subdir}`,
