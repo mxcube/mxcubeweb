mxcube/mxcubeweb

commit ce78a67cf9e006ed4285784769281eecf1192704

* removed reduxForm from Login * updated to latest react-bootstrap and latest react-redux * removed sticky header in Sample Grid ; would be great to remove react-sticky dependency if we agree to do differently * tried to simplify components markup as much as possible * removed funky css : let's decide later if we really want that

mxcube3/ui/components/Login/Login.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { withRouter } from "react-router";
-import { Input, ButtonInput, Alert } from 'react-bootstrap';
-import { reduxForm } from 'redux-form';
+import { Grid, Row, Col, FormGroup, FormControl, ControlLabel, Alert, Button } from 'react-bootstrap';
 import logo from '../../img/mxcube_logo20.png';
 import loader from '../../img/loader.gif';
 import './Login.css';
@@ -21,8 +20,9 @@ class LoginComponent extends React.Component {
 
   signIn() {
     this.props.setLoading(true);
-    const fields = this.props.fields;
-    this.props.signIn(fields.username.value, fields.password.value);
+    const username = this.loginID.value;
+    const password = this.password.value;
+    this.props.signIn(username, password);
   }
 
   handleKeyPress(target) {
@@ -32,61 +32,42 @@ class LoginComponent extends React.Component {
   }
 
   render() {
-    const { fields: { username, password } } = this.props;
     const loginInfo = this.props.loginInfo;
 
-    return (
-      <div className="container">
-        <div className="row row-centered">
-          <div>
-            <img src={logo} className="img-logo" alt="" />
-          </div>
-          <h3 >Welcome to {loginInfo.beamline_name} at {loginInfo.synchrotron_name}</h3>
-          <div className="col-md-5 col-centered">
-            {(this.props.showError ? <Alert bsStyle="danger"><h4>Failed Sign In</h4></Alert> : '')}
-            <div className="well well-left h5">
-             <div>
-              <form>
-                <Input
-                  label="LoginID"
-                  ref="proposal"
-                  type="text"
-                  name="proposal"
-                  placeholder={loginInfo.loginType}
-                  {...username} required autoFocus
-                />
-                <Input
-                  label="Password"
-                  ref="password"
-                  type="password"
-                  name="password"
-                  placeholder="Password"
-                  {...password} required
-                  onKeyPress={this.handleKeyPress}
-                />{' '}
-                <ButtonInput
-                  id="submit"
-                  bsStyle="primary"
-                  value="Sign in"
-                  onClick={this.signIn}
-                />
-              </form>
-            </div>
-            </div>
-          <div className={this.props.loading ? '' : 'hidden'}>
-            <img src={loader} className="img-responsive" alt="" />
-          </div>
-          </div>
-        </div>
-      </div>
-          );
+    if (this.props.loading) {
+      return <img src={loader} className="centered" alt="" />
+    }
+
+    return (<Grid>
+        {(this.props.showError ? <Alert bsStyle="danger"><h4>Authentification invalide.</h4></Alert> : '')}
+        <Row>
+          <center>
+            <img src={logo} style={{ width: '192px', height: '248px', marginTop: '50px'}} />
+            <h3 style={{marginBottom: '15px'}}>Welcome to {loginInfo.beamline_name} at {loginInfo.synchrotron_name}</h3>
+          </center>
+        </Row>
+        <Row>
+          {(this.props.showError ? <Alert bsStyle="danger"><h4>Failed Sign In</h4></Alert> : '')}
+          <Col xs={4} xsOffset={4}>
+            <Row>
+              <Col xs={12}>
+                <FormGroup>
+                  <ControlLabel>LoginID</ControlLabel>                  
+                  <FormControl type="text" placeholder={loginInfo.loginType} autoFocus required inputRef={(ref)=>{this.loginID=ref}}/>
+                </FormGroup>
+                <FormGroup>
+                  <ControlLabel>Password</ControlLabel>
+                  <FormControl type="password" placeholder="Password" required inputRef={(ref)=>{this.password=ref}}/>
+                </FormGroup>
+                <Button block bsStyle="primary" onClick={this.signIn}>Sign in</Button>
+              </Col>
+            </Row>
+          </Col>
+        </Row>
+      </Grid>);
   }
 }
 
 var Login = withRouter(LoginComponent);
-Login = reduxForm({ // <----- THIS IS THE IMPORTANT PART!
-  form: 'loginForm',                           // a unique name for this form
-  fields: ['username', 'password'] // all the fields in your form
-})(Login);
 
 export default Login;

mxcube3/ui/components/Cryo/DefaultInput.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { Button, Input, ButtonToolbar } from 'react-bootstrap';
+import { Button, FormControl, ButtonToolbar } from 'react-bootstrap';
 
 import './style.css';
 
@@ -15,7 +15,7 @@ export default class DefaultInput extends React.Component {
 
 
   getValue() {
-    return this.refs.input.getValue();
+    return this.input.value; 
   }
 
 
@@ -37,8 +37,8 @@ export default class DefaultInput extends React.Component {
   render() {
     return (
       <form ref="popinput-form" className="form-inline" onSubmit={this.submit} noValidate>
-        <Input ref="input" type={this.props.dataType} style={{ width: this.props.inputSize }}
-          placeholder="" className="input-sm" defaultValue={this.props.value}
+        <FormControl ref={(ref)=>{this.input=ref}} style={{ width: this.props.inputSize }}
+          placeholder="" defaultValue={this.props.value}
         />
         <ButtonToolbar style={{ 'margin-left': '0px' }} className="form-group editable-buttons">
           <Button bsStyle="primary" className="btn-sm" onClick={this.save}>

mxcube3/ui/components/MXNavbar/MXNavbar.jsx
@@ -11,10 +11,12 @@ export default class MXNavbar extends React.Component {
     document.title = `MxCuBE-3 ${propInfo}`;
 
     return (
-      <Navbar inverse>
-        <NavBrand>
-          MXCuBE 3
-        </NavBrand>
+      <Navbar inverse fixedTop>
+        <Navbar.Header>
+          <Navbar.Brand>
+            MXCuBE 3
+          </Navbar.Brand>
+        </Navbar.Header>
         <Nav>
           <LinkContainer to="/">
             <NavItem eventKey={1}>Sample Overview</NavItem>

mxcube3/ui/components/Main.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Grid, Row, Col } from 'react-bootstrap';
 import MXNavbarContainer from '../containers/MXNavbarContainer';
 import TaskContainer from '../containers/TaskContainer';
 import PleaseWaitDialog from '../containers/PleaseWaitDialog';
@@ -22,13 +23,9 @@ export default class Main extends React.Component {
         <PassControlDialog />
         <ConfirmCollectDialog />
         <MXNavbarContainer location={this.props.location} />
-        <div className="container-fluid o-wrapper" id="o-wrapper">
-          <div className="row">
-            <div className="col-xs-12 main-content">
-              {this.props.children}
-            </div>
-          </div>
-        </div>
+        <Grid fluid>
+            {this.props.children}
+        </Grid>
       </div>
     );
   }

mxcube3/ui/components/PopInput/DefaultInput.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { Button, Input, ButtonToolbar } from 'react-bootstrap';
+import { Form, Button, FormControl, ButtonToolbar } from 'react-bootstrap';
 
 import './style.css';
 
@@ -15,7 +15,7 @@ export default class DefaultInput extends React.Component {
 
 
   getValue() {
-    return this.refs.input.getValue();
+    return this.input.value;
   }
 
 
@@ -36,11 +36,11 @@ export default class DefaultInput extends React.Component {
 
   render() {
     return (
-      <form ref="popinput-form" className="form-inline" onSubmit={this.submit} noValidate>
-        <Input ref="input" type={this.props.dataType} style={{ width: this.props.inputSize }}
-          placeholder="" className="input-sm" defaultValue={this.props.value}
+      <Form inline onSubmit={this.submit}>
+        <FormControl ref={ ref => { this.input = ref }} type={this.props.dataType} style={{ width: this.props.inputSize }}
+          placeholder="" defaultValue={this.props.value}
         />
-        <ButtonToolbar style={{ 'margin-left': '0px' }} className="form-group editable-buttons">
+        <ButtonToolbar style={{ marginLeft: '0px' }} className="form-group editable-buttons">
           <Button bsStyle="primary" className="btn-sm" onClick={this.save}>
             <i className="glyphicon glyphicon-ok" />
           </Button>
@@ -48,14 +48,13 @@ export default class DefaultInput extends React.Component {
             <i className="glyphicon glyphicon-remove" />
           </Button>
         </ButtonToolbar>
-      </form>
+      </Form>
     );
   }
 }
 
 
 DefaultInput.defaultProps = {
-  className: '',
   dataType: 'number',
   inputSize: '100px',
   value: 0,

mxcube3/ui/components/RemoteAccess/Observer.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
-import { Input, ButtonInput } from 'react-bootstrap';
+import { Form, ControlLabel, FormControl, Button } from 'react-bootstrap';
 import { setLoading } from '../../actions/general';
 import { requestControl } from '../../actions/remoteAccess';
 
@@ -10,10 +10,6 @@ class Observer extends React.Component {
     super(props);
     this.askForControl = this.askForControl.bind(this);
     this.cancelControlRequest = this.cancelControlRequest.bind(this);
-  }
-
-  componentDidUpdate() {
-    this.refs.name.refs.input.value = this.getName();
   }
 
   getName() {
@@ -32,8 +28,8 @@ class Observer extends React.Component {
     this.props.askForControlDialog(true, 'Asking for control',
                                    'Please wait while asking for control',
                                    true, this.cancelControlRequest);
-    const message = this.refs.message.refs.input.value;
-    const name = this.refs.name.refs.input.value;
+    const message = this.message.value;
+    const name = this.name.value;
 
     this.props.requestControl(true, message, name, this.props.login.loginInfo);
   }
@@ -43,33 +39,30 @@ class Observer extends React.Component {
   }
 
   render() {
-    return (
-      <div>
-        <form className="col-md-5">
-          <Input
-            ref="name"
-            label="Name"
-            id="name"
-            type="text"
-            defaultValue={this.getName()}
-          />
-          <Input
-            ref="message"
-            label="Message"
-            id="message"
-            type="textarea"
-            defaultValue="Please give me control"
-            rows="3"
-          />
-          <ButtonInput
-            id="submit"
+    return  <Form>
+          <FormGroup>
+            <ControlLabel>Name</ControlLabel>
+            <FormControl
+              ref={(ref)=>{this.name = ref }}
+              type="text"
+              defaultValue={this.getName()}
+            />
+          </FormGroup>
+          <FormGroup>
+            <ControlLabel>Message</ControlLabel>
+            <FormControl
+              ref={(ref)=>{this.message = ref}}
+              componentClass="textarea"
+              defaultValue="Please give me control"
+              rows="3"
+            />
+          </FormGroup>
+          <Button
             bsStyle="primary"
             value="Ask for control"
             onClick={this.askForControl}
           />
-        </form>
-      </div>
-    );
+    </Form>
   }
 }
 

mxcube3/ui/components/RemoteAccess/ObserverDialog.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
-import { Modal, Button, Input } from 'react-bootstrap';
+import { Modal, Button, FormControl } from 'react-bootstrap';
 import { showObserverDialog, setMaster } from '../../actions/remoteAccess';
 
 export class ObserverDialog extends React.Component {
@@ -27,7 +27,7 @@ export class ObserverDialog extends React.Component {
   }
 
   accept() {
-    const name = this.refs.name.refs.input.value;
+    const name = this.name.value;
 
     if (name) {
       this.props.setMaster(false, name);
@@ -60,9 +60,8 @@ export class ObserverDialog extends React.Component {
           continue.
         </Modal.Body>
         <Modal.Footer>
-          <Input
-            ref="name"
-            id="name"
+          <FormControl
+            ref={(ref)=>{this.name=ref}}
             type="text"
             placeholder="Your name"
           />

mxcube3/ui/components/RemoteAccess/PassControlDialog.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
-import { Modal, Button, Input } from 'react-bootstrap';
+import { Modal, Button, FormControl } from 'react-bootstrap';
 import { requestControlResponse } from '../../actions/remoteAccess';
 
 export class PassControlDialog extends React.Component {
@@ -41,12 +41,12 @@ export class PassControlDialog extends React.Component {
   }
 
   accept() {
-    const message = this.refs.message.refs.input.value;
+    const message = this.message.value;
     this.props.requestControlResponse(true, message);
   }
 
   reject() {
-    const message = this.refs.message.refs.input.value;
+    const message = this.message.value;
     this.props.requestControlResponse(false, message);
   }
 
@@ -71,9 +71,8 @@ export class PassControlDialog extends React.Component {
           </div>
         </Modal.Body>
         <Modal.Footer>
-          <Input
-            ref="message"
-            id="name"
+          <FormControl
+            ref={(ref)=>{ this.message=ref }}
             defaultValue="Here you go !"
             type="textarea"
             placeholder="Message"

commit c649f2572277ebf2718865c3a2c6e916cedc2af7

components/Login: removed bad error message that was introduced by mistake in commit ce78a67c

mxcube3/ui/components/Login/Login.jsx
@@ -39,12 +39,6 @@ export default class LoginComponent extends React.Component {
     }
 
     return (<Grid>
-        { this.props.showError ?
-          <Alert bsStyle="danger">
-            <h4>Authentification invalide.</h4>
-          </Alert>
-          : ''
-        }
         { (this.props.showForm === 'SelectProposals') ?
           <SelectProposal
             show