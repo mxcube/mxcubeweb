50e2d58112e870476efc45e34e8faeca964ba801
bliss administrator <blissadm@id29control.esrf.fr>
2018-02-08 16:49:53 +0100

    Code cleanup

--- mxcube3/routes/limsutils.py
+++ mxcube3/routes/limsutils.py
@@ -65,7 +65,7 @@ def synch_sample_list_with_queue(current_queue=None):
             if data.get("sampleName", ""):
                 sample.pop("sampleName")
 
-            if  data.get("proteinAcronym", ""):
+            if data.get("proteinAcronym", ""):
                 sample.pop("proteinAcronym")
 
             sample_list_update_sample(loc, sample)
@@ -150,11 +150,6 @@ def lims_login(loginID, password):
         return dict({'status': {'code': '0'}})
 
     if mxcube.db_connection.loginType.lower() == 'user':
-        # soap will autocreate a session if empty, this is the only reason for this
-        # login_res = mxcube.db_connection.login(loginID, password)
-        # the rest interface does not create session, but the soap login only returns one proposal
-        # if we auth by username we need all the associated proposals for later select
-
         try:
             proposals = mxcube.db_connection.get_proposals_by_user(loginID)
 
@@ -163,15 +158,11 @@ def lims_login(loginID, password):
         except:
             logging.getLogger('HWR').error('[LIMS] Could not retreive proposal list, %s' % sys.exc_info()[1])
             return dict({'status': {'code': '0'}})
+
         for prop in session['proposal_list']:
-            # if len(prop['Session']) == 0:
             todays_session = mxcube.db_connection.get_todays_session(prop)
             prop['Session'] = [todays_session['session']]
-            # elif not prop['Session'][0]['scheduled']:
-            #     todays_session = mxcube.db_connection.get_todays_session(prop)
-            #     prop['Session'] = [todays_session]
 
-    	# append a dummy proposal data so staff can select instead of their own research
         if hasattr(mxcube.session, 'commissioning_fake_proposal') and mxcube.session.is_inhouse(loginID, None):
             dummy = mxcube.session.commissioning_fake_proposal
     	    session['proposal_list'].append(dummy)
@@ -181,17 +172,19 @@ def lims_login(loginID, password):
 
     else:
         try:
-            aux = mxcube.db_connection.login(loginID, password)
-            status = aux['status']
-            aux = mxcube.db_connection.get_proposal(
-                 aux['Proposal']['code'], aux['Proposal']['number'])
+            login_data = mxcube.db_connection.login(loginID, password)
+            status = login_data['status']
+
+            proposal = mxcube.db_connection.\
+                       get_proposal(login_data['Proposal']['code'],
+                                    login_data['Proposal']['number'])
         except:
             logging.getLogger('HWR').error('[LIMS] Could not login to LIMS')
             return dict({'status': {'code': '0'}})
 
-        login_res['proposalList'] = [aux]
+        login_res['proposalList'] = [proposal]
         login_res['status'] = status
-        session['proposal_list'] = [aux]
+        session['proposal_list'] = [proposal]
 
     logging.getLogger('HWR').info('[LIMS] Logged in, proposal data: %s' % login_res)
 
