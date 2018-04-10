dff2a9b166ca427f30e2539aa02639dfa6fe0a6c
bliss administrator <blissadm@id29control.esrf.fr>
2018-04-10 08:45:15 +0200

    Not restricting access to main, so that redirect for non logged in users works

--- mxcube3/routes/Main.py
+++ mxcube3/routes/Main.py
@@ -6,13 +6,12 @@ import logging
 @mxcube.route('/samplechanger')
 @mxcube.route('/logging')
 @mxcube.route('/remoteaccess')
-@mxcube.route('/')
 @mxcube.restrict
 def serve_static_file():
     logging.getLogger('HWR').info('[Main] Serving main page')
     return mxcube.send_static_file('index.html')
 
-
+@mxcube.route('/')
 @mxcube.route('/login')
 def unrestricted_serve_static_file():
     logging.getLogger('HWR').info('[Main] Serving main page')
