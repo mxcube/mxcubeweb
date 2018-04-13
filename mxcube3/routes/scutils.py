e1780dd387579d4279b5355bae930f814f746567
Mikel Eguiraun <mikel.eguiraun@maxiv.lu.se>
2018-04-13 10:50:19 +0200

    fix for ispyb

--- mxcube3/routes/scutils.py
+++ mxcube3/routes/scutils.py
@@ -18,7 +18,7 @@ def sc_contents_add(sample):
 
     if code:
         mxcube.SC_CONTENTS.get("FROM_CODE")[code] = sample
-    else:
+    if location:
         mxcube.SC_CONTENTS.get("FROM_LOCATION")[location] = sample
 
 
