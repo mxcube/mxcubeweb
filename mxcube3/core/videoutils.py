4717a6800f6c6083c4cef944e922c64c7c5ec7b8
Marcus Oskarsson <marcus.oscarsson@esrf.fr>
2019-02-04 11:47:39 +0100

    future imports

--- mxcube3/core/videoutils.py
+++ mxcube3/core/videoutils.py
@@ -1,5 +1,9 @@
 # -*- coding: utf-8 -*-
 """Utility functions for video streaming."""
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
 import fcntl
 import io
 import os
--- mxcube3/state_storage.py
+++ mxcube3/state_storage.py
@@ -1,3 +1,7 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
 from flask_socketio import emit, join_room, leave_room
 from mxcube3 import socketio
 from mxcube3 import app as mxcube
