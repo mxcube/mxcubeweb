b00f12addc114afabbf8c7b0d6b8b4c5566ef106
Marcus Oskarsson <marcus.oscarsson@esrf.fr>
2020-04-16 13:52:02 +0200

    Added mising npm install to docker image

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -5,10 +5,15 @@
 
 conda activate mxcube
 
-cd /opt/mxcube3
 vncserver :1 -geometry 1680x1050 -depth 24 &
 
-redis-server &
-python3 mxcube3-server -r test/HardwareObjectsMockup.xml --log-file mxcube.log &
+cd /opt/mxcube3
+
+npm install
 npm start &
+
+redis-server &
+
+python3 mxcube3-server -r test/HardwareObjectsMockup.xml --log-file mxcube.log &
+
 wait
