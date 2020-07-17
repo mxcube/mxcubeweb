4ba690e773a096193f6cecb1801f96c4613b2ead
bliss administrator <blissadm@id231control.esrf.fr>
2020-07-17 10:07:59 +0200

    energyscan has changed to energy_scan.

--- mxcube3/core/qutils.py
+++ mxcube3/core/qutils.py
@@ -1213,8 +1213,7 @@ def set_energy_scan_params(model, entry, task_data, sample_model):
     :param dict task_data: Dictionary with new parameters
     """
     params = task_data["parameters"]
-
-    ftype = blcontrol.beamline.energyscan.getProperty("file_suffix", "raw").strip()
+    ftype = blcontrol.beamline.energy_scan.getProperty("file_suffix", "raw").strip()
 
     model.path_template.set_from_dict(params)
     model.path_template.suffix = ftype
