f13be73fff827dcd055c7dc6e35cf6cc0b697b2e
Marcus Oskarsson <marcus.oscarsson@esrf.fr>
2021-11-09 14:55:46 +0100

    Fixed typing error

--- mxcube3/core/models.py
+++ mxcube3/core/models.py
@@ -39,7 +39,7 @@ class HOActuatorValueChangeModel(BaseModel):
 
 
 class HOBeamRawValueModel(BaseModel):
-    apertureList: List[float] = Field([0], description="List of available apertures")
+    apertureList: List[str] = Field([0], description="List of available apertures")
     currentAperture: float = Field(0, description="Current aperture size")
     position: Tuple[float, float] = Field((0, 0), description="Beam position on OAV")
     shape: str = Field("ellipse", descrption="Beam shape")
