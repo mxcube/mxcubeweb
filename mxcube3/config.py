e415812606f327fe825d618ec05d7bd241a799d6
Marcus Oskarsson <marcus.oscarsson@esrf.fr>
2022-01-10 14:38:55 +0100

    Added method to load a yaml config file from the predefined configuration root directory

--- mxcube3/config.py
+++ mxcube3/config.py
@@ -11,7 +11,6 @@ class FlaskConfig:
     ALLOWED_CORS_ORIGINS = "*"
     SECURITY_PASSWORD_SALT = "ASALT"
 
-
 class AppConfig:
     VIDEO_FORMAT = "MPEG1"
     adapter_properties = []
@@ -22,9 +21,11 @@ class AppConfig:
 class Config:
     flask = FlaskConfig()
     app = AppConfig()
+    CONFIG_ROOT_PATH = ""
 
     def __init__(self, fpath):
         with open(fpath) as f:
+            Config.CONFIG_ROOT_PATH = os.path.dirname(fpath)
             config = ruamel.yaml.load(f.read(), ruamel.yaml.RoundTripLoader)
 
             for key, value in config["server"].items():
@@ -32,3 +33,12 @@ class Config:
 
             for key, value in config["mxcube"].items():
                 setattr(self.app, key, value)
+
+    def load_config(self, component_name):
+        fpath = os.path.join(Config.CONFIG_ROOT_PATH, f"{component_name}.yaml")
+        config = None
+
+        with open(fpath) as f:
+            config = ruamel.yaml.load(f.read(), ruamel.yaml.RoundTripLoader)
+
+        return config
