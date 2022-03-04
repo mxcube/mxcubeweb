fa07adf7164431ae0eb38c3660528a9bffc9b98a
Marcus Oskarsson <marcus.oscarsson@esrf.fr>
2022-03-04 16:24:04 +0100

    Moved redux related code to store.js

--- ui/src/index.js
+++ ui/src/index.js
@@ -4,16 +4,8 @@ import './main.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router, Route, browserHistory, IndexRoute } from 'react-router';
-import { createStore, applyMiddleware, compose } from 'redux';
 import { Provider } from 'react-redux';
-import { createLogger } from 'redux-logger';
-import thunk from 'redux-thunk';
 import { persistStore } from 'redux-persist';
-// import crosstabSync from 'redux-persist-crosstab';
-import {
-  createStateSyncMiddleware,
-  initMessageListener,
-} from "redux-state-sync";
 import SampleViewContainer from './containers/SampleViewContainer';
 import SampleGridViewContainer from './containers/SampleGridViewContainer';
 import SampleChangerContainer from './containers/SampleChangerContainer';
@@ -22,11 +14,12 @@ import LoggerContainer from './containers/LoggerContainer';
 import RemoteAccessContainer from './containers/RemoteAccessContainer';
 import HelpContainer from './containers/HelpContainer';
 import Main from './components/Main';
-import rootReducer from './reducers';
 import { serverIO } from './serverIO';
 import { getLoginInfo, startSession } from './actions/login';
 import logo from './img/mxcube_logo20.png';
 import loadingAnimation from './img/loading-animation.gif';
+
+import {store, statePersistor} from './store';
 
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import { library } from '@fortawesome/fontawesome-svg-core';
@@ -37,45 +30,12 @@ import { fab } from '@fortawesome/free-brands-svg-icons';
 // Add all icons to the library so you can use it in your page
 library.add(fas, far, fab);
 
-// Logger MUST BE the last middleware
-const middleware = [thunk, createLogger(), createStateSyncMiddleware()];
-
-// passing several store enhancers to createStore need to be compose together
-const composedEnhancers = compose(
-  applyMiddleware(...middleware)
-  // autoRehydrate()
-);
-
-const store = createStore(rootReducer, composedEnhancers);
-
-initMessageListener(store);
-
 if (module.hot) {
   // Enable Webpack hot module replacement for reducers
   module.hot.accept('./reducers', () => {
     const nextReducer = require('./reducers');
     store.replaceReducer(nextReducer);
   });
-}
-
-class ServerStorage {
-  setItem(key, value) {
-    if (store.getState().login.user.inControl) {
-      serverIO.uiStorage.setItem(key, value);
-    }
-  }
-
-  getItem(key, cb) {
-    serverIO.uiStorage.getItem(key, cb);
-  }
-
-  removeItem(key) {
-    serverIO.uiStorage.removeItem(key);
-  }
-
-  getAllKeys(cb) {
-    serverIO.uiStorage.getAllKeys(cb);
-  }
 }
 
 function requireAuth(nextState, replace, callback) {
@@ -85,35 +45,7 @@ function requireAuth(nextState, replace, callback) {
     if (!state.login.loggedIn) {
       replace('/login');
     } else {
-      const persistor = persistStore(
-        store,
-        {
-          blacklist: [
-            'remoteAccess',
-            'beamline',
-            'sampleChanger',
-            'form',
-            'login',
-            'general',
-            'logger',
-            'shapes',
-            'sampleView',
-            'taskResult',
-            'sampleChangerMaintenance',
-            'uiproperties',
-          ],
-          storage: new ServerStorage(),
-        },
-        () => {
-          /* eslint-disable react/no-set-state */
-          // this.setState({ initialized: true });
-          /* eslint-enable react/no-set-state */
-        }
-      );
-
-      serverIO.connectStateSocket(persistor);
-      // crosstabSync(persistor);
-
+      serverIO.connectStateSocket(statePersistor);
       serverIO.listen(store);
       store.dispatch(startSession());
     }
