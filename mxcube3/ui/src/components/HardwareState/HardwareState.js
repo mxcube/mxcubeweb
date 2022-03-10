30411336ebb8394ac9d6c1c74f7b0fc923391ad4
JB <jbflorial2@gmail.com>
2022-03-10 19:59:51 +0000

    Creating Hardware state component

---
+++ mxcube3/ui/src/components/HardwareState/HardwareState.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Alert } from 'react-bootstrap';
+
+import '../context-menu-style.css';
+/* eslint-disable react/no-multi-comp */
+
+export default class HardwareState extends React.Component {
+  render() {
+    let titleBackground;
+
+    if (this.props.state === 'READY') {
+      titleBackground = 'success';
+    } else if (this.props.state === 'MOVING') {
+      titleBackground = 'warning';
+    } else if (this.props.state === 'LOADING') {
+      titleBackground = 'warning';
+    } else if (this.props.state === 'DISABLED') {
+      titleBackground = 'danger';
+    } else {
+      titleBackground = 'danger';
+    }
+
+    return (
+      <Alert style={{ marginTop: '0.5em' }} variant={titleBackground}>
+        {this.props.hardwareName}
+        {' '}
+        {this.props.state}
+      </Alert>
+    );
+  }
+}
