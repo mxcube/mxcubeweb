a5520afb25363853aceb61e35f381fe8f89a7cd2
JB <jbflorial2@gmail.com>
2022-03-13 12:24:53 +0000

    remove service worker

--- mxcube3/ui/src/index.js
+++ mxcube3/ui/src/index.js
@@ -220,9 +220,3 @@ render(
   <App />,
   rootElement
 );
-
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
