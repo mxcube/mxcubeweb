aaab984dec24d634e507fd9da70aa612699dd38a
JB <jbflorial2@gmail.com>
2022-09-05 16:14:56 +0200

    Temporary add new sample to Cell 1 , Puck 1

--- ui/src/actions/sampleGrid.js
+++ ui/src/actions/sampleGrid.js
@@ -26,6 +26,8 @@ export function addSamplesToList(samplesData) {
       if (!sampleData.sampleID) {
         lastSampleID++;
         sampleData.sampleID = lastSampleID.toString();
+        sampleData['cell_no'] = 1
+        sampleData['puck_no'] = 1
       }
     }
 
