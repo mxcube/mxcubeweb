7471ae0a9fcaa0772613523febaf467d4af310fa
Marcus Oskarsson <oscarsso@esrf.fr>
2023-08-25 14:02:19 +0200

    Added possibility to display images in braggy

--- ui/src/actions/general.js
+++ ui/src/actions/general.js
@@ -347,3 +347,28 @@ export function showConfirmClearQueueDialog(show = true) {
     show,
   };
 }
+
+export function sendDisplayImage(path, imgNum) {
+  return () => {
+    fetch(
+      `mxcube/api/v0.1/detector/display_image/?path=${path}&img_num=${imgNum}`,
+      {
+        method: 'GET',
+        credentials: 'include',
+        headers: {
+          Accept: 'application/json',
+          'Content-type': 'application/json',
+        },
+      },
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        let braggyPath = data.path.replace('/data/visitor/', '');
+        braggyPath = braggyPath.split('/').slice(1).join('/');
+        window.open(
+          `https://braggy.mxcube3.esrf.fr/?file=${braggyPath}/image_${data.img_num}.h5.dataset`,
+          'braggy',
+        );
+      });
+  };
+}
