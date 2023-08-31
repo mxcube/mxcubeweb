0801b107292568a987c6ef7b6f99ac7e39f47afe
Marcus Oskarsson <oscarsso@esrf.fr>
2023-08-31 10:16:54 +0200

    Nope it was the static path to flask

--- ui/cypress/e2e/app.cy.js
+++ ui/cypress/e2e/app.cy.js
@@ -1,7 +1,7 @@
 describe('app', () => {
   it('loads the login page', () => {
     cy.visit('http://localhost:8081');
-    // cy.get('input[placeholder*="Login ID"]').should('be.visible');
+    cy.get('input[placeholder*="Login ID"]').should('be.visible');
     cy.get('input[placeholder*="Password"]').should('be.visible');
     cy.get('button[type=submit]').should('be.visible');
   });
