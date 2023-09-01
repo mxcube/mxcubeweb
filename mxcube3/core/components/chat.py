5d69bfd05c3ccfb8ce86fab8bbef8217b18d263a
Marcus Oskarsson <oscarsso@esrf.fr>
2023-09-01 10:36:44 +0200

    Only displaying messages for the concerned users

--- mxcube3/core/components/chat.py
+++ mxcube3/core/components/chat.py
@@ -11,34 +11,36 @@ class Chat(ComponentBase):
 
     def db_add_message(self, user, message):
         _m = self.app.server.user_datastore.create_message(message=message)
-        self.app.server.user_datastore.add_message_to_user(user, _m)
 
-    def append_message(self, message, username):
-        user = current_user.nickname
+        active_users = self.app.usermanager.active_logged_in_users()
+
+        for _username in active_users:
+            _user = self.app.usermanager.get_user(_username)
+            self.app.server.user_datastore.add_message_to_user(_user, _m)
+
+    def append_message(self, message, user):
 
         data = {
             "message": message,
-            "username": username,
-            "nickname": user,
-            "host": current_user.current_login_ip,
+            "username": user.username,
+            "nickname": user.nickname,
+            "host": user.current_login_ip,
             "date": datetime.datetime.now().strftime("%H:%M"),
         }
 
-        self.db_add_message(current_user, message)
+        self.db_add_message(user, message)
         self.app.server.emit("ra_chat_message", data, namespace="/hwr")
 
     def get_all_messages(self):
         message_list = []
 
-        for _m in self.app.server.user_datastore.get_all_messages():
-            user = _m.users.all()[0]
-
+        for _m in current_user.messages:
             message_list.append(
                 {
                     "message": _m.message,
-                    "username": user.username,
-                    "nickname": user.nickname,
-                    "host": user.current_login_ip,
+                    "username": _m.username,
+                    "nickname": _m.nickname,
+                    "host": _m.host,
                     "date": _m.at.strftime("%H:%M"),
                 }
             )
