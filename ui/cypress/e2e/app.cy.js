623bb2cd7b96825ddeffa8253cf5af6b19e2fab7
Marcus Oskarsson <oscarsso@esrf.fr>
2023-09-01 16:07:01 +0200

    Added a few simple test cases

--- ui/cypress/e2e/app.cy.js
+++ ui/cypress/e2e/app.cy.js
@@ -1,8 +1,36 @@
-describe('app', () => {
-  it('loads the login page', () => {
+Cypress.Commands.add('login', (username, password) => {
+  cy.visit('http://localhost:8081');
+  cy.get('input[placeholder*="Login ID"]').type('idtest0');
+  cy.get('input[placeholder*="Password"]').type('0000');
+  cy.get('button[type=submit]').click();
+  cy.get('[class=navbar-brand]').contains('MXCuBE');
+});
+
+describe('login', () => {
+  it('cant login with invalid credentials', () => {
     cy.visit('http://localhost:8081');
-    cy.get('input[placeholder*="Login ID"]').should('be.visible');
-    cy.get('input[placeholder*="Password"]').should('be.visible');
-    cy.get('button[type=submit]').should('be.visible');
+    cy.get('input[placeholder*="Login ID"]').type('idte0');
+    cy.get('input[placeholder*="Password"]').type('0000');
+    cy.get('button[type=submit]').click();
+    cy.get('pre*[class^="Login_error"]').should('be.visible');
+  });
+
+  it('can login with valid credentials', () => {
+    cy.visit('http://localhost:8081');
+    cy.get('input[placeholder*="Login ID"]').type('idtest0');
+    cy.get('input[placeholder*="Password"]').type('0000');
+    cy.get('button[type=submit]').click();
+    cy.get('[class=navbar-brand]').contains('MXCuBE');
+  });
+});
+
+describe('app', () => {
+  beforeEach(() => {
+    cy.login();
+  });
+
+  it('displays collection page', () => {
+    cy.visit('http://localhost:8081');
+    cy.get('[class=navbar-brand]').contains('MXCuBE');
   });
 });
