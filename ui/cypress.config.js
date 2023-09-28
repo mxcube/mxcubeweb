328a186a4bb8029dc4fe3475de24569282e65a64
Marcus Oskarsson <oscarsso@esrf.fr>
2023-09-28 17:09:21 +0200

    Ahh, i'm getting desperate, proxy ... base url issue ?

--- ui/cypress.config.js
+++ ui/cypress.config.js
@@ -2,8 +2,12 @@ const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:8888',
+    baseUrl: 'http://127.0.0.1:8888',
     supportFile: 'cypress/support.js',
     screenshotsFolder: 'cypress/debug',
+    setupNodeEvents(on, config) {
+      // bind to the event we care about
+      require('cypress-log-to-output').install(on);
+    },
   },
 });
--- ui/cypress/support.js
+++ ui/cypress/support.js
@@ -2,7 +2,7 @@
 import '@testing-library/cypress/add-commands';
 
 Cypress.Commands.add('login', (username = 'idtest0', password = '0000') => {
-  cy.visit('/datacollection');
+  cy.visit('http://127.0.0.1:8888');
   cy.findByRole('heading', { name: 'MXCuBE' }).should('be.visible');
   cy.findByLabelText('Login ID').type(username);
   cy.findByLabelText('Password').type(password);
