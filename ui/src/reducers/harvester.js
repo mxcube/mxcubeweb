d8f9471930a4099aeec73b4d67d9c71615ae9186
FLorial Jean Baptiste <jflorial@embl.fr>
2023-11-20 17:41:07 +0100

    Add Harvester reducers and Actions

---
+++ ui/src/reducers/harvester.js
@@ -0,0 +1,22 @@
+const INITIAL_STATE = { contents: {}, state: 'READY' };
+
+export default (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+    case 'SET_HARVESTER_CONTENTS': {
+      return { ...state, contents: action.data.harvesterContents };
+    }
+    case 'SET_INITIAL_STATE': {
+      return {
+        ...state,
+        state: action.data.harvesterState.state,
+        contents: action.data.harvesterContents,
+      };
+    }
+    case 'SET_HARVESTER_STATE': {
+      return { ...state, state: action.state };
+    }
+    default: {
+      return state;
+    }
+  }
+};
