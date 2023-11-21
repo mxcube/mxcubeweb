1eb4788f34c3f73d86a0ad16979c0c721b222a85
FLorial Jean Baptiste <jflorial@embl.fr>
2023-11-21 16:01:35 +0100

    Update Harvester and Maintenance Class equipment View

--- ui/src/components/Equipment/Harvester.jsx
+++ ui/src/components/Equipment/Harvester.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
-import { Card, Button } from 'react-bootstrap';
+import { Accordion, Card, Button, Badge } from 'react-bootstrap';
 import { contextMenu, Menu, Item, Separator } from 'react-contexify';
 
+import {CopyToClipboard} from 'react-copy-to-clipboard';
+import { MdContentCopy } from 'react-icons/md';
 import { FcCancel, FcRefresh, FcUpload, FcCollect } from 'react-icons/fc';
 
 import ImageViewer from '../ImageViewer/ImageViewer.jsx';
@@ -29,9 +31,11 @@ export default class Harvester extends React.Component {
     this.harvestCrystal = this.harvestCrystal.bind(this);
     this.harvestAndLoadCrystal = this.harvestAndLoadCrystal.bind(this);
     this.unload = this.unload.bind(this);
-    this.abort = this.abort.bind(this);
+    this.onCopy = this.onCopy.bind(this);
     this.state = {
       items: getItems(160),
+      value: '',
+      copied: false,
     };
 
     this.sampleItems = [];
@@ -71,38 +75,27 @@ export default class Harvester extends React.Component {
     this.props.unload('');
   }
 
-  abort() {
-    this.props.abort();
+  onCopy() {
+    this.setState({ copied: true });
+    setTimeout(() => this.setState({ copied: false }), 1000);
   }
 
   // display some buttons depending on available features
   render() {
-    let current = '';
-    let abortButton = '';
 
     const crystalUUID = this.props.contents.harverster_crystal_list;
     // const crystalUUID = crystalUUIDT? crystalUUIDT.concat(crystalUUIDT).concat(crystalUUIDT) : [];
     let sampleStateBackground = (key) => {
       if (key === 'ready_to_execute') {
-        return 'label-success';
+        return 'success';
       } else if (key === 'harvested' || key === 'needs_repositionning') {
-        return 'label-warning';
+        return 'warning';
       } else if (key === 'failed') {
-        return 'label-danger';
+        return 'danger';
       } else {
-        return 'label-info';
+        return 'info';
       }
     };
-
-    if (this.props.state === 'MOVING') {
-      abortButton = (
-        <Button bsStyle="default" className="abortButton" onClick={this.abort}>
-          <FcCancel /> Abort
-        </Button>
-      );
-    } else {
-      abortButton = '';
-    }
 
     const crystalMenu = (key) => (
       <Menu id={key} onShown={() => {}} onHidden={() => {}}>
@@ -120,79 +113,90 @@ export default class Harvester extends React.Component {
     );
 
     return (
-      <Card header="Contents">
-        <Card.Header style={{ marginBottom: '1em' }}>
-          <span style={{ marginLeft: '10px' }}>
-            Number Of Available Pins : {this.props.contents.number_of_pins}
-          </span>
-        </Card.Header>
-        <div style={{ padding: '1em' }}>
-          <Button variant="outline-secondary" onClick={this.props.refresh}>
-            <FcRefresh /> Refresh
-          </Button>
-          <span style={{ marginLeft: '1em' }}>{abortButton}</span>
-          {current}
-          <div style={{ marginBottom: '1em' }} />
-          <div
-            className="grid-container"
-            style={{ maxHeight: '70vh', overflowY: 'auto' }}
-          >
-            {crystalUUID
-              ? crystalUUID.map((item, index) => [
-                  <div
-                    key={item.crystal_uuid}
-                    className="grid-item"
-                    onMouseDown={this.onMouseDown}
-                    onMouseUp={this.onMouseUp}
-                    onMouseMove={this.onMouseMove}
-                    tabIndex={0}
-                    role="button"
-                    onContextMenu={(e) =>
-                      this.showContextMenu(e, item.crystal_uuid)
-                    }
-                  >
-                    <div
-                      className="selection-rubber-band"
-                      id="selectionRubberBand"
-                    />
-                    <h4 className="text-center">
-                      <span className="label label-primary">{item.name}</span>
-                    </h4>
-                    <img src={item.img_url} className="img-responsive" />
-                    <ImageViewer
-                      galleryView={false}
-                      imgUrl={item.img_url}
-                      imgAlt=""
-                      imgTargetX={item.img_target_x}
-                      imgTargetY={item.img_target_y}
-                    />
-                    <div className="row crystal-uuid-caption">
-                      <div className="col-md-12">
-                        <span
-                          className={`label ${sampleStateBackground(
-                            item.state,
-                          )} btn-product`}
-                        >
-                          {item.state ? item.state.replace('_', ' ') : ''}
-                        </span>
-                      </div>
-                      <div className="crystal-uuid-caption">
-                        <p>{item.crystal_uuid}</p>
-                      </div>
-                    </div>
-                  </div>,
-                  crystalMenu(item.crystal_uuid),
-                ])
-              : null}
-          </div>
+      <Accordion header="Contents" defaultActiveKey="0">
+        <Accordion.Item eventKey="0">
+          <Accordion.Header>
+            <span style={{ marginLeft: '10px' }}>
+              Number Of Available Pins : {this.props.contents.number_of_pins}
+            </span>
+          </Accordion.Header>
+          <Accordion.Body>
+            <div style={{ padding: '1em' }}>
+              <Button variant="outline-secondary mb-2" onClick={this.props.refresh}>
+                <FcRefresh /> Refresh
+              </Button>
+              <div
+                className="ha-grid-container"
+              >
+                {crystalUUID
+                  ? crystalUUID.map((item, index) => [
+                      <div
+                        key={item.crystal_uuid}
+                        className="ha-grid-item"
+                        // tabIndex={0}
+                        role="button"
+                        onContextMenu={(e) =>
+                          this.showContextMenu(e, item.crystal_uuid)
+                        }
+                      >
+                        <h6 className="text-center mt-1">
+                          <Badge bg="primary" className="text-center">
+                            {item.name}
+                          </Badge>
+                        </h6>
+                        <ImageViewer
+                          galleryView={false}
+                          imgUrl={item.img_url}
+                          imgAlt=""
+                          imgTargetX={item.img_target_x}
+                          imgTargetY={item.img_target_y}
+                        />
+                        <div className="crystal-uuid-caption">
+                          <div className="mt-1">
+                            <Badge
+                              bg={sampleStateBackground(item.state)}
+                            >
+                              {item.state ? item.state.replace('_', ' ') : ''}
+                            </Badge>
+                          </div>
+                          <div className="crystal-uuid-caption">
+                            <span className='me-1'>{item.crystal_uuid}</span>
+                            <CopyToClipboard
+                              className="copy-link copy-link-ha"
+                              text={item.crystal_uuid}
+                              onCopy={this.onCopy}
+                            >
+                              <Button variant="content" className="btn-copy-link">
+                                <MdContentCopy style={{ float: 'right' }} size="" />
+                                <span
+                                  className={`tooltiptext ${
+                                    this.state.copied ? 'copy-link-glow' : ''
+                                  }`}
+                                  id="myTooltip"
+                                >
+                                  {this.state.copied
+                                    ? 'crystal uuid Copied'
+                                    : 'Copy crystal uuid to Clipboard'}
+                                </span>
+                              </Button>
+                            </CopyToClipboard>
+                          </div>
+                        </div>
+                      </div>,
+                      crystalMenu(item.crystal_uuid),
+                    ])
+                  : null}
+              </div>
 
-          {/* <svg width="100%" height="100%" viewBox="0 0 100 100"
-          >
-            <image href="/files/2917/fxlogo.png" x="0" y="0" height="100" width="100" />
-            <circle cx="50" cy="50" r="5"/>
-          </svg> */}
-        </div>
-      </Card>
+              {/* <svg width="100%" height="100%" viewBox="0 0 100 100"
+              >
+                <image href="/files/2917/fxlogo.png" x="0" y="0" height="100" width="100" />
+                <circle cx="50" cy="50" r="5"/>
+              </svg> */}
+            </div>
+          </Accordion.Body>
+        </Accordion.Item>
+      </Accordion>
     );
   }
 }
