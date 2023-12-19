b72c92b8af62be59680c7d42045375d60b7c6304
Marcus Oskarsson <oscarsso@esrf.fr>
2023-12-19 14:40:48 +0100

    Fixed loading state of login component

--- ui/src/components/Login/Login.jsx
+++ ui/src/components/Login/Login.jsx
@@ -33,72 +33,74 @@ function LoginComponent(props) {
         <img src={logo} width="80" alt="" />
         MXCuBE
       </h1>
-      <Form.Group className="mb-3">
-        <InputGroup>
-          <InputGroup.Text>
-            <i className="fas fa-user" />
-          </InputGroup.Text>
-          <Controller
-            name="username"
-            control={control}
-            rules={{ required: 'Login ID is required' }}
-            render={({ field }) => (
-              <Form.Control
-                type="text"
-                aria-label="Login ID"
-                placeholder="Login ID"
-                autoFocus // eslint-disable-line jsx-a11y/no-autofocus
-                required
-                isInvalid={!!errors.username}
-                {...field}
-              />
+      <fieldset className={styles.fieldset} disabled={loading}>
+        <Form.Group className="mb-3">
+          <InputGroup>
+            <InputGroup.Text>
+              <i className="fas fa-user" />
+            </InputGroup.Text>
+            <Controller
+              name="username"
+              control={control}
+              rules={{ required: 'Login ID is required' }}
+              render={({ field }) => (
+                <Form.Control
+                  type="text"
+                  aria-label="Login ID"
+                  placeholder="Login ID"
+                  autoFocus // eslint-disable-line jsx-a11y/no-autofocus
+                  required
+                  isInvalid={!!errors.username}
+                  {...field}
+                />
+              )}
+            />
+            {errors.username && (
+              <Form.Control.Feedback type="invalid">
+                {errors.username.message}
+              </Form.Control.Feedback>
             )}
-          />
-          {errors.username && (
-            <Form.Control.Feedback type="invalid">
-              {errors.username.message}
-            </Form.Control.Feedback>
-          )}
-        </InputGroup>
-      </Form.Group>
-      <Form.Group className="mb-3">
-        <InputGroup>
-          <InputGroup.Text>
-            <i className="fas fa-lock" />
-          </InputGroup.Text>
-          <Controller
-            name="password"
-            control={control}
-            rules={{ required: 'Password is required' }}
-            render={({ field }) => (
-              <Form.Control
-                type="password"
-                aria-label="Password"
-                placeholder="Password"
-                required
-                isInvalid={!!errors.password}
-                {...field}
-              />
+          </InputGroup>
+        </Form.Group>
+        <Form.Group className="mb-3">
+          <InputGroup>
+            <InputGroup.Text>
+              <i className="fas fa-lock" />
+            </InputGroup.Text>
+            <Controller
+              name="password"
+              control={control}
+              rules={{ required: 'Password is required' }}
+              render={({ field }) => (
+                <Form.Control
+                  type="password"
+                  aria-label="Password"
+                  placeholder="Password"
+                  required
+                  isInvalid={!!errors.password}
+                  {...field}
+                />
+              )}
+            />
+            {errors.password && (
+              <Form.Control.Feedback type="invalid">
+                {errors.password.message}
+              </Form.Control.Feedback>
             )}
-          />
-          {errors.password && (
-            <Form.Control.Feedback type="invalid">
-              {errors.password.message}
-            </Form.Control.Feedback>
+          </InputGroup>
+        </Form.Group>
+        <Button type="submit" size="lg" className={styles.btn}>
+          {loading && (
+            <img className={styles.loader} src={loader} width="25" alt="" />
           )}
-        </InputGroup>
-      </Form.Group>
-      <Button type="submit" size="lg" className={styles.btn}>
-        {loading && (
-          <img className={styles.loader} src={loader} width="25" alt="" />
+          Sign in
+        </Button>
+        {!loading && showError && (
+          <Alert className="mt-3" variant="danger">
+            <pre className={styles.errorMsg}>{errorMessage}</pre>
+          </Alert>
         )}
-        Sign in
-      </Button>
-      {!loading && showError && (
-        <Alert className="mt-3" variant="danger">
-          <pre className={styles.errorMsg}>{errorMessage}</pre>
-        </Alert>
-      )}
+      </fieldset>
     </Form>
   );
 }
