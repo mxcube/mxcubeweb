53efa39920134841705c8dda8d37899a17b9c6fa
FLorial Jean Baptiste <jflorial@embl.fr>
2024-03-25 14:45:37 +0100

    Do not display the component if there is no camera setup
    Add key on parent div

--- ui/src/components/BeamlineCamera/BeamlineCamera.jsx
+++ ui/src/components/BeamlineCamera/BeamlineCamera.jsx
@@ -27,108 +27,103 @@ export default function BeamlineCamera(props) {
 
   function renderVideo() {
     const DraggableElements = [];
-    if (cameraSetup) {
-      for (const [key, camera] of cameraSetup.components.entries()) {
-        DraggableElements.push(
-          showVideoModal[key] ? (
-            <div className="draggableHandle">
-              <Draggable
-                key={key}
-                defaultPosition={{ x: 200, y: 100 + 50 * key }}
-              >
-                <Card className={styles.draggableHandle}>
-                  <Card.Header>
-                    <Stack direction="horizontal" gap={3}>
-                      <div className={styles.headerTitle}>{camera.label}</div>
-                      <div className="p-2 ms-auto">
-                        <Button
-                          variant="outline-secondary"
-                          onClick={() =>
-                            handleImageClick(
-                              camera.url,
-                              camera.width,
-                              camera.height,
-                            )
-                          }
-                          size="sm"
-                        >
-                          <img src={pip} alt="PIP Icon" />
-                        </Button>
-                      </div>
-                      <div className="vr" />
-                      <div>
-                        <MdClose
-                          color="red"
-                          onClick={() => handleShowVideoModal(key, false)}
-                          size="1.5em"
-                          className={styles.closeBtn}
-                        />
-                      </div>
-                    </Stack>
-                  </Card.Header>
-                  <Card.Body>
-                    {camera.format !== 'mp4' ? (
-                      <img
-                        src={camera.url}
-                        alt={camera.label}
-                        width={camera.width}
-                        height={camera.height}
+    for (const [key, camera] of cameraSetup.components.entries()) {
+      DraggableElements.push(
+        showVideoModal[key] ? (
+          <div key={`draggable-video_${key}`} className="draggableHandle">
+            <Draggable defaultPosition={{ x: 200, y: 100 + 50 * key }}>
+              <Card className={styles.draggableHandle}>
+                <Card.Header>
+                  <Stack direction="horizontal" gap={3}>
+                    <div className={styles.headerTitle}>{camera.label}</div>
+                    <div className="p-2 ms-auto">
+                      <Button
+                        variant="outline-secondary"
+                        onClick={() =>
+                          handleImageClick(
+                            camera.url,
+                            camera.width,
+                            camera.height,
+                          )
+                        }
+                        size="sm"
+                      >
+                        <img src={pip} alt="PIP Icon" />
+                      </Button>
+                    </div>
+                    <div className="vr" />
+                    <div>
+                      <MdClose
+                        color="red"
+                        onClick={() => handleShowVideoModal(key, false)}
+                        size="1.5em"
+                        className={styles.closeBtn}
                       />
-                    ) : (
-                      <video
-                        src={camera.url}
-                        alt={camera.label}
-                        width={camera.width}
-                        height={camera.height}
-                      />
-                    )}
-                  </Card.Body>
-                </Card>
-              </Draggable>
-            </div>
-          ) : null,
-        );
-      }
+                    </div>
+                  </Stack>
+                </Card.Header>
+                <Card.Body>
+                  {camera.format !== 'mp4' ? (
+                    <img
+                      src={camera.url}
+                      alt={camera.label}
+                      width={camera.width}
+                      height={camera.height}
+                    />
+                  ) : (
+                    <video
+                      src={camera.url}
+                      alt={camera.label}
+                      width={camera.width}
+                      height={camera.height}
+                    />
+                  )}
+                </Card.Body>
+              </Card>
+            </Draggable>
+          </div>
+        ) : null,
+      );
     }
     return DraggableElements;
   }
 
   function renderCamera() {
     const dropdownItem = [];
-    if (cameraSetup) {
-      for (const [key, camera] of cameraSetup.components.entries()) {
-        dropdownItem.push(
-          <Dropdown.Item
-            key={key}
-            onClick={() => handleShowVideoModal(key, true)}
-          >
-            {camera.label} <i className="fas fa-video" />
-          </Dropdown.Item>,
-          <Dropdown.Divider />,
-        );
-      }
+    for (const [key, camera] of cameraSetup.components.entries()) {
+      dropdownItem.push(
+        <Dropdown.Item
+          key={key}
+          onClick={() => handleShowVideoModal(key, true)}
+        >
+          {camera.label} <i className="fas fa-video" />
+        </Dropdown.Item>,
+        <Dropdown.Divider />,
+      );
     }
     return dropdownItem;
   }
 
-  return [
-    <Dropdown
-      title="Beamline Cameras"
-      id="beamline-cameras-dropdown"
-      variant="outline-secondary"
-      autoClose="outside"
-      key="beamline-cameras-dropdown"
-    >
-      <Dropdown.Toggle
+  return cameraSetup?.components.length > 0 ? (
+    <>
+      <Dropdown
+        title="Beamline Cameras"
+        id="beamline-cameras-dropdown"
         variant="outline-secondary"
-        size="sm"
-        className="mb-1"
-        style={{ width: '150px' }}
+        autoClose="outside"
+        key="beamline-cameras-dropdown"
       >
-        Beamline Cameras
-      </Dropdown.Toggle>
-      <Dropdown.Menu>{renderCamera()}</Dropdown.Menu>
-    </Dropdown>,
-    renderVideo(),
-  ];
+        <Dropdown.Toggle
+          variant="outline-secondary"
+          size="sm"
+          className="mb-1"
+          style={{ width: '150px' }}
+        >
+          Beamline Cameras
+        </Dropdown.Toggle>
+        <Dropdown.Menu>{renderCamera()}</Dropdown.Menu>
+      </Dropdown>
+      {renderVideo()}
+    </>
+  ) : null;
 }
