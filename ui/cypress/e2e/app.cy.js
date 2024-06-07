03cf66f52953e552e43116965e5355142133edbe
walesch-yan <yan.walesch@esrf.fr>
2024-06-07 14:00:30 +0200

    Added tests for login behavior
    Added 2 e2e tests for login with the reserved passwords "wrong" and "ispybDown" respectively. In both cases, the login attempt should show the message "Could not authenticate".

--- ui/cypress/e2e/app.cy.js
+++ ui/cypress/e2e/app.cy.js
@@ -6,6 +6,16 @@ describe('login', () => {
     cy.findByText('Could not authenticate').should('be.visible');
   });
 
+  it("can't login with reserved password: wrong", () => {
+    cy.login('idtest0', 'wrong');
+    cy.findByText('Could not authenticate').should('be.visible');
+  });
+
+  it("can't login with reserved password: ispybDown", () => {
+    cy.login('idtest0', 'ispybDown');
+    cy.findByText('Could not authenticate').should('be.visible');
+  });
+
   it('can login with valid credentials', () => {
     cy.login();
     cy.findByRole('link', { name: 'MXCuBE-Web (OSC)' }).should('be.visible');
