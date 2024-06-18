40683f9afdd56e708e6cf5f75336a6c366620893
walesch-yan <yan.walesch@esrf.fr>
2024-06-18 17:17:20 +0200

    added 3-click centring tests

---
+++ ui/cypress/e2e/queue.cy.js
@@ -0,0 +1,14 @@
+/* global cy, it, describe, beforeEach */
+
+describe('queue', () => {
+  beforeEach(() => {
+    cy.login();
+    cy.findByRole('link', { name: 'MXCuBE-Web (OSC)' }).should('be.visible');
+    cy.takeControl();
+  });
+
+  it('mount a test sample', () => {
+    cy.mountSample('test', 'test');
+    cy.findByText('Sample: test - test').should('be.visible');
+  });
+});
