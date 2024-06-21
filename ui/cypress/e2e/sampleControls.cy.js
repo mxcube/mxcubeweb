c12cdead924795e59cec2b68df740163ab0469e4
walesch-yan <yan.walesch@esrf.fr>
2024-06-21 10:50:07 +0200

    use more descriptive and accessible query methods in Cypress tests

--- ui/cypress/e2e/sampleControls.cy.js
+++ ui/cypress/e2e/sampleControls.cy.js
@@ -9,7 +9,7 @@ describe('3-click centring', () => {
 
   it('3-click centring should not work without sample', () => {
     cy.clearSamples();
-    cy.contains('button', '3-click centring').click();
+    cy.findByRole('button', { name: '3-click centring' }).click();
     cy.findByRole('alert', 'Error: There is no sample mounted').should(
       'be.visible',
     );
@@ -17,7 +17,7 @@ describe('3-click centring', () => {
 
   it('Each click is rotating the sample by 90 degrees', () => {
     cy.mountSample();
-    cy.contains('button', '3-click centring').click();
+    cy.findByRole('button', { name: '3-click centring' }).click();
     cy.get('.form-control[name="diffractometer.phi"]')
       .invoke('val')
       .then((value) => {
@@ -29,7 +29,7 @@ describe('3-click centring', () => {
           cy.wait(1000);
           cy.reload();
           // press the centring button again after reload to stay in the correct mode
-          cy.contains('button', '3-click centring').click();
+          cy.findByRole('button', { name: '3-click centring' }).click();
           cy.get('.form-control[name="diffractometer.phi"]')
             .invoke('val')
             .should('equal', omegaValue.toFixed(2));
