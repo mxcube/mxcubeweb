188954767004ca430ea101ed1465c8feadda88cb
Axel Bocciarelli <axelboc@gmail.com>
2024-06-28 11:33:59 +0200

    Remove unused FontAwesome deps and init code

--- ui/src/index.js
+++ ui/src/index.js
@@ -8,13 +8,6 @@ import { store } from './store';
 import DefaultErrorBoundary from './containers/DefaultErrorBoundary';
 
 import '@fortawesome/fontawesome-free/css/all.min.css';
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { fas } from '@fortawesome/free-solid-svg-icons';
-import { far } from '@fortawesome/free-regular-svg-icons';
-import { fab } from '@fortawesome/free-brands-svg-icons';
-
-// Add all icons to the library so you can use it in your page
-library.add(fas, far, fab);
 
 if (module.hot) {
   // Enable Webpack hot module replacement for reducers
