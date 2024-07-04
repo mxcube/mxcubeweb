97fbf84fd466f000c9d49e7a140946e39a1b6bd4
Elmir Jagudin <elmir.jagudin@maxiv.lu.se>
2024-07-04 10:28:21 +0200

    adds --export-yaml-config command line argument
    Adds --export-yaml-config command line argument to mxcubeweb-server
    script. When used, the HWO YAML config files will be written to the
    specified directory.
    
    The aim of this feature is assist in migrating beamline XML
    configuration to YAML style.

--- mxcubeweb/__init__.py
+++ mxcubeweb/__init__.py
@@ -98,6 +98,13 @@ def parse_args(argv):
         default=False,
     )
 
+    opt_parser.add_argument(
+        "--export-yaml-config",
+        dest="yaml_export_directory",
+        type=Path,
+        help="write YAML configuration to specified path",
+    )
+
     # If `argv` is `None`, then `argparse.ArgumentParser.parse_args`
     # will know to read from `sys.argv` instead.
     return opt_parser.parse_args(argv)
@@ -111,7 +118,9 @@ def build_server_and_config(test=False, argv=None):
         # as the hwr_directory. I need it for sensible managing of a multi-beamline test set-up
         # without continuously editing the main config files.
         # Note that the machinery was all there in the core already. rhfogh.
-        HWR.init_hardware_repository(cmdline_options.hwr_directory)
+        HWR.init_hardware_repository(
+            cmdline_options.hwr_directory, cmdline_options.yaml_export_directory
+        )
         config_path = HWR.get_hardware_repository().find_in_repository("mxcube-web")
 
         cfg = Config(config_path)
