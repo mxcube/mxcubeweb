8b8eeb75ea2fc011ce05097912da552fb12ac2ff
Axel Bocciarelli <axelboc@gmail.com>
2024-08-19 15:55:33 +0200

    Refactor `UserList`

--- ui/src/components/RemoteAccess/UserList.jsx
+++ ui/src/components/RemoteAccess/UserList.jsx
@@ -1,90 +1,65 @@
 import React from 'react';
-import { bindActionCreators } from 'redux';
-import { connect } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { Row, Col, Button, Card } from 'react-bootstrap';
 import { giveControl, logoutUser } from '../../actions/remoteAccess';
 
-class UserList extends React.Component {
-  getObservers() {
-    const observers = [];
+function UserList() {
+  const dispatch = useDispatch();
 
-    for (const observer of this.props.remoteAccess.observers) {
-      observers.push(
-        <Row key={observer.username} className="mt-3">
+  const user = useSelector((state) => state.login.user);
+  const observers = useSelector((state) => state.remoteAccess.observers);
+
+  return (
+    <Card className="mb-3">
+      <Card.Header>Users</Card.Header>
+      <Card.Body>
+        <Row>
           <Col sm={4}>
-            <span style={{ lineHeight: '24px' }}>
-              {observer.nickname || <em>Not provided</em>}
-            </span>
+            <b>Name</b>
           </Col>
-          <Col sm={3}>
-            <span style={{ lineHeight: '24px' }}>{observer.ip}</span>
+          <Col sm={4}>
+            <b>Host</b>
           </Col>
-          {this.props.login.user.inControl && (
-            <Col sm={5}>
-              <Button
-                size="sm"
-                variant="outline-secondary"
-                className="me-3"
-                onClick={() => this.props.giveControl(observer.username)}
-              >
-                Give control
-              </Button>
-              {this.props.login.user.isstaff && (
-                <span>
-                  &nbsp;
+          <Col sm={4}>
+            <span>&nbsp;</span>
+          </Col>
+        </Row>
+        {observers.map((observer) => (
+          <Row key={observer.username} className="mt-3">
+            <Col sm={4}>
+              <span style={{ lineHeight: '24px' }}>
+                {observer.nickname || <em>Not provided</em>}
+              </span>
+            </Col>
+            <Col sm={3}>
+              <span style={{ lineHeight: '24px' }}>{observer.ip}</span>
+            </Col>
+            {user.inControl && (
+              <Col sm={5}>
+                <Button
+                  size="sm"
+                  variant="outline-secondary"
+                  className="me-3"
+                  onClick={() => dispatch(giveControl(observer.username))}
+                >
+                  Give control
+                </Button>
+                {user.isstaff && (
                   <Button
                     size="sm"
                     variant="outline-secondary"
-                    onClick={() => this.props.logoutUser(observer.username)}
+                    onClick={() => dispatch(logoutUser(observer.username))}
                   >
                     Logout
                   </Button>
-                </span>
-              )}
-            </Col>
-          )}
-        </Row>,
-      );
-    }
-
-    return observers;
-  }
-
-  render() {
-    return (
-      <Card className="mb-3">
-        <Card.Header>Users</Card.Header>
-        <Card.Body>
-          <Row>
-            <Col sm={4}>
-              <b>Name</b>
-            </Col>
-            <Col sm={4}>
-              <b>Host</b>
-            </Col>
-            <Col sm={4}>
-              <span>&nbsp;</span>
-            </Col>
+                )}
+              </Col>
+            )}
           </Row>
-          {this.getObservers()}
-        </Card.Body>
-      </Card>
-    );
-  }
+        ))}
+      </Card.Body>
+    </Card>
+  );
 }
 
-function mapStateToProps(state) {
-  return {
-    remoteAccess: state.remoteAccess,
-    login: state.login,
-  };
-}
-
-function mapDispatchToProps(dispatch) {
-  return {
-    giveControl: bindActionCreators(giveControl, dispatch),
-    logoutUser: bindActionCreators(logoutUser, dispatch),
-  };
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(UserList);
+export default UserList;
