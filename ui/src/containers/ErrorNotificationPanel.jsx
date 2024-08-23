2e020f873b3eb68878351ee2527692b0fb2ab710
Axel Bocciarelli <axelboc@gmail.com>
2024-08-23 13:30:01 +0200

    Show error when another observer is already asking for control

--- ui/src/containers/ErrorNotificationPanel.jsx
+++ ui/src/containers/ErrorNotificationPanel.jsx
@@ -1,46 +1,32 @@
-/* eslint-disable react/jsx-handler-names */
 import React from 'react';
-import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { Modal, Alert } from 'react-bootstrap';
 import { showErrorPanel } from '../actions/general';
 
-export class ErrorNotificationPanel extends React.Component {
-  render() {
-    return (
-      <Modal show={this.props.show} onHide={this.props.hideErrorPanel}>
-        <div style={{ marginBottom: '-20px' }}>
-          <Alert
-            variant="danger"
-            onClose={this.props.hideErrorPanel}
-            dismissible
-          >
-            <strong>Error:&nbsp;</strong>
-            {this.props.message}
-          </Alert>
-        </div>
-      </Modal>
-    );
-  }
+function ErrorNotificationPanel() {
+  const dispatch = useDispatch();
+
+  const show = useSelector((state) => state.general.showErrorPanel);
+  const message = useSelector((state) => state.general.errorMessage);
+
+  return (
+    <Modal
+      show={show}
+      onHide={() => dispatch(showErrorPanel(false))}
+      data-default-styles
+    >
+      <div style={{ marginBottom: '-20px' }}>
+        <Alert
+          variant="danger"
+          onClose={() => dispatch(showErrorPanel(false))}
+          dismissible
+        >
+          <strong>Error:&nbsp;</strong>
+          {message}
+        </Alert>
+      </div>
+    </Modal>
+  );
 }
 
-function mapStateToProps(state) {
-  return {
-    show: state.general.showErrorPanel,
-    message: state.general.errorMessage,
-  };
-}
-
-function mapDispatchToProps(dispatch) {
-  return {
-    hideErrorPanel: bindActionCreators(
-      showErrorPanel.bind(null, false),
-      dispatch,
-    ),
-  };
-}
-
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps,
-)(ErrorNotificationPanel);
+export default ErrorNotificationPanel;
