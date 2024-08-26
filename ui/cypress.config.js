8b85280299541ecbbe81a67cf9373bbe5eba49b0
Axel Bocciarelli <axelboc@gmail.com>
2024-08-26 13:20:49 +0200

    Improve Cypress logging to help debug CI

--- ui/cypress.config.js
+++ ui/cypress.config.js
@@ -1,10 +1,14 @@
 import { defineConfig } from 'cypress';
+import installLogsPrinter from 'cypress-terminal-report/src/installLogsPrinter.js';
 
 export default defineConfig({
   e2e: {
     baseUrl: 'http://localhost:5173',
     supportFile: 'cypress/support.js',
     screenshotsFolder: 'cypress/debug',
+    setupNodeEvents(on) {
+      installLogsPrinter(on);
+    },
   },
   viewportWidth: 1200,
   viewportHeight: 800,
