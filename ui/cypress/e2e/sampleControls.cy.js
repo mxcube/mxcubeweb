b95e9bbb28dd6c187b24040e60d224a600143802
Axel Bocciarelli <axelboc@gmail.com>
2024-10-17 11:06:46 +0200

    Merge and improve 3-click centring tests

--- ui/cypress/e2e/sampleControls.cy.js
+++ ui/cypress/e2e/sampleControls.cy.js
@@ -1,41 +1,42 @@
 /* global cy, it, describe, beforeEach */
 
-describe('3-click centring', () => {
+describe('Sample controls', () => {
   beforeEach(() => {
     cy.loginWithControl();
   });
 
-  it('displays error when no sample is mounted', () => {
+  it('3-click centring', () => {
     cy.clearSamples(); // another test may have mounted a sample
     cy.findByRole('link', { name: /Data collection/u, hidden: true }).click();
 
+    // Start 3-click centring
     cy.findByRole('button', { name: '3-click centring' }).click();
-    cy.findByRole('alert', 'Error: There is no sample mounted').should(
-      'be.visible',
-    );
-  });
 
-  it('rotates sample by 90 degrees on click', () => {
+    // Error because no sample is mounted
+    cy.findByRole('alert').should('contain', 'There is no sample mounted');
+    cy.findByRole('button', { name: 'Close alert' }).click();
+
+    // Mount sample
     cy.mountSample();
     cy.findByRole('button', { name: 'Sample: test - test' }).should(
       'be.visible',
     );
 
+    // Start 3-click centring again
     cy.findByRole('button', { name: '3-click centring' }).click();
     cy.findByText(/Clicks left: 3/u, { hidden: true }).should('exist');
 
-    cy.findByTestId('MotorInput_value_diffractometer.phi')
+    // Click on sample view to rotate sample once
+    cy.findByLabelText('Omega')
       .invoke('val')
       .then((initialValue) => {
         cy.get('.canvas-container').click();
         cy.findByText(/Clicks left: 2/u, { hidden: true }).should('exist');
 
-        // Wait for omega motor to finish moving
-        cy.wait(1000);
-
-        cy.findByTestId('MotorInput_value_diffractometer.phi')
-          .invoke('val')
-          .should('equal', (Number.parseFloat(initialValue) + 90).toFixed(2));
+        cy.findByLabelText('Omega').should(
+          'have.value',
+          (Number.parseFloat(initialValue) + 90).toFixed(2),
+        );
       });
   });
 });
