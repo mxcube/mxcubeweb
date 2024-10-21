0065e481745cfe4dd8bd4bdfb0f984495c5e79fe
Axel Bocciarelli <axelboc@gmail.com>
2024-10-21 13:49:30 +0200

    Extract `VideoSizeControl` from `SampleControls`

---
+++ ui/src/components/SampleControls/VideoSizeControl.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import { Dropdown } from 'react-bootstrap';
+import { useDispatch, useSelector } from 'react-redux';
+
+import { setVideoSize } from '../../actions/sampleview';
+import styles from './SampleControls.module.css';
+
+function VideoSizeControl() {
+  const dispatch = useDispatch();
+
+  const videoSizes = useSelector((state) => state.sampleview.videoSizes);
+  const width = useSelector((state) => state.sampleview.width);
+
+  return (
+    <Dropdown
+      drop="down-centered"
+      onSelect={(i) => dispatch(setVideoSize(...videoSizes[i]))}
+    >
+      <Dropdown.Toggle className={styles.controlDropDown}>
+        <i className={`${styles.controlIcon} fas fa-video`} />
+        <i className={`${styles.dropDownIcon} fas fa-sort-down`} />
+        <span className={styles.controlLabel}>Video size</span>
+      </Dropdown.Toggle>
+
+      <Dropdown.Menu className={styles.dropDownMenu}>
+        {videoSizes.map(([w, h], i) => {
+          const isActive = w.toString() === width;
+          return (
+            <Dropdown.Item
+              key={`${w}_${h}`}
+              className={styles.dropDownItem}
+              eventKey={i}
+              active={isActive}
+            >
+              <span className={`${isActive ? 'fas' : 'far'} fa-circle me-1`} />{' '}
+              {`${w} x ${h}`}
+            </Dropdown.Item>
+          );
+        })}
+      </Dropdown.Menu>
+    </Dropdown>
+  );
+}
+
+export default VideoSizeControl;
