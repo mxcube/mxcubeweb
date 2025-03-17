353b348764f527d51e240e0f0ecfb292be891f93
rhfogh <rhfogh@zoho.eu>
2025-03-17 11:10:32 +0000

    Hunting ruff

--- mxcubeweb/core/components/queue.py
+++ mxcubeweb/core/components/queue.py
@@ -706,14 +706,14 @@ class Queue(ComponentBase):
         """
         Retrieves the model and the queue entry for the model node with id <id>
 
-        :param int id: Node id of node to retrieve
+        :param int _id: Node id of node to retrieve
         :returns: The tuple model, entry
         :rtype: Tuple
         """
         if _id is None:
             return (
                 HWR.beamline.queue_model.get_model_root(),
-                HWR.beamline.queue_manager
+                HWR.beamline.queue_manager,
             )
         model = HWR.beamline.queue_model.get_node(int(_id))
         entry = HWR.beamline.queue_manager.get_entry_with_model(model)
