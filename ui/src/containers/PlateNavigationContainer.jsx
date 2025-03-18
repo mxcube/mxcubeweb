3aab28b636fa66a4fe33f5eaed63141110f06c4c
ssrfmx <qinwenming@sari.ac.cn>
2025-03-18 13:03:24 +0800

    静态表格写好，没有前后端连接

--- ui/src/containers/PlateNavigationContainer.jsx
+++ ui/src/containers/PlateNavigationContainer.jsx
@@ -1,24 +1,39 @@
-
-
-import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React, { useState } from 'react';
 import { Button, Table } from 'react-bootstrap';
 import './PlateNavigationContainer.css';
 
 const PlateNavigationContainer = () => {
-    const rows = ['A','B','C','D','E','F','G','H'];
+    const rows = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'];
     const cols = 12;
+    // 使用一个字符串来记录当前选中按钮的标识，初始为 null 表示没有选中的按钮
+    const [selectedButton, setSelectedButton] = useState(null);
+
+    const handleButtonClick = (rowIndex, colIndex) => {
+        // 生成当前按钮的唯一标识
+        const buttonId = `${rowIndex + 1}:${colIndex + 1}`;
+        console.log('buttonId')
+        console.log(buttonId)
+        // 更新选中按钮的标识
+        setSelectedButton(buttonId);
+    };
 
     const renderTable = () => {
         return rows.map((row, rowIndex) => (
             <tr key={rowIndex}>
-                {Array.from({ length: cols }).map((_, colIndex) => (
-                    <td key={colIndex}>
-                        <Button variant="primary">
-                            {`${row}-${colIndex + 1}`}
-                        </Button>
-                    </td>
-                ))}
+                {Array.from({ length: cols }).map((_, colIndex) => {
+                    const buttonId = `${rowIndex + 1}:${colIndex + 1}`;
+                    const isSelected = selectedButton === buttonId;
+                    return (
+                        <td key={colIndex}>
+                            <Button
+                                variant={isSelected ? 'primary' : 'outline-secondary'}
+                                onClick={() => handleButtonClick(rowIndex, colIndex)}
+                            >
+                                {row}-{colIndex+1}
+                            </Button>
+                        </td>
+                    );
+                })}
             </tr>
         ));
     };
@@ -27,16 +42,14 @@ const PlateNavigationContainer = () => {
         <>
             <div>
                 <b>Navigation:</b>
-            </div>            
+            </div>
             <div className="container mt-1 plate-navi-table-container">
                 <Table bordered>
                     <tbody>{renderTable()}</tbody>
                 </Table>
             </div>
             <div></div>
-        
         </>
-        
     );
 };
 
