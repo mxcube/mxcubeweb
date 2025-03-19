2802960fc835995e48a56061e36aae954f319e35
walesch-yan <yan.walesch@esrf.fr>
2025-03-19 13:35:13 +0100

    refactor HelpContainer

--- ui/src/containers/HelpContainer.jsx
+++ ui/src/containers/HelpContainer.jsx
@@ -1,6 +1,15 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import { Container, Card, Row, Col, Button, Form } from 'react-bootstrap';
+import React, { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
+import {
+  Container,
+  Card,
+  Row,
+  Col,
+  Button,
+  Form,
+  Alert,
+} from 'react-bootstrap';
+import { Controller, useForm } from 'react-hook-form';
 
 import { sendFeedback } from '../api/login';
 
@@ -8,184 +17,174 @@ import characterisation from '../help_videos/mx3-characterisation.ogv';
 import interleaved from '../help_videos/mx3-interleaved.ogv';
 import mesh from '../help_videos/mx3-mesh.ogv';
 
-export class HelpContainer extends React.Component {
-  constructor(props) {
-    super(props);
-    this.handleSubmit = this.handleSubmit.bind(this);
-    this.sender = '';
-    this.content = '';
-  }
+const DEFAULT_HELPFORM_VALUES = { email: '', content: '' };
 
-  handleSubmit() {
-    sendFeedback(this.sender.value, this.content.value);
-    this.sender.value = '';
-    this.content.value = '';
-  }
+function HelpContainer() {
+  const {
+    control,
+    handleSubmit: makeOnSubmit,
+    reset,
+    register,
+    setError,
+    formState: { isDirty, isSubmitSuccessful, errors },
+  } = useForm({
+    defaultValues: DEFAULT_HELPFORM_VALUES,
+  });
+  const user = useSelector((state) => state.login.user);
+  const serverVersion = useSelector((state) => state.general.serverVersion);
+  const [showSuccess, setShowSuccess] = useState(false);
 
-  localContactPanel() {
-    let panel = null;
-
-    if (this.props.login.user) {
-      const familyName = this.props.login.user.nickname || '';
-      const givenName = this.props.login.user.nickname || '';
-      const email = this.props.login.user.email || '';
-      const tel = '';
-
-      panel = (
-        <Card className="mb-3">
-          <Card.Header>
-            <span>Local Contact</span>
-            <span className="me-3 position-absolute end-0">
-              <i className="fas fa-user" />
-            </span>
-          </Card.Header>
-          <Card.Body>
-            <span>
-              Name: {givenName} {familyName}
-              <br />
-              Email: {email}
-              <br />
-              Tel: {tel} <br />
-            </span>
-          </Card.Body>
-        </Card>
-      );
+  async function handleSubmit(data) {
+    try {
+      await sendFeedback(data.email, data.content);
+    } catch {
+      setError('content', {
+        type: 'manual',
+        message: 'Failed to send feedback.',
+      });
     }
-    return panel;
   }
 
-  render() {
-    let links = [];
-
-    if (import.meta.env.VITE_HELP_LINKS) {
-      links = import.meta.env.VITE_HELP_LINKS.map((link) => (
-        // eslint-disable-next-line react/jsx-key
-        <div>
-          <a target="_blank" href={link.url} rel="noreferrer">
-            {link.name}
-          </a>
-        </div>
-      ));
+  useEffect(() => {
+    if (isSubmitSuccessful) {
+      reset();
+      setShowSuccess(true);
     }
+  }, [isSubmitSuccessful, reset]);
 
-    return (
-      <Container fluid className="mt-4">
-        <Row>
-          <Col sm={12} className="d-flex">
-            <Col sm={4}>
-              {this.localContactPanel()}
+  useEffect(() => {
+    if (isDirty) {
+      setShowSuccess(false);
+    }
+  }, [isDirty]);
+
+  return (
+    <Container fluid className="mt-4">
+      <Row>
+        <Col sm={12} className="d-flex">
+          <Col sm={4}>
+            {user && (
               <Card className="mb-3">
                 <Card.Header>
-                  <span>Feedback</span>
-                  <span className="me-3 position-absolute end-0">
-                    <i className="fas fa-envelope" />
-                  </span>
+                  Local Contact
+                  <i className="me-3 position-absolute end-0 fas fa-user" />
                 </Card.Header>
+                <Card.Body>
+                  <div>Name: {user.nickname}</div>
+                  <div>Email: {user.email}</div>
+                </Card.Body>
+              </Card>
+            )}
+            <Card className="mb-3">
+              <Card.Header>
+                Feedback
+                <i className="me-3 position-absolute end-0 fas fa-envelope" />
+              </Card.Header>
 
-                <Card.Body>
-                  <Form>
-                    <Form.Group className="mb-3">
-                      <Form.Label htmlFor="EmailAddress">
-                        Your email, Name or Proposal
-                      </Form.Label>
-                      <Form.Control
-                        required
-                        type="email"
-                        id="EmailAddress"
-                        placeholder="Your contact information (email, Name or Proposal)"
-                        ref={(ref) => {
-                          this.sender = ref;
-                        }}
-                      />
-                    </Form.Group>
-                    <Form.Group className="mb-3">
-                      <Form.Label htmlFor="Content">Content : </Form.Label>
-                      <Form.Control
-                        required
-                        as="textarea"
-                        rows={7}
-                        id="Content"
-                        placeholder="Let us know whats on your mind !"
-                        ref={(ref) => {
-                          this.content = ref;
-                        }}
-                      />
-                    </Form.Group>
-                    <Form.Group className="jus">
-                      <Button onClick={this.handleSubmit}>Submit</Button>
-                    </Form.Group>
-                  </Form>
-                </Card.Body>
-              </Card>
-              <Card className="mb-2">
-                <Card.Header>
-                  <span>About MXCuBE-Web</span>
-                </Card.Header>
-                <Card.Body>
-                  <span>Version: {this.props.general.serverVersion}</span>
-                </Card.Body>
-              </Card>
-            </Col>
-            <Col sm={1} />
-            <Col xs={7}>
-              <Card className="mb-2">
-                <Card.Header>Video Tutorials</Card.Header>
-                <Card.Body>
-                  <Row>
-                    <Col className="col-xs-4">
-                      <span>
-                        <b>Characterisation </b> <br />
-                        <video width="230" height="132" controls>
-                          <source src={characterisation} type="video/mp4" />
-                        </video>
-                      </span>
-                    </Col>
-                    <Col className="col-xs-4">
-                      <span>
-                        <b>Interleaved </b> <br />
-                        <video width="230" height="132" controls>
-                          <source src={interleaved} type="video/mp4" />
-                        </video>
-                      </span>
-                    </Col>
-                    <Col className="col-xs-4">
-                      <span>
-                        <b>Mesh </b> <br />
-                        <video width="230" height="132" controls>
-                          <source src={mesh} type="video/mp4" />
-                        </video>
-                      </span>
-                    </Col>
-                  </Row>
-                </Card.Body>
-              </Card>
-              {import.meta.env.VITE_HELP_LINKS ? (
-                <Card
-                  header={
-                    <div>
-                      <span>Help Links</span>
-                      <span className="me-3 position-absolute end-0">
-                        <i className="fas fa-info-circle" />
-                      </span>
-                    </div>
-                  }
-                >
-                  <span>{links}</span>
-                </Card>
-              ) : null}
-            </Col>
+              <Card.Body>
+                <Form onSubmit={makeOnSubmit(handleSubmit)}>
+                  <Form.Group className="mb-3">
+                    <Form.Label htmlFor="EmailAddress">
+                      Your email, Name or Proposal
+                    </Form.Label>
+                    <Controller
+                      name="email"
+                      control={control}
+                      rules={{ required: 'Email is required' }}
+                      render={() => (
+                        <Form.Control
+                          type="text"
+                          id="EmailAddress"
+                          placeholder="Your contact information (email, Name or Proposal)"
+                          required
+                          isInvalid={errors.email}
+                          {...register('email')}
+                        />
+                      )}
+                    />
+                    {errors.email && (
+                      <Form.Control.Feedback type="invalid">
+                        {errors.email.message}
+                      </Form.Control.Feedback>
+                    )}
+                  </Form.Group>
+                  <Form.Group className="mb-3">
+                    <Form.Label htmlFor="Content">Content : </Form.Label>
+                    <Controller
+                      name="content"
+                      control={control}
+                      rules={{ required: 'Content is required' }}
+                      render={() => (
+                        <Form.Control
+                          as="textarea"
+                          rows={7}
+                          id="Content"
+                          placeholder="Let us know whats on your mind !"
+                          required
+                          isInvalid={errors.content}
+                          {...register('content')}
+                        />
+                      )}
+                    />
+                    {errors.content && (
+                      <Form.Control.Feedback type="invalid">
+                        {errors.content.message}
+                      </Form.Control.Feedback>
+                    )}
+                  </Form.Group>
+                  <Form.Group>
+                    <Button type="submit">Submit</Button>
+                  </Form.Group>
+                  {showSuccess && (
+                    <Alert
+                      variant="success"
+                      className="mt-3"
+                      onClose={() => setShowSuccess(false)}
+                      dismissible
+                    >
+                      Feedback sent successfully
+                    </Alert>
+                  )}
+                </Form>
+              </Card.Body>
+            </Card>
+            <Card className="mb-2">
+              <Card.Header>About MXCuBE-Web</Card.Header>
+              <Card.Body>Version: {serverVersion}</Card.Body>
+            </Card>
           </Col>
-        </Row>
-      </Container>
-    );
-  }
+          <Col sm={1} />
+          <Col xs={7}>
+            <Card className="mb-2">
+              <Card.Header>Video Tutorials</Card.Header>
+              <Card.Body>
+                <Row>
+                  <Col className="col-xs-4">
+                    <h3>Characterisation </h3>
+                    <video width="230" height="132" controls>
+                      <source src={characterisation} type="video/mp4" />
+                    </video>
+                  </Col>
+                  <Col className="col-xs-4">
+                    <h3>Interleaved </h3>
+                    <video width="230" height="132" controls>
+                      <source src={interleaved} type="video/mp4" />
+                    </video>
+                  </Col>
+                  <Col className="col-xs-4">
+                    <h3>Mesh </h3>
+                    <video width="230" height="132" controls>
+                      <source src={mesh} type="video/mp4" />
+                    </video>
+                  </Col>
+                </Row>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Col>
+      </Row>
+    </Container>
+  );
 }
 
-function mapStateToProps(state) {
-  return {
-    login: state.login,
-    general: state.general,
-  };
-}
-
-export default connect(mapStateToProps)(HelpContainer);
+export default HelpContainer;
