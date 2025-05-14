mxcube/esrf-mxcubeweb

add _config to get some beamline properties.

mxcubeweb/core/components/queue.py
@@ -2336,7 +2336,7 @@ class Queue(ComponentBase):
                 "subDirTemplate": "{ACRONYM}/{ACRONYM}-{NAME}",
                 "experiment_type": "",
             },
-            "limits": HWR.beamline.acquisition_limit_values,
+            "limits": HWR.beamline._config.acquisition_limit_values,
             "requires": requires if requires else [],
             "name": display_name if display_name else task_name,
             "queue_entry": task_name,
@@ -2366,7 +2366,7 @@ class Queue(ComponentBase):
     def get_available_tasks(self):
         task_info = {}
 
-        for task, available in HWR.beamline.available_methods.items():
+        for task, available in HWR.beamline._config.available_methods.items():
             if available:
                 task_info[task] = self.get_default_task_parameters(task)
 

mxcubeweb/core/components/sampleview.py
@@ -223,7 +223,7 @@ class SampleView(ComponentBase):
         dm.connect("centringFailed", self.wait_for_centring_finishes)
         dm.connect("centringAccepted", self.centring_add_current_point)
         HWR.beamline.sample_view.connect("newGridResult", self.handle_grid_result)
-        self._click_limit = int(HWR.beamline.click_centring_num_clicks or 3)
+        self._click_limit = int(HWR.beamline._config.click_centring_num_clicks or 3)
 
     def set_image_size(self, width, height):
         HWR.beamline.sample_view.camera.restart_streaming((width, height))

mxcubeweb/routes/main.py
@@ -47,13 +47,14 @@ def init_route(app, server, url_prefix):
     @server.restrict
     @server.validate(resp=Response(HTTP_200=AppSettingsModel))
     def mxcube_mode():
+        _blc = HWR.beamline._config
         return jsonify(
             {
                 "mode": app.CONFIG.app.mode,
                 "version": __version__.__version__,
-                "mesh_result_format": HWR.beamline.mesh_result_format,
-                "use_native_mesh": HWR.beamline.use_native_mesh,
-                "enable_2d_points": HWR.beamline.enable_2d_points,
+                "mesh_result_format": _blc.mesh_result_format,
+                "use_native_mesh": _blc.use_native_mesh,
+                "enable_2d_points": _blc.enable_2d_points,
             }
         )
 

mxcube/mxcubeweb

mxcubeweb/core/components/queue.py
@@ -2331,7 +2331,7 @@ class Queue(ComponentBase):
                 "subDirTemplate": "{ACRONYM}/{ACRONYM}-{NAME}",
                 "experiment_type": "",
             },
-            "limits": HWR.beamline.acquisition_limit_values,
+            "limits": HWR.beamline.config.acquisition_limit_values,
             "requires": requires if requires else [],
             "name": display_name if display_name else task_name,
             "queue_entry": task_name,
@@ -2361,7 +2361,7 @@ class Queue(ComponentBase):
     def get_available_tasks(self):
         task_info = {}
 
-        for task, available in HWR.beamline.available_methods.items():
+        for task, available in HWR.beamline.config.available_methods.items():
             if available:
                 task_info[task] = self.get_default_task_parameters(task)
 

mxcubeweb/core/components/sampleview.py
@@ -223,7 +223,7 @@ class SampleView(ComponentBase):
         dm.connect("centringFailed", self.wait_for_centring_finishes)
         dm.connect("centringAccepted", self.centring_add_current_point)
         HWR.beamline.sample_view.connect("newGridResult", self.handle_grid_result)
-        self._click_limit = int(HWR.beamline.click_centring_num_clicks or 3)
+        self._click_limit = int(HWR.beamline.config.click_centring_num_clicks or 3)
 
     def set_image_size(self, width, height):
         HWR.beamline.sample_view.camera.restart_streaming((width, height))

mxcubeweb/routes/main.py
@@ -47,13 +47,14 @@ def init_route(app, server, url_prefix):
     @server.restrict
     @server.validate(resp=Response(HTTP_200=AppSettingsModel))
     def mxcube_mode():
+        _blc = HWR.beamline.config
         return jsonify(
             {
                 "mode": app.CONFIG.app.mode,
                 "version": __version__.__version__,
-                "mesh_result_format": HWR.beamline.mesh_result_format,
-                "use_native_mesh": HWR.beamline.use_native_mesh,
-                "enable_2d_points": HWR.beamline.enable_2d_points,
+                "mesh_result_format": _blc.mesh_result_format,
+                "use_native_mesh": _blc.use_native_mesh,
+                "enable_2d_points": _blc.enable_2d_points,
             }
         )
 