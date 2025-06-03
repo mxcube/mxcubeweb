c5c847b8155e8a1a2327ba5a471e9a160374fc3e
Mathis Loussert <mathis.loussert@esrf.fr>
2025-06-03 10:59:30 +0200

    Correction B103 0o666

--- mxcubeweb/app.py
+++ mxcubeweb/app.py
@@ -227,7 +227,7 @@ class MXCUBEApplication:
                 fpt = open(log_file, "w")
                 fpt.write(" ")
                 fpt.close()
-            os.chmod(log_file, 0o666)
+            os.chmod(log_file, 0o600)
             Path(log_file).touch()
 
             log_file_handler = TimedRotatingFileHandler(
@@ -240,7 +240,7 @@ class MXCUBEApplication:
                 fpt = open(uilog_file, "w")
                 fpt.write(" ")
                 fpt.close()
-            os.chmod(uilog_file, 0o666)
+            os.chmod(uilog_file, 0o600)
             Path(uilog_file).touch()
 
             uilog_file_handler = TimedRotatingFileHandler(
