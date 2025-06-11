35abc9e05183a493a47aaab0981e54ae1510294f
Mathis Loussert <mathis.loussert@esrf.fr>
2025-06-11 10:35:42 +0200

    B113 request without timeout

--- mxcubeweb/core/components/user/usermanager.py
+++ mxcubeweb/core/components/user/usermanager.py
@@ -236,7 +236,7 @@ class BaseUserManager(ComponentBase):
                 data={
                     "grant_type": "refresh_token",
                     "refresh_token": current_user.refresh_token,
-                },
+                },timeout=5
             ).json()
         )
 
@@ -596,7 +596,7 @@ class UserManager(BaseUserManager):
                     "client_id": self.app.CONFIG.sso.CLIENT_ID,
                     "client_secret": self.app.CONFIG.sso.CLIENT_SECRET,
                     "refresh_token": refresh_token,
-                },
+                },timeout=5
             )
 
 
