54b51b039100ea9d53799d26ab48189ef1c8e499
dominikatrojanowska <154609837+dominikatrojanowska@users.noreply.github.com>
2025-06-13 09:13:34 +0200

    Revert "Fix custom tasks duplication issue:"
    This reverts commit 5b3fced0fe1983c8dd7eba49cbee08b4b2dbd1f0.

--- mxcubeweb/core/components/queue.py
+++ mxcubeweb/core/components/queue.py
@@ -1582,9 +1582,7 @@ class Queue(ComponentBase):
             )
 
         full_path, process_path = HWR.beamline.session.get_full_path(
-            os.path.join(
-                params.get("subdir", ""), params.get("experiment_name", "") or ""
-            ),
+            os.path.join(params.get("subdir", ""), params.get("experiment_name", "")),
             task_name,
         )
         acq.path_template.directory = full_path
