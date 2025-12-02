4b9d00525593b67348752c9980b02558a174cbd4
Axel Bocciarelli <axelboc@gmail.com>
2025-12-02 09:18:53 +0100

    Convert `SampleViewContainer` to function component

--- ui/src/containers/SampleViewContainer.jsx
+++ ui/src/containers/SampleViewContainer.jsx
@@ -1,14 +1,7 @@
-/* eslint-disable react/destructuring-assignment */
-import { Component } from 'react';
 import { Col, Container, Row } from 'react-bootstrap';
-import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
+import { connect, useDispatch } from 'react-redux';
 
-import {
-  executeCommand,
-  logFrontEndTraceBack,
-  setAttribute,
-} from '../actions/beamline';
+import { executeCommand, setAttribute } from '../actions/beamline';
 import { addShape } from '../actions/sampleview';
 import { showTaskForm } from '../actions/taskForm';
 import PlateManipulator from '../components/Equipment/PlateManipulator';
@@ -24,14 +17,22 @@ import DefaultErrorBoundary from './DefaultErrorBoundary';
 import SampleQueueContainer from './SampleQueueContainer';
 import styles from './SampleViewContainer.module.css';
 
-class SampleViewContainer extends Component {
-  constructor(props) {
-    super(props);
-    this.getControlAvailability = this.getControlAvailability.bind(this);
-  }
+function SampleViewContainer(props) {
+  const {
+    uiproperties,
+    currentSampleID,
+    shapes = {},
+    mode,
+    sampleList,
+    defaultParameters,
+    groupFolder,
+    hardwareObjects,
+    sampleChangerContents,
+  } = props;
 
-  getControlAvailability(name) {
-    const { uiproperties } = this.props;
+  const dispatch = useDispatch();
+
+  function getControlAvailability(name) {
     const available = uiproperties.sample_view_video_controls.components.find(
       (component) => component.id === name && component.show === true,
     );
@@ -39,126 +40,119 @@ class SampleViewContainer extends Component {
     return available?.show || false;
   }
 
-  render() {
-    const { uiproperties, currentSampleID, shapes = {} } = this.props;
+  if (!('sample_view_motors' in uiproperties)) {
+    return null;
+  }
 
-    if (!('sample_view_motors' in uiproperties)) {
-      return null;
-    }
+  const phase_control = uiproperties.sample_view?.components?.find(
+    (c) => c.attribute === 'phase_control',
+  );
+  const beam_size = uiproperties?.sample_view?.components.find(
+    (c) => c.attribute === 'beam_size',
+  );
 
-    const phase_control = uiproperties.sample_view?.components?.find(
-      (c) => c.attribute === 'phase_control',
-    );
-    const beam_size = uiproperties?.sample_view?.components.find(
-      (c) => c.attribute === 'beam_size',
-    );
+  const points = Object.fromEntries(
+    Object.entries(shapes).filter(([_, shape]) => shape.t === 'P'),
+  );
+  const twoDPoints = Object.fromEntries(
+    Object.entries(shapes).filter(([_, shape]) => shape.t === '2DP'),
+  );
+  const lines = Object.fromEntries(
+    Object.entries(shapes).filter(([_, shape]) => shape.t === 'L'),
+  );
+  const grids = Object.fromEntries(
+    Object.entries(shapes).filter(([_, shape]) => shape.t === 'G'),
+  );
+  const selectedGrids = Object.values(grids).filter((s) => s.selected);
 
-    const points = Object.fromEntries(
-      Object.entries(shapes).filter(([_, shape]) => shape.t === 'P'),
-    );
-    const twoDPoints = Object.fromEntries(
-      Object.entries(shapes).filter(([_, shape]) => shape.t === '2DP'),
-    );
-    const lines = Object.fromEntries(
-      Object.entries(shapes).filter(([_, shape]) => shape.t === 'L'),
-    );
-    const grids = Object.fromEntries(
-      Object.entries(shapes).filter(([_, shape]) => shape.t === 'G'),
-    );
-    const selectedGrids = Object.values(grids).filter((s) => s.selected);
+  return (
+    <Container fluid>
+      <Row
+        style={{
+          background: '#fafafa',
+          borderBottom: '1px solid lightgray',
+          paddingBottom: '0em',
+        }}
+      >
+        <Col sm={12}>
+          <DefaultErrorBoundary>
+            <BeamlineSetupContainer />
+          </DefaultErrorBoundary>
+        </Col>
+      </Row>
+      <Row className="gx-3 mt-2 pt-1">
+        <Col sm={2} xxl={1} className={styles.controllers}>
+          <DefaultErrorBoundary>
+            {phase_control !== undefined && (
+              <div className={motorInputStyles.container}>
+                <label className={motorInputStyles.label} htmlFor="PhaseInput">
+                  {phase_control.label}
+                </label>
+                <PhaseInput />
+              </div>
+            )}
+            {beam_size !== undefined && (
+              <div className={motorInputStyles.container}>
+                <label
+                  className={motorInputStyles.label}
+                  htmlFor="ApertureInput"
+                >
+                  {beam_size.label}
+                </label>
+                <ApertureInput />
+              </div>
+            )}
 
-    return (
-      <Container fluid>
-        <Row
-          style={{
-            background: '#fafafa',
-            borderBottom: '1px solid lightgray',
-            paddingBottom: '0em',
-          }}
-        >
-          <Col sm={12}>
-            <DefaultErrorBoundary>
-              <BeamlineSetupContainer />
-            </DefaultErrorBoundary>
-          </Col>
-        </Row>
-        <Row className="gx-3 mt-2 pt-1">
-          <Col sm={2} xxl={1} className={styles.controllers}>
-            <DefaultErrorBoundary>
-              {phase_control !== undefined && (
-                <div className={motorInputStyles.container}>
-                  <label
-                    className={motorInputStyles.label}
-                    htmlFor="PhaseInput"
-                  >
-                    {phase_control.label}
-                  </label>
-                  <PhaseInput />
-                </div>
-              )}
-              {beam_size !== undefined && (
-                <div className={motorInputStyles.container}>
-                  <label
-                    className={motorInputStyles.label}
-                    htmlFor="ApertureInput"
-                  >
-                    {beam_size.label}
-                  </label>
-                  <ApertureInput />
-                </div>
-              )}
-
-              {this.props.mode === 'SSX-CHIP' && (
-                <SSXChipControl
-                  showForm={this.props.showForm}
-                  currentSampleID={currentSampleID}
-                  sampleData={this.props.sampleList[currentSampleID]}
-                  defaultParameters={this.props.defaultParameters}
-                  groupFolder={this.props.groupFolder}
-                  hardwareObjects={this.props.hardwareObjects}
-                  uiproperties={uiproperties.sample_view_motors}
-                  addShape={this.props.addShape}
-                  grids={grids}
-                  selectedGrids={selectedGrids}
-                  setAttribute={this.props.setAttribute}
-                  sendExecuteCommand={this.props.sendExecuteCommand}
-                />
-              )}
-              {this.props.sampleChangerContents.name === 'PlateManipulator' && (
-                <PlateManipulator inPopover />
-              )}
-
-              <MotorControls />
-            </DefaultErrorBoundary>
-          </Col>
-          <Col sm={6}>
-            <DefaultErrorBoundary>
-              <ContextMenu
-                getControlAvailability={this.getControlAvailability}
-              />
-              <SampleImage
-                points={points}
-                twoDPoints={twoDPoints}
-                lines={lines}
+            {mode === 'SSX-CHIP' && (
+              <SSXChipControl
+                showForm={(...args) => dispatch(showTaskForm(...args))}
+                currentSampleID={currentSampleID}
+                sampleData={sampleList[currentSampleID]}
+                defaultParameters={defaultParameters}
+                groupFolder={groupFolder}
+                hardwareObjects={hardwareObjects}
+                uiproperties={uiproperties.sample_view_motors}
+                addShape={(...args) => dispatch(addShape(...args))}
                 grids={grids}
                 selectedGrids={selectedGrids}
+                setAttribute={(...args) => dispatch(setAttribute(...args))}
+                sendExecuteCommand={(...args) =>
+                  dispatch(executeCommand(...args))
+                }
               />
-            </DefaultErrorBoundary>
-          </Col>
-          <Col
-            sm={4}
-            xxl={5}
-            className={styles.queue}
-            style={{ display: 'flex' }}
-          >
-            <DefaultErrorBoundary>
-              <SampleQueueContainer />
-            </DefaultErrorBoundary>
-          </Col>
-        </Row>
-      </Container>
-    );
-  }
+            )}
+            {sampleChangerContents.name === 'PlateManipulator' && (
+              <PlateManipulator inPopover />
+            )}
+
+            <MotorControls />
+          </DefaultErrorBoundary>
+        </Col>
+        <Col sm={6}>
+          <DefaultErrorBoundary>
+            <ContextMenu getControlAvailability={getControlAvailability} />
+            <SampleImage
+              points={points}
+              twoDPoints={twoDPoints}
+              lines={lines}
+              grids={grids}
+              selectedGrids={selectedGrids}
+            />
+          </DefaultErrorBoundary>
+        </Col>
+        <Col
+          sm={4}
+          xxl={5}
+          className={styles.queue}
+          style={{ display: 'flex' }}
+        >
+          <DefaultErrorBoundary>
+            <SampleQueueContainer />
+          </DefaultErrorBoundary>
+        </Col>
+      </Row>
+    </Container>
+  );
 }
 
 function mapStateToProps(state) {
@@ -176,17 +170,4 @@ function mapStateToProps(state) {
   };
 }
 
-function mapDispatchToProps(dispatch) {
-  return {
-    addShape: bindActionCreators(addShape, dispatch),
-    showForm: bindActionCreators(showTaskForm, dispatch),
-    setAttribute: bindActionCreators(setAttribute, dispatch),
-    sendExecuteCommand: bindActionCreators(executeCommand, dispatch),
-    logFrontEndTraceBack: bindActionCreators(logFrontEndTraceBack, dispatch),
-  };
-}
-
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps,
-)(SampleViewContainer);
+export default connect(mapStateToProps)(SampleViewContainer);
