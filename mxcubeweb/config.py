1db775884780e3e4ae8477d9ff3a895b44cd8679
Pedro Benetton <pedro.benetton@lnls.br>
2026-01-28 10:29:38 -0300

    Implement environment variable reading at yaml files
    ConfigLoader now replaces any string that starts with "_ENV_" with the
    environment variable of that value. It walk recusrively through all
    the data that has been collected from yaml file, assumes this data
    is inside lists or dictionaries, and performs the change at the
    strings.
    
    The reason for implementing this is because we need multiple
    CLIENT_ID variables. In this case, setting _ENV_CLIENT_ID in
    the environment and then setting CLIENT_ID: _ENV_CLIENT_ID in
    the yaml configuration file works.

--- mxcubeweb/config.py
+++ mxcubeweb/config.py
@@ -20,9 +20,34 @@ from mxcubeweb.core.models.configmodels import (
 
 class ConfigLoader:
     @staticmethod
+    def resolve_env(value):
+        if isinstance(value, str) and value.startswith("_ENV_"):
+            env_value = os.getenv(value)
+            if env_value is None:
+                error_msg = f"{value} starts with _ENV_ but was not set in environment"
+                raise RuntimeError(error_msg)
+            logging.getLogger("HWR").debug(f"Environment variable {value} has been set")
+            return env_value
+        return value
+
+    @staticmethod
+    def walk_and_resolve(data):
+        if isinstance(data, dict):
+            return {
+                k: ConfigLoader.walk_and_resolve(ConfigLoader.resolve_env(v))
+                for k, v in data.items()
+            }
+        if isinstance(data, list):
+            return [
+                ConfigLoader.walk_and_resolve(ConfigLoader.resolve_env(x)) for x in data
+            ]
+        return ConfigLoader.resolve_env(data)
+
+    @staticmethod
     def load(path: str, schema: BaseModel):
         with open(os.path.join(path), encoding="utf-8") as f:
             config = ruamel.yaml.YAML().load(f.read())
+            config = ConfigLoader.walk_and_resolve(config)
             try:
                 model = schema.parse_obj(config)
             except ValidationError:
